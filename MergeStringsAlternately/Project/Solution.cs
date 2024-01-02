007761ced9c1d81d242d3e49fafeda661bd671d4
Wirt4 <wirt.salthouse@gmail.com>
2024-01-01 16:14:56 -0800

    update to pass tests

--- MergeStringsAlternately/Project/Solution.cs
+++ MergeStringsAlternately/Project/Solution.cs
@@ -3,6 +3,26 @@
 public class Solution
 {
  public string MergeAlternately(string word1, string word2) {
-        return "";
+        string merged = "";
+
+        int i = 0;
+        int j = 0;
+
+        while (i < word1.Length && j < word2.Length){
+            merged += word1[i];
+            merged += word2[j];
+            i++;
+            j++;
+        }
+
+        if (i< word1.Length){
+            merged += word1.Substring(i);
+        }
+
+        if (i < word2.Length){
+            merged += word2.Substring(j);
+        }
+
+        return merged;
     }
 }
