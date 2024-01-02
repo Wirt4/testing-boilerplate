8ec6adf2468476fec7a6acc8e8eee310b46be030
Wirt4 <wirt.salthouse@gmail.com>
2024-01-01 16:08:47 -0800

    added failing unit test

---
+++ MergeStringsAlternately/Project/Solution.cs
@@ -0,0 +1,8 @@
+﻿namespace Project;
+
+public class Solution
+{
+ public string MergeAlternately(string word1, string word2) {
+        return "";
+    }
+}
--- MergeStringsAlternately/Tests/UnitTest1.cs
+++ MergeStringsAlternately/Tests/UnitTest1.cs
@@ -1,10 +1,16 @@
 namespace Tests;
 
+using Project;
+using static Project.Solution;
+
 public class UnitTest1
 {
     [Fact]
     public void Test1()
     {
+        Solution solution = new Solution();
+        string result = solution.MergeAlternately("abc", "pqr");
 
+        Assert.Equal("apbqcr", result);
     }
 }
