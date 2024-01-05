4ba600f272b6e4a92c60dda4e55ac94a604a7d75
Wirt4 <wirt.salthouse@gmail.com>
2024-01-04 20:34:24 -0800

    reduce duplicated work

--- LeetCodeSolutions/CanPlaceFlowers/FlowerBed.cs
+++ LeetCodeSolutions/CanPlaceFlowers/FlowerBed.cs
@@ -1,12 +1,52 @@
 namespace LeetCodeSolutions;
 public class FlowerBed{
-     public bool CanPlaceFlowers(int[] flowerbed, int n) {
-        
+    
+    private void markAndDeprecate(ref int[] flowerbed, int ndx, ref int n){
+        flowerbed[ndx] = 1;
+        n--;
+    }
 
+    private bool isFree(ref int[] flowerbed, int ndx){
+        return flowerbed[ndx] == 0;
+    }
+     public bool CanPlaceFlowers(int[] flowerbed, int n) {
+
+        bool oneFlowerAndFirstFree = n == 1 && isFree(ref flowerbed, 0);
+        
         if (flowerbed.Length == 1){
-            return flowerbed[0] == 0;
+            return oneFlowerAndFirstFree;
+        }
+        
+        if(flowerbed.Length == 2){
+            return oneFlowerAndFirstFree && isFree(ref flowerbed, 1);
         }
 
-        return n == 1;
+        for(int i = 0; i<flowerbed.Length; i++){
+
+            if (!isFree (ref flowerbed, i)){
+                continue;
+            }
+
+            if (i == 0){
+                if (isFree(ref flowerbed, i+1)){
+                    markAndDeprecate(ref flowerbed, i, ref n);
+                }
+                continue;
+            }
+
+            if (i == flowerbed.Length - 1){
+                if (isFree(ref flowerbed, i-1)){
+                    markAndDeprecate(ref flowerbed, i, ref n);
+                }
+                continue;
+            }
+
+            if (isFree(ref flowerbed, i-1) && isFree(ref flowerbed, i+1)){
+                markAndDeprecate(ref flowerbed, i, ref n);
+            }
+
+        }
+
+        return n <= 0;
     }
 }
