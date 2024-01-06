5ae43d028f8f4dd0fc7c7cae4bdb8f7baa141999
Wirt4 <wirt.salthouse@gmail.com>
2024-01-05 18:54:00 -0800

    version submitted to leetcode

--- LeetCodeSolutions/ReverseWordsInAString/ReverseWords.cs
+++ LeetCodeSolutions/ReverseWordsInAString/ReverseWords.cs
@@ -1,7 +1,30 @@
+using System.Text;
+
 namespace LeetCodeSolutions;
 
 public class ReverseWordsSolution{
      public string ReverseWords(string s) {
-        return s;
+        char space = ' ';
+        string [] words = s.Trim().Split(space);
+
+
+        int wordsLength = words.Length;
+        if (wordsLength == 1){
+            return words[0];
+        }
+
+        StringBuilder builder = new StringBuilder(s.Length);
+        
+        for(int i = wordsLength - 1; i >= 0; i--){
+            if (!String.IsNullOrEmpty(words[i])){
+                builder.Append(words[i]);
+                if (i > 0){
+                       builder.Append(space);
+                }
+            }
+        }
+
+
+        return builder.ToString();
     }
 }
--- LeetCodeTests/ReverseWordsTests.cs
+++ LeetCodeTests/ReverseWordsTests.cs
@@ -14,4 +14,22 @@ public class ReverseWordsTests{
         ReverseWordsSolution rWords = new ReverseWordsSolution();
         Assert.Equal("goodbye", rWords.ReverseWords("goodbye"));
     }
+
+    [Fact]
+    public void twoWords(){
+        ReverseWordsSolution rWords = new ReverseWordsSolution();
+        Assert.Equal("boingo oingo", rWords.ReverseWords("oingo boingo"));
+    }
+
+    [Fact]
+    public void oneWordExtraSpaces(){
+         ReverseWordsSolution rWords = new ReverseWordsSolution();
+        Assert.Equal("music", rWords.ReverseWords("    music "));
+    }
+
+     [Fact]
+     public void twoWordsExtraSpaces(){
+        ReverseWordsSolution rWords = new ReverseWordsSolution();
+        Assert.Equal("music great", rWords.ReverseWords(" great   music "));
+    }
 }
