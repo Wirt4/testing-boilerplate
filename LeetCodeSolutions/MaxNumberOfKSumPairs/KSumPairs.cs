d65443838fa418c6df6399610a70541cf04bf707
Wirt4 <wirt.salthouse@gmail.com>
2024-01-11 13:04:05 -0800

    pass initial tests

--- LeetCodeSolutions/MaxNumberOfKSumPairs/KSumPairs.cs
+++ LeetCodeSolutions/MaxNumberOfKSumPairs/KSumPairs.cs
@@ -1,7 +1,43 @@
+using System.Globalization;
+
 namespace LeetCodeSolutions;
 
 public class KSumPairs{
     public int MaxOperations(int[] nums, int k){
-        return 2;
+        Dictionary <int, int> valueFrequency = new Dictionary<int, int>();
+        foreach(int num in nums ){
+            if (valueFrequency.ContainsKey(num)){
+                valueFrequency[num] += 1;
+                continue;
+            }
+            valueFrequency.Add(num, 1);
+        }
+
+        int pairs = 0;
+
+        foreach(int num in nums){
+            int diff = k-num;
+            if (!valueFrequency.ContainsKey(diff) || valueFrequency[diff] <=0){
+                continue;
+            }
+
+            int min = valueFrequency[num];
+
+            if (valueFrequency[diff]< valueFrequency[num]){
+                min = valueFrequency[diff];
+            }
+
+            if (valueFrequency[diff] ==valueFrequency[min]){
+                pairs += min/2;
+            }else{
+                  pairs += min;
+            }
+
+            valueFrequency[num] -= min;
+            valueFrequency[diff] -= min;
+
+
+        }
+        return pairs;
     }
 }
