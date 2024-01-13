ca7e072f9b138d19aaacddee165a71dae03ca9a6
Wirt4 <wirt.salthouse@gmail.com>
2024-01-12 19:33:32 -0800

    update to more performant algorithm

--- LeetCodeSolutions/DailyTemperatures/Temperatures.cs
+++ LeetCodeSolutions/DailyTemperatures/Temperatures.cs
@@ -1,53 +1,46 @@
 using System.Collections.Concurrent;
 using System.Reflection.Metadata.Ecma335;
+using System.Security.AccessControl;
 
 namespace LeetCodeSolutions;
 public class Temperatures{
+    private class TemperatureOnDate{
+        private int day;
+        private int temperature;
+        public TemperatureOnDate(int day, int temperature){
+            this.day = day;
+            this.temperature = temperature;
+        }
+
+        public int Temperature => temperature;
+        public int Day => day;
+    }
     public int [] DailyTemperatures(int[] temperatures){
-    Stack <int> left = new Stack<int>();
-    Stack <int> right = new Stack<int>();
-    //build stacks 
-    for (int i=0; i< temperatures.Length; i++){
-        int cur = temperatures[i];
-
-        while (left.Count > 0 && cur < temperatures[left.Peek()]){
-            right.Push(left.Pop());
+        int arrLen = temperatures.Length;
+        if (arrLen == 1){
+            return [0];
         }
 
-        while (right.Count > 0 && cur >= temperatures[right.Peek()] ){
-            left.Push(right.Pop());
+        int[] counts = new int[arrLen];
+
+        Stack<TemperatureOnDate> trackStack = new Stack<TemperatureOnDate>();
+        int lastIndex = arrLen -1;
+        trackStack.Push( new TemperatureOnDate (lastIndex, temperatures[lastIndex]));
+        for(int i= lastIndex-1; i>=0; i--){
+            int currentCount =1;
+            while (trackStack.Count  > 0 && trackStack.Peek().Temperature <= temperatures[i]){
+                TemperatureOnDate temp = trackStack.Pop();
+                currentCount += temp.Day;
+            }
+
+            if (trackStack.Count ==0){
+                currentCount =0;
+            }
+            trackStack.Push(new TemperatureOnDate(currentCount, temperatures[i]));
+
+            counts[i] = currentCount;
+
         }
-
-        left.Push(i);
-    }
-
-    int [] counts = new int[temperatures.Length];
-
-    //query them
-    for (int j=0; j < temperatures.Length; j ++){
-        int cur = temperatures[j];
-
-        while (left.Count > 0 && cur < temperatures[left.Peek()]){
-            right.Push(left.Pop());
-        }
-
-        while (right.Count > 0 && cur >= temperatures[right.Peek()]){
-            left.Push(right.Pop());
-        }
-
-        left.Pop();
-
-        bool indexFound =  false;
-        int searchIndex = j;
-
-        while(!indexFound && searchIndex < temperatures.Length){
-            searchIndex++;
-            indexFound =  right.Contains(searchIndex);
-        }
-
-        counts[j] = indexFound ? searchIndex - j: 0;
-    }
-
-    return counts;
+        return counts;
     }
 }
