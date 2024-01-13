c89a3518e1d7504dd448e5fc7bf5a9ca8c31ba31
Wirt4 <wirt.salthouse@gmail.com>
2024-01-12 19:05:06 -0800

    stack-based solution passes tests

--- LeetCodeSolutions/DailyTemperatures/Temperatures.cs
+++ LeetCodeSolutions/DailyTemperatures/Temperatures.cs
@@ -1,19 +1,50 @@
+using System.Collections.Concurrent;
+using System.Reflection.Metadata.Ecma335;
+
 namespace LeetCodeSolutions;
 public class Temperatures{
     public int [] DailyTemperatures(int[] temperatures){
+    Stack <int> left = new Stack<int>();
+    Stack <int> right = new Stack<int>();
+    //build stacks 
+    for (int i=0; i< temperatures.Length; i++){
+        int cur = temperatures[i];
+
+        while (left.Count > 0 && cur < temperatures[left.Peek()]){
+            right.Push(left.Pop());
+        }
+
+        while (right.Count > 0 && cur >= temperatures[right.Peek()] ){
+            left.Push(right.Pop());
+        }
+
+        left.Push(i);
+    }
+
     int [] counts = new int[temperatures.Length];
 
-    for (int i=0; i< temperatures.Length; i++){
-        int numDays=0;
-        for (int j = i+1; j < temperatures.Length; j++){
-            if (temperatures[j] > temperatures[i]){
-                numDays = j-i;
-                break;
-            }
-        }
-        counts[i] = numDays;
+    //query them
+    for (int j=0; j < temperatures.Length; j ++){
+        int cur = temperatures[j];
+
+        while (left.Count > 0 && cur < temperatures[left.Peek()]){
+            right.Push(left.Pop());
         }
 
-        return counts;
+        while (right.Count > 0 && cur >= temperatures[right.Peek()]){
+            left.Push(right.Pop());
+        }
+
+        bool indexFound =  false;
+        int searchIndex = j;
+        while(!indexFound && searchIndex < temperatures.Length){
+            searchIndex++;
+            indexFound =  right.Contains(searchIndex);
+        }
+
+        counts[j] = indexFound ? searchIndex-j: 0;
+    }
+
+    return counts;
     }
 }
