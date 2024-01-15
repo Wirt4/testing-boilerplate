8c7608e29fedb3025bcf877a24c4f588d27b9bea
Wirt4 <wirt.salthouse@gmail.com>
2024-01-15 14:14:30 -0800

    added breaking case

--- LeetCodeTests/MaximumAverageSubarrayITests.cs
+++ LeetCodeTests/MaximumAverageSubarrayITests.cs
@@ -12,4 +12,11 @@ public class MaximumAverageSubarrayTests{
         MaximumAverageSubarrayI solution = new MaximumAverageSubarrayI();
         Assert.Equal( 12.75000, solution.FindMaxAverage([1,12,-5,-6,50,3], 4));
     }
+
+    [Fact]
+        public void kIsOne(){
+        MaximumAverageSubarrayI solution = new MaximumAverageSubarrayI();
+        Assert.Equal( 50.00000, solution.FindMaxAverage([1,12,-5,-6,50,3], 1));
+    }
+
 }
