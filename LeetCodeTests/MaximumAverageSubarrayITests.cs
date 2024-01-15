3340d2cbc2272b9f52ba21aeff9ef400acc1ed94
Wirt4 <wirt.salthouse@gmail.com>
2024-01-15 14:11:50 -0800

    add test for example one

--- LeetCodeTests/MaximumAverageSubarrayITests.cs
+++ LeetCodeTests/MaximumAverageSubarrayITests.cs
@@ -6,4 +6,10 @@ public class MaximumAverageSubarrayTests{
         MaximumAverageSubarrayI solution = new MaximumAverageSubarrayI();
         Assert.Equal( 5.00000, solution.FindMaxAverage([5], 1));
     }
+
+    [Fact]
+    public void Example1(){
+        MaximumAverageSubarrayI solution = new MaximumAverageSubarrayI();
+        Assert.Equal( 12.75000, solution.FindMaxAverage([1,12,-5,-6,50,3], 4));
+    }
 }
