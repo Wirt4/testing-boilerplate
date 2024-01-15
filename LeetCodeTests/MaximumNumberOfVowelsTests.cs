27e41fd37974a738aff956f44ec6d7629ee5b463
Wirt4 <wirt.salthouse@gmail.com>
2024-01-15 15:29:48 -0800

    add example 1 test

--- LeetCodeTests/MaximumNumberOfVowelsTests.cs
+++ LeetCodeTests/MaximumNumberOfVowelsTests.cs
@@ -3,6 +3,11 @@ using LeetCodeSolutions;
 
 public class MaximumNumberOfVowelsTests{
     [Fact]
+    public void example1(){
+        VowelWindow solution = new VowelWindow();
+        Assert.Equal(3, solution.MaxVowels("abciiidef", 3));
+    }
+    [Fact]
     public void noVowels1(){
         VowelWindow solution = new VowelWindow();
         Assert.Equal(0, solution.MaxVowels("d", 1));
