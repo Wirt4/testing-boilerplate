b941bb1867f75e43f40a9bc06dea4e567dc2d944
Wirt4 <wirt.salthouse@gmail.com>
2024-01-15 15:03:20 -0800

    OOP version

--- LeetCodeSolutions/MaxiumAverageSubarrayI/MaximumAverageSubarrayI.cs
+++ LeetCodeSolutions/MaxiumAverageSubarrayI/MaximumAverageSubarrayI.cs
@@ -1,21 +1,40 @@
+using System.Diagnostics.CodeAnalysis;
+
 namespace LeetCodeSolutions;
 
 public class MaximumAverageSubarrayI{
-
-    private double getAverageFromRange(int []nums, int startNdx, int EndNdx){
-        int sum = 0;
-        for (int i = startNdx; i < EndNdx; i++){
-            sum += nums[i];
-        }
-        return 1.0 * sum / (EndNdx - startNdx);
-    }
-     public double FindMaxAverage(int[] nums, int k) {
-        double highestAverage = getAverageFromRange(nums, 0, k);
-        for (int i = 1; i + k < nums.Length; i++){
-            double currentAverage = getAverageFromRange(nums, i, i+k);
-            if (currentAverage > highestAverage){
-                highestAverage = currentAverage;
+   
+    private class RangeFinder{
+        private int sum;
+        private int[] nums;
+        private int segmentLength;
+        public double Average => 1.0 * sum / segmentLength;
+        public RangeFinder(int []nums, int k){
+            segmentLength = k;
+            this.nums = nums;
+            for (int i = 0; i< k; i++){
+                sum += nums[i];
             }
+        }
+
+        public double windowAverage(int lastNdx){
+            sum -= nums[lastNdx - segmentLength];
+            sum += nums[lastNdx];
+            return Average;
+        }
+    }
+
+
+    
+     public double FindMaxAverage(int[] nums, int k) {
+        RangeFinder rangeFinder = new RangeFinder(nums, k);
+
+        double highestAverage = rangeFinder.Average;
+        for(int i = k; i< nums.Length; i++){
+            double windowAverage = rangeFinder.windowAverage(i);
+            if (windowAverage > highestAverage){
+                highestAverage = windowAverage;
+            } 
         }
         return highestAverage;
     }
