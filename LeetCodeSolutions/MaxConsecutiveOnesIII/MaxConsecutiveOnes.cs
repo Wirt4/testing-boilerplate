11c33ba9fc518e2fd1ff20ff6d866b2ba75554cc
Wirt4 <wirt.salthouse@gmail.com>
2024-01-17 10:20:47 -0800

    reduce duplicated work

--- LeetCodeSolutions/MaxConsecutiveOnesIII/MaxConsecutiveOnes.cs
+++ LeetCodeSolutions/MaxConsecutiveOnesIII/MaxConsecutiveOnes.cs
@@ -8,42 +8,46 @@ public class MaxConsecutiveOnes{
         private int _endNdx;
         private int[] _binaryArr;
         public Window(int[] binaryArr){
-            _startNdx = 0;
-            _endNdx = _startNdx;
-            while (_endNdx < binaryArr.Length && binaryArr[_endNdx]==1){
-                _endNdx++;
-            }
-
             _binaryArr = binaryArr;
+            _Shift(0);
         }
+        /// <summary>
+        /// measures the longest span of ones, provided up to a set amount of Zeroes may be flipped
+        /// </summary>
+        /// <param name="fippableZeroes"></param>
+        /// <returns></returns>
         public int GetAdjustedSpan(int fippableZeroes){
             int extendedNdx = _endNdx;
+
             while (fippableZeroes > 0 && extendedNdx < _binaryArr.Length ){
                 if (_binaryArr[extendedNdx] == 0){
                     fippableZeroes--;
                 }
                 extendedNdx++;
             }
+
             return extendedNdx - _startNdx;
         }
-
+/// <summary>
+/// indicates if another window shift is possible
+/// </summary>
         public bool HasRemainingOnes => _endNdx < _binaryArr.Length;
+
+        private void _Shift(int startNdx){
+            _startNdx = startNdx;
+            _endNdx = _startNdx;
+
+             while (_endNdx < _binaryArr.Length && _binaryArr[_endNdx] == 1){
+                _endNdx++;
+            }
+        }
 
         /// <summary>
         ///  shifts the window state to the right
         /// </summary>
         public void Shift(){
-
-            _startNdx = _endNdx + 1;
-            _endNdx = _startNdx;
-
-             while (_endNdx < _binaryArr.Length && _binaryArr[_endNdx]==1){
-                _endNdx++;
-            }
-
-
+            _Shift(_endNdx +1);
         }
-
 
     }
      public int LongestOnes(int[] nums, int k) {
