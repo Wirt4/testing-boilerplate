d0ca0310d16805f9c9cff8bcae106d13138a79c5
Wirt4 <wirt.salthouse@gmail.com>
2024-01-17 09:37:53 -0800

    start testing

---
+++ LeetCodeSolutions/MaxConsecutiveOnesIII/MaxConsecutiveOnes.cs
@@ -0,0 +1,11 @@
+namespace LeetCodeSolutions;
+
+public class MaxConsecutiveOnes{
+     public int LongestOnes(int[] nums, int k) {
+        if (nums[0] == 1){
+            return 1;
+        }
+        
+        return 0;
+    }
+}
---
+++ LeetCodeTests/MaxConsecutiveOnes.cs
@@ -0,0 +1,24 @@
+using LeetCodeSolutions;
+
+namespace Tests;
+
+public class MaxConsecutiveOnesIIITests{
+    private MaxConsecutiveOnes _maxOnes;
+    public MaxConsecutiveOnesIIITests(){
+        _maxOnes = new MaxConsecutiveOnes();
+    }
+    [Fact]
+    public void LengthOneKisZero(){
+        Assert.Equal(0, _maxOnes.LongestOnes([0],0));
+    }
+
+     [Fact]
+    public void LengthOneAllOnes(){
+        Assert.Equal(1, _maxOnes.LongestOnes([1],0));
+    }
+
+    [Fact]
+    public void Example1(){
+            Assert.Equal(6, _maxOnes.LongestOnes([1,1,1,0,0,0,1,1,1,1,0],2));
+    }
+}
