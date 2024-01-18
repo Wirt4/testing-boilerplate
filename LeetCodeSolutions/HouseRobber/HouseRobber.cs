a6597ba43169aac102c77439c5c7b03e60c2fd42
Wirt4 <wirt.salthouse@gmail.com>
2024-01-18 15:02:14 -0800

    fiddle with levels of abstraction

--- LeetCodeSolutions/HouseRobber/HouseRobber.cs
+++ LeetCodeSolutions/HouseRobber/HouseRobber.cs
@@ -3,29 +3,32 @@ using System.Data;
 namespace LeetCodeSolutions;
 
 public class HouseRobber{
-    private class HouseValue{
+    private class House{
         private readonly int _location;
-        private readonly int _value;
-        public HouseValue(int location, int routeValue){
+        private int _value;
+        public House(int routeValue, int location){
             _location = location;
             _value = routeValue;
-
         }
 
         public int Location => _location;
         public int Value => _value;
-        public bool IsAdjacentTo(int ndx){
-            return Math.Abs(ndx - _location) == 1;
+        public bool IsNextDoor(House house){
+            return Math.Abs(house.Location - _location) == 1;
+        }
+
+        public void AddValue(int additionalValue){
+            _value += additionalValue;
         }
     }
      
     private class SortedHouseValues{
-        private Stack<HouseValue> stack;
+        private readonly Stack<House> stack;
         public SortedHouseValues(){
-            stack = new Stack<HouseValue>();
+            stack = new Stack<House>();
         }
 
-        public void PushinSequnce(HouseValue a, HouseValue b){
+        public void PushinSequnce(House a, House b){
             if (a.Value > b.Value){
             stack.Push(b);
             stack.Push(a);
@@ -35,53 +38,52 @@ public class HouseRobber{
         stack.Push(b);
         }
 
-        public HouseValue createCurrent(int value, int index ){
-            return new HouseValue(index, value + stack.Peek().Value );
+        //can this take a House Argument as a thing?
+        public bool TopIsNextDoor(House house){
+            return stack.Peek().IsNextDoor(house);
         }
 
-        public bool TopIsAdjacent(int index){
-            return stack.Peek().IsAdjacentTo(index);
-        }
-
-        public HouseValue Pop(){
+        public House Pop(){
             return stack.Pop();
         }
 
-        public void Push(HouseValue v){
+        public void Push(House v){
             stack.Push(v);
         }
+
+        public int TopValue(){
+            return stack.Peek().Value;
+        }
+
     }
 
      public int Rob(int[] nums) {
-        if (nums.Length ==1){
+        if (nums.Length == 1){
             return nums[0];
         }
 
         int lastIndex =  nums.Length -1;
-        HouseValue last = new HouseValue(lastIndex, nums[lastIndex]);
+        House last = new House(nums[lastIndex], lastIndex);
+        int penultimateIndex = lastIndex - 1;
+        House penultimate = new House(nums[penultimateIndex], penultimateIndex);
 
-        int penultimateIndex = lastIndex -1;
-        HouseValue penultimate = new HouseValue(penultimateIndex, nums[penultimateIndex]);
         SortedHouseValues houseValues = new SortedHouseValues();
-        houseValues.PushinSequnce(last,penultimate);
+        houseValues.PushinSequnce(last, penultimate);
        
-
-        //create a sorted stack that we never have to check if it's empty
-        
-        for (int i = penultimateIndex - 1; i>= 0; i--){
-            HouseValue current;
-            if (houseValues.TopIsAdjacent(i)){
-                HouseValue temp = houseValues.Pop();
-                current = houseValues.createCurrent(nums[i], i);
+        for (int i = penultimateIndex - 1; i >= 0; i--){
+            House current = new House(nums[i], i);
+            if (houseValues.TopIsNextDoor(current)){
+                House temp = houseValues.Pop();
+                current.AddValue(houseValues.TopValue());
                 houseValues.PushinSequnce(temp, current);
                 continue;
             }
 
-            current = houseValues.createCurrent(nums[i], i);
+            current.AddValue(houseValues.TopValue());
             houseValues.Push(current);
             
         }
         
-        return houseValues.Pop().Value;
+        return houseValues.TopValue();
     }
 }
