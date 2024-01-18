102f1afa39fb892825c0d7d716f7a93b1ed3144f
Wirt4 <wirt.salthouse@gmail.com>
2024-01-18 12:50:15 -0800

    simplify unnnecessary work

--- LeetCodeSolutions/MinCostClimbingStairs/Stairs.cs
+++ LeetCodeSolutions/MinCostClimbingStairs/Stairs.cs
@@ -1,26 +1,14 @@
 namespace LeetCodeSolutions;
 public class Stairs{
      public int MinCostClimbingStairs(int[] cost) {
+
         int[] minCostFromPosition = new int[cost.Length];
-        for (int i = cost.Length -1; i >=0; i--){
-            if (i >= cost.Length -2){
-                minCostFromPosition[i] = cost[i];
-                continue;
-            }
-            
-            if (minCostFromPosition[i+1] < minCostFromPosition[i+2]){
-                minCostFromPosition[i] = cost[i] + minCostFromPosition[i+1];
-                continue;
-            }
+        cost.CopyTo(minCostFromPosition, 0);
 
-             minCostFromPosition[i] = cost[i] + minCostFromPosition[i+2];
-
+        for (int i = cost.Length - 3; i >= 0; i--){
+            minCostFromPosition[i] += minCostFromPosition[i+1] > minCostFromPosition[i+2] ? minCostFromPosition[i+2]: minCostFromPosition[i+1];
         }
 
-        if (minCostFromPosition[0] < minCostFromPosition[1]){
-            return minCostFromPosition[0];
-        }
-
-        return minCostFromPosition[1];
+        return minCostFromPosition[0] < minCostFromPosition[i] ? minCostFromPosition[0]: minCostFromPosition[1];
     }
 }
