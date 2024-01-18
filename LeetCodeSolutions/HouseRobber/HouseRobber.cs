a97977f61bc38ee5070bf756415572e0972c4cf6
Wirt4 <wirt.salthouse@gmail.com>
2024-01-18 13:01:08 -0800

    first test

---
+++ LeetCodeSolutions/HouseRobber/HouseRobber.cs
@@ -0,0 +1,7 @@
+namespace LeetCodeSolutions;
+
+public class HouseRobber{
+     public int Rob(int[] nums) {
+        return -1;
+    }
+}
---
+++ LeetCodeTests/HouseRobberTests.cs
@@ -0,0 +1,14 @@
+using LeetCodeSolutions;
+
+namespace Tests;
+public class HouseRobberTests{
+    private HouseRobber _solution;
+    public HouseRobberTests(){
+        _solution = new HouseRobber();
+    }
+
+    [Fact]
+    public void MinimumLength(){
+        Assert.Equal(4, _solution.Rob([4]));
+    }
+}
