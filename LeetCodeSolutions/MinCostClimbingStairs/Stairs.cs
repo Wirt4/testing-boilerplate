a51e992b7ed021258d7c405c73fc533245f1d080
Wirt4 <wirt.salthouse@gmail.com>
2024-01-18 12:54:13 -0800

    spacing

--- LeetCodeSolutions/MinCostClimbingStairs/Stairs.cs
+++ LeetCodeSolutions/MinCostClimbingStairs/Stairs.cs
@@ -1,10 +1,10 @@
 namespace LeetCodeSolutions;
 public class Stairs{
     private int minFromIndex(int ndx, ref int[] positionalCost){
-        if (positionalCost[ndx] < positionalCost[ndx+1]){
+        if (positionalCost[ndx] < positionalCost[ndx + 1]){
             return positionalCost[ndx];
         }
-        return positionalCost[ndx +1];
+        return positionalCost[ndx + 1];
     }
      public int MinCostClimbingStairs(int[] cost) {
 
@@ -12,7 +12,7 @@ public class Stairs{
         cost.CopyTo(minCostFromPosition, 0);
 
         for (int i = cost.Length - 3; i >= 0; i--){
-            minCostFromPosition[i] += minFromIndex(i+1, ref minCostFromPosition);
+            minCostFromPosition[i] += minFromIndex(i + 1, ref minCostFromPosition);
         }
 
         return minFromIndex(0, ref minCostFromPosition);
