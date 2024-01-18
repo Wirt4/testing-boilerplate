ebe32119975f0761581ac8757b9399980a3a664b
Wirt4 <wirt.salthouse@gmail.com>
2024-01-18 14:23:30 -0800

    DRY some functionality

--- LeetCodeSolutions/HouseRobber/HouseRobber.cs
+++ LeetCodeSolutions/HouseRobber/HouseRobber.cs
@@ -18,25 +18,29 @@ public class HouseRobber{
             return Math.Abs(ndx - _location) == 1;
         }
     }
+     
+     private void PushinSequnce(HouseValue a, HouseValue b, ref Stack<HouseValue> stack){
+        if (a.Value > b.Value){
+            stack.Push(b);
+            stack.Push(a);
+            return;
+        }
+        stack.Push(a);
+        stack.Push(b);
+     }
      public int Rob(int[] nums) {
         if (nums.Length ==1){
             return nums[0];
         }
 
-        Stack <HouseValue> highestRoutes = new Stack<HouseValue>();
         int lastIndex =  nums.Length -1;
         HouseValue last = new HouseValue(lastIndex, nums[lastIndex]);
 
         int penultimateIndex = lastIndex -1;
         HouseValue penultimate = new HouseValue(penultimateIndex, nums[penultimateIndex]);
-
-        if (last.Value > penultimate.Value){
-            highestRoutes.Push(penultimate);
-            highestRoutes.Push(last);
-        }else{
-            highestRoutes.Push(last);
-            highestRoutes.Push(penultimate);
-        }
+        Stack <HouseValue> highestRoutes = new Stack<HouseValue>();
+        PushinSequnce(last,penultimate, ref highestRoutes);
+       
 
         //create a sorted stack that we never have to check if it's empty
         
@@ -46,15 +50,7 @@ public class HouseRobber{
                 HouseValue temp = highestRoutes.Pop();
                 int lootValue =  highestRoutes.Peek().Value + nums[i];
                 current = new HouseValue(i, lootValue);
-                
-                if (current.Value > temp.Value){
-                    highestRoutes.Push(temp);
-                    highestRoutes.Push(current);
-                }else{
-                    highestRoutes.Push(current);
-                    highestRoutes.Push(temp);
-                }
-
+                PushinSequnce(temp, current, ref highestRoutes);
             }else{
                 int lootValue =  highestRoutes.Peek().Value + nums[i];
                 current = new HouseValue(i, lootValue);
