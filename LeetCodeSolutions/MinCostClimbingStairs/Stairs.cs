b705e5390bd76c4055499f87a803bb3df1cd5f5f
Wirt4 <wirt.salthouse@gmail.com>
2024-01-18 12:53:51 -0800

    remove duplicated work

--- LeetCodeSolutions/MinCostClimbingStairs/Stairs.cs
+++ LeetCodeSolutions/MinCostClimbingStairs/Stairs.cs
@@ -1,14 +1,20 @@
 namespace LeetCodeSolutions;
 public class Stairs{
+    private int minFromIndex(int ndx, ref int[] positionalCost){
+        if (positionalCost[ndx] < positionalCost[ndx+1]){
+            return positionalCost[ndx];
+        }
+        return positionalCost[ndx +1];
+    }
      public int MinCostClimbingStairs(int[] cost) {
 
         int[] minCostFromPosition = new int[cost.Length];
         cost.CopyTo(minCostFromPosition, 0);
 
         for (int i = cost.Length - 3; i >= 0; i--){
-            minCostFromPosition[i] += minCostFromPosition[i+1] > minCostFromPosition[i+2] ? minCostFromPosition[i+2]: minCostFromPosition[i+1];
+            minCostFromPosition[i] += minFromIndex(i+1, ref minCostFromPosition);
         }
 
-        return minCostFromPosition[0] < minCostFromPosition[i] ? minCostFromPosition[0]: minCostFromPosition[1];
+        return minFromIndex(0, ref minCostFromPosition);
     }
 }
