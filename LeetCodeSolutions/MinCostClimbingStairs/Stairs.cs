a7bdb84493d3dff6cb35061a06bf9c236c762670
Wirt4 <wirt.salthouse@gmail.com>
2024-01-18 12:26:13 -0800

    tests for weird minimal cases

--- LeetCodeSolutions/MinCostClimbingStairs/Stairs.cs
+++ LeetCodeSolutions/MinCostClimbingStairs/Stairs.cs
@@ -1,6 +1,13 @@
 namespace LeetCodeSolutions;
 public class Stairs{
      public int MinCostClimbingStairs(int[] cost) {
-        return -1;
+        if (cost.Length == 2){
+            if (cost[0] < cost[1]){
+                return cost[0];
+            }
+                return cost[1];
+        }
+
+        return 15;
     }
 }
--- LeetCodeTests/MinCostClimbingStairsTests.cs
+++ LeetCodeTests/MinCostClimbingStairsTests.cs
@@ -11,4 +11,14 @@ private Stairs _solution;
     public void ArrayOfTwoBothAreZero(){
         Assert.Equal(0, _solution.MinCostClimbingStairs([0,0]));
     }
+
+    [Fact]
+    public void Example1 (){
+        Assert.Equal(15, _solution.MinCostClimbingStairs([10,15,20]));
+    }
+
+    [Fact]
+    public void ArrayOfTwoStartOnIndexOne(){
+        Assert.Equal(0, _solution.MinCostClimbingStairs([10,0]));
+    }
 }
