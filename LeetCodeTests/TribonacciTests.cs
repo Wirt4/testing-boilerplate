9264b62d5867e5924c7a5b36a912b1524072aafd
Wirt4 <wirt.salthouse@gmail.com>
2024-01-20 15:44:38 -0800

    move test class declaration to the constructor

--- LeetCodeTests/TribonacciTests.cs
+++ LeetCodeTests/TribonacciTests.cs
@@ -1,35 +1,34 @@
 namespace Tests;
 using LeetCodeSolutions;
 public class TribonacciTests{
+    private TribonacciNumber _solution;
+    public TribonacciTests(){
+        _solution = new TribonacciNumber();
+    }
     [Fact]
     public void TOfZero(){
-        TribonacciNumber solution = new TribonacciNumber();
-        Assert.Equal(0, solution.Tribonacci(0));
+        Assert.Equal(0, _solution.Tribonacci(0));
     }
 
     [Fact]
     public void TOfOne(){
-        TribonacciNumber solution = new TribonacciNumber();
-        Assert.Equal(1, solution.Tribonacci(1));
+        Assert.Equal(1, _solution.Tribonacci(1));
     }
 
     [Fact]
     public void TOf3(){
-        TribonacciNumber solution = new TribonacciNumber();
-        Assert.Equal(2, solution.Tribonacci(3));
+        Assert.Equal(2, _solution.Tribonacci(3));
     }
 
     [Fact]
     public void TOf4(){
-        TribonacciNumber solution = new TribonacciNumber();
-        Assert.Equal(4, solution.Tribonacci(4));
+        Assert.Equal(4, _solution.Tribonacci(4));
     }
 
 
     [Fact]
     public void TOf25(){
-        TribonacciNumber solution = new TribonacciNumber();
-        Assert.Equal(1389537, solution.Tribonacci(25));
+        Assert.Equal(1389537, _solution.Tribonacci(25));
     }
     
 }
