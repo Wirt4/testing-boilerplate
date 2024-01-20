224f1bc2adc8f4665aa380c032d6cc38f30d5465
Wirt4 <wirt.salthouse@gmail.com>
2024-01-20 15:43:09 -0800

    re-factor to use functional encapsulation

--- LeetCodeSolutions/Tribonacci/TribonacciNumber.cs
+++ LeetCodeSolutions/Tribonacci/TribonacciNumber.cs
@@ -1,18 +1,22 @@
 namespace LeetCodeSolutions;
 
 public class TribonacciNumber{
+    private int[] TribonacciArray(int maxValue = 37){
+        int[] tribArr = new int[maxValue + 1];
+        for (int i =1; i<= 2 && i<= maxValue; i++){
+            tribArr[i] = 1;
+        }
+        return tribArr;
+    }
+  
     public int Tribonacci(int n){
-        int maxN = 37;
         
-        int[] fibArr = new int[maxN+1];
-        fibArr[0] = 0;
-        fibArr[1] = 1;
-        fibArr[2] = 1;
+        int[] fibArr = TribonacciArray(n);
 
         for (int t = 0; t+3 < fibArr.Length; t++){
             fibArr[t+3] = fibArr[t] + fibArr[t+1] + fibArr[t+2];
         }
-        
+
         return fibArr[n];
 
     }
