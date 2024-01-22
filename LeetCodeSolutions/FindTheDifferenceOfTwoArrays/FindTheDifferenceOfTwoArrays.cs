31a183a438b0a180aaebf943c9d6187153000966
Wirt4 <wirt.salthouse@gmail.com>
2024-01-21 19:54:44 -0800

    reduce duplicated work

--- LeetCodeSolutions/FindTheDifferenceOfTwoArrays/FindTheDifferenceOfTwoArrays.cs
+++ LeetCodeSolutions/FindTheDifferenceOfTwoArrays/FindTheDifferenceOfTwoArrays.cs
@@ -2,24 +2,26 @@ using System.Collections;
 
 namespace LeetCodeSolutions;
 public class FindTheDifferenceOfTwoArraysSolution {
+    private ArrayList ANotPresentInB(HashSet<int> setA, HashSet<int> setB){
+        ArrayList answer = new ArrayList();
+        foreach(int num in setA){
+            if (setB.Contains(num)) continue;
+            answer.Add(num);
+        }
+        return answer;
+    }
+
+    private int[] Coerce(ArrayList list){
+        return (int [])list.ToArray(typeof(int));
+    }
     public IList<IList<int>> FindDifference(int[] nums1, int[] nums2) {
         HashSet<int> setOne = new HashSet<int>(nums1);
         HashSet<int> setTwo = new HashSet<int>(nums2);
 
-        ArrayList answer1 = new ArrayList();
-        foreach(int num in setOne){
-            if (setTwo.Contains(num)) continue;
-            answer1.Add(num);
-        }
-
-        ArrayList answer2 = new ArrayList();
-
-        foreach(int num in setTwo){
-            if (setOne.Contains(num)) continue;
-            answer2.Add(num);
-        }
-
-        IList<IList<int>> foo = [(int [])answer1.ToArray(typeof(int)),(int [])answer2.ToArray(typeof(int))];
+        ArrayList answer1 = ANotPresentInB(setOne, setTwo);
+        ArrayList answer2 = ANotPresentInB(setTwo, setOne);
+    
+        IList<IList<int>> foo = [Coerce(answer1), Coerce(answer2)];
         return foo;
     }
 }
