69363a8fa7a3b69ab58034ec875104d873eceee4
Wirt4 <wirt.salthouse@gmail.com>
2024-01-21 18:46:46 -0800

    put both in same loop

--- LeetCodeSolutions/FindPivotIndex/FindPivotIndex.cs
+++ LeetCodeSolutions/FindPivotIndex/FindPivotIndex.cs
@@ -2,26 +2,22 @@ namespace LeetCodeSolutions;
 public class PivotSolution{
      public int PivotIndex(int[] nums) {
 
-        if (nums.Length==1){
+        if (nums.Length == 1){
             return 0;
         }
 
         int[] leftHandSums = new int[nums.Length];
         int leftHandSum = nums[0];
+        int[] rightHandSums = new int[nums.Length];
+        int rightHandSum = nums[nums.Length -1];
 
         for (int i=1; i<nums.Length; i++){
             leftHandSums[i] = leftHandSum;
             leftHandSum += nums[i];
-        }
 
-        int[] rightHandSums = new int[nums.Length];
-        int rightHandSum = nums[nums.Length -1];
-
-        for (int j = nums.Length - 2; j >=0; j--){
+            int j = nums.Length- 1- i;
             rightHandSums[j] = rightHandSum;
             rightHandSum += nums[j];
-
-
         }
 
         for (int k =0; k < nums.Length; k++){
@@ -29,7 +25,7 @@ public class PivotSolution{
             if (leftHandSums[k] == rightHandSums[k]){
                 return k;
             }
-            
+
         }
 
         return -1;
