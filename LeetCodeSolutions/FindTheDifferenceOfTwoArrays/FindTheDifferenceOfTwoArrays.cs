752d05cdff20959da9648fd41fab7f94a310b2bd
Wirt4 <wirt.salthouse@gmail.com>
2024-01-21 19:49:10 -0800

    passets tests

--- LeetCodeSolutions/FindTheDifferenceOfTwoArrays/FindTheDifferenceOfTwoArrays.cs
+++ LeetCodeSolutions/FindTheDifferenceOfTwoArrays/FindTheDifferenceOfTwoArrays.cs
@@ -1,7 +1,25 @@
+using System.Collections;
+
 namespace LeetCodeSolutions;
 public class FindTheDifferenceOfTwoArraysSolution {
     public IList<IList<int>> FindDifference(int[] nums1, int[] nums2) {
-        IList<IList<int>> foo = [[1,3],[4,6]];
+        HashSet<int> setOne = new HashSet<int>(nums1);
+        HashSet<int> setTwo = new HashSet<int>(nums2);
+
+        ArrayList answer1 = new ArrayList();
+        foreach(int num in setOne){
+            if (setTwo.Contains(num)) continue;
+            answer1.Add(num);
+        }
+
+        ArrayList answer2 = new ArrayList();
+
+        foreach(int num in setTwo){
+            if (setOne.Contains(num)) continue;
+            answer2.Add(num);
+        }
+
+        IList<IList<int>> foo = [(int [])answer1.ToArray(typeof(int)),(int [])answer2.ToArray(typeof(int))];
         return foo;
     }
 }
