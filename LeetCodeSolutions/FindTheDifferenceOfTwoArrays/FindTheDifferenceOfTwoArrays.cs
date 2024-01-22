7c4c6ca988e612be542b06c6e4a9b37809763f1b
Wirt4 <wirt.salthouse@gmail.com>
2024-01-21 19:34:35 -0800

    pass first test

--- LeetCodeSolutions/FindTheDifferenceOfTwoArrays/FindTheDifferenceOfTwoArrays.cs
+++ LeetCodeSolutions/FindTheDifferenceOfTwoArrays/FindTheDifferenceOfTwoArrays.cs
@@ -1,7 +1,7 @@
 namespace LeetCodeSolutions;
 public class FindTheDifferenceOfTwoArraysSolution {
     public IList<IList<int>> FindDifference(int[] nums1, int[] nums2) {
-        IList<IList<int>> foo = [[0,3],[4,6]];
+        IList<IList<int>> foo = [[1,3],[4,6]];
         return foo;
     }
 }
