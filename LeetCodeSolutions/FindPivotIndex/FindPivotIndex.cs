ba76b8d12591649ce59bf5683d44b76cb35ce57a
Wirt4 <wirt.salthouse@gmail.com>
2024-01-21 18:42:47 -0800

    passes example2

--- LeetCodeSolutions/FindPivotIndex/FindPivotIndex.cs
+++ LeetCodeSolutions/FindPivotIndex/FindPivotIndex.cs
@@ -2,6 +2,28 @@ namespace LeetCodeSolutions;
 public class PivotSolution{
      public int PivotIndex(int[] nums) {
         if (nums.Length==1)return 0;
-        return 3;
+        int[] leftHandSums = new int[nums.Length];
+        int leftHandSum = nums[0];
+
+        for (int i=1; i<nums.Length; i++){
+            leftHandSums[i] = leftHandSum;
+            leftHandSum += nums[i];
+        }
+
+        int[] rightHandSums = new int[nums.Length];
+        int rightHandSum = nums[nums.Length -1];
+
+        for (int j = nums.Length - 2; j >=0; j--){
+            rightHandSums[j] = rightHandSum;
+            rightHandSum += nums[j];
+
+
+        }
+
+        for (int k =0; k < nums.Length; k++){
+            if (leftHandSums[k] == rightHandSums[k]) return k;
+        }
+
+        return -1;
     }
 }
--- LeetCodeTests/FindPivotIndex-tests.cs
+++ LeetCodeTests/FindPivotIndex-tests.cs
@@ -14,4 +14,9 @@ public class FindPivotIndexTests{
     public void Example1(){
         Assert.Equal(3, solution.PivotIndex([1,7,3,6,5,6]));
     }
+
+    [Fact]
+    public void Example3(){
+        Assert.Equal(-1, solution.PivotIndex([1,2,3]));
+    }
 }
