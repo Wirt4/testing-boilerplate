1f52d5c9186f4303394040916ad91358c3be53c1
Wirt4 <wirt.salthouse@gmail.com>
2024-01-22 22:33:42 -0800

    add brackets to returns

--- LeetCodeSolutions/DetermineIfTwoStringsAreClose/DetermineIfTwoStringsAreClose.cs
+++ LeetCodeSolutions/DetermineIfTwoStringsAreClose/DetermineIfTwoStringsAreClose.cs
@@ -5,7 +5,7 @@ namespace LeetCodeSolutions;
 public class CloseStringsSolution{
     private Dictionary<char, int> CreateFrequencyTable(string word){
         Dictionary<char, int> table =[];
-        
+
         foreach(char ch in word){
 
             if (table.TryGetValue(ch, out int value)){
@@ -36,21 +36,34 @@ public class CloseStringsSolution{
     }
    
     public bool CloseStrings(string word1, string word2) {
-        if (word1 == word2) return true;
-        if (word1.Length != word2.Length) return false;
+        if (word1 == word2) {
+            return true;
+        }
+
+        if (word1.Length != word2.Length) {
+            return false;
+        }
 
         Dictionary<char, int> table1 = CreateFrequencyTable(word1);
         Dictionary<char, int> table2 = CreateFrequencyTable(word2);
 
-        if (table1.Count != table2.Count) return false;
+        if (table1.Count != table2.Count){
+            return false;
+        }
 
         bool matchingCounts = true;
+
         foreach(KeyValuePair<char, int> pair in table1){
-            if (!table2.ContainsKey(pair.Key)) return false;
+            if (!table2.ContainsKey(pair.Key)){
+                return false;
+            
+            }
             matchingCounts = matchingCounts && table2[pair.Key] == pair.Value;
         }
 
-        if (matchingCounts) return true;
+        if (matchingCounts){
+            return true;
+        }
 
         Dictionary<int, int> charCounts1 = FrequencyOfCounts(table1);
         Dictionary<int, int> charCounts2 = FrequencyOfCounts(table2);
@@ -58,8 +71,13 @@ public class CloseStringsSolution{
         if (charCounts1.Count != charCounts2.Count) return false;
 
         foreach (KeyValuePair<int, int> pair in charCounts1){
-            if (!charCounts2.TryGetValue(pair.Key, out int value)) return false;
-            if (value != pair.Value) return false;
+            if (!charCounts2.TryGetValue(pair.Key, out int value)){
+                return false;
+            }
+            
+            if (value != pair.Value){
+                return false;
+            }
         }
 
 
