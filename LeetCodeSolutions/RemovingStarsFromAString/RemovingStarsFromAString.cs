0199fb9e007eb55a912f0414bb6725b781a76ff4
Wirt4 <wirt.salthouse@gmail.com>
2024-01-26 14:37:48 -0800

    refactor to use enhanced for loop

--- LeetCodeSolutions/RemovingStarsFromAString/RemovingStarsFromAString.cs
+++ LeetCodeSolutions/RemovingStarsFromAString/RemovingStarsFromAString.cs
@@ -1,18 +1,37 @@
 namespace LeetCodeSolutions;
 
 public class RemovingStarsFromAStringSolution{
+
+    private class StringWrapper{
+        private string _s;
+        public StringWrapper(string s){
+            _s = s;
+        }
+
+        public bool IsValidIndex(int index){
+            return index < _s.Length;
+        }
+
+        public bool StarAtLocation(int index){
+            if (IsValidIndex(index)){
+                return _s[index] == '*';
+            }
+
+            return false;
+        }
+    }
      public string RemoveStars(string s) {
         // set up a stack of chars
         Stack<char> nonStars = new();
-        int ndx = 0;
-        while (ndx < s.Length){
-            while (ndx < s.Length && s[ndx] == '*'){
+
+        foreach (char ch in s){
+            if (ch == '*'){
                 nonStars.Pop();
-                ndx ++;
+                continue;
             }
-            if (ndx < s.Length) nonStars.Push(s[ndx]);
-            ndx++;
+            nonStars.Push(ch);
         }
+
         char [] chars = nonStars.ToArray();
         Array.Reverse(chars);
         string starsRemoved = new(chars);
