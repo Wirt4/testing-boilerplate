30fc9aa060b242c3fe068233e81aede10122cc48
Wirt4 <wirt.salthouse@gmail.com>
2024-01-26 12:55:39 -0800

    add failing test

--- LeetCodeTests/EqualRowAndColumnPairs-tests.cs
+++ LeetCodeTests/EqualRowAndColumnPairs-tests.cs
@@ -3,10 +3,18 @@ using LeetCodeSolutions;
 namespace Tests;
 
 public class EqualRowAndColumnPairsTests{
+    private EqualRowAndColumnPairsSolution solution;
+    public EqualRowAndColumnPairsTests(){
+        solution = new();
+    }
     [Fact]
     public void Example1 (){
-        EqualRowAndColumnPairsSolution solution = new();
         Assert.Equal(1, solution.EqualPairs([[3,2,1],[1,7,6],[2,7,7]]));
 
+    }
+
+    [Fact]
+    public void Example2(){
+        Assert.Equal(3, solution.EqualPairs([[3,1,2,2],[1,4,4,5],[2,4,2,2],[2,4,2,2]]));
     }
 }
