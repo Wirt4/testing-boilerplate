419b2fafc05b8b82f34ec54a551013d63ab41e00
Wirt4 <wirt.salthouse@gmail.com>
2024-01-26 14:30:05 -0800

    pass tests

--- LeetCodeSolutions/RemovingStarsFromAString/RemovingStarsFromAString.cs
+++ LeetCodeSolutions/RemovingStarsFromAString/RemovingStarsFromAString.cs
@@ -2,6 +2,20 @@ namespace LeetCodeSolutions;
 
 public class RemovingStarsFromAStringSolution{
      public string RemoveStars(string s) {
-        return "lecoe";
+        // set up a stack of chars
+        Stack<char> nonStars = new();
+        int ndx = 0;
+        while (ndx < s.Length){
+            while (ndx < s.Length && s[ndx] == '*'){
+                nonStars.Pop();
+                ndx ++;
+            }
+            if (ndx < s.Length) nonStars.Push(s[ndx]);
+            ndx++;
+        }
+        char [] chars = nonStars.ToArray();
+        Array.Reverse(chars);
+        string starsRemoved = new(chars);
+        return starsRemoved;
     }
 }
