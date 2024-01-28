fbdca1caad5ad3b926d015cf337ac8729000f119
Wirt4 <wirt.salthouse@gmail.com>
2024-01-28 15:20:34 -0800

    back to case where have to adjust the test

--- LeetCodeTests/NumberOfRecentCallsTests.cs
+++ LeetCodeTests/NumberOfRecentCallsTests.cs
@@ -9,5 +9,7 @@ using LeetCodeSolutions;
   public void Example1(){
     Assert.Equal(1, _solution.Ping(1));
     Assert.Equal(2, _solution.Ping(100));
+    Assert.Equal(3, _solution.Ping(3001));
+    Assert.Equal(3, _solution.Ping(3002));
   }
 }
