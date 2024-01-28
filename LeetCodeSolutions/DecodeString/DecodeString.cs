0d23d76fe23f812e1bf0e1225cd87f74edd7f97a
Wirt4 <wirt.salthouse@gmail.com>
2024-01-27 23:32:21 -0800

    more minimalist stuff

--- LeetCodeSolutions/DecodeString/DecodeString.cs
+++ LeetCodeSolutions/DecodeString/DecodeString.cs
@@ -1,12 +1,28 @@
+using System.Text;
 using System.Text.RegularExpressions;
 
 namespace LeetCodeSolutions;
 public class DecodeStringSolution {
     public string DecodeString(string s) {
         string pattern = "^[a-zA]*$";
-        if (Regex.IsMatch(s,pattern)){
-            return s;
-        }
-        return "tttt";
+
+        if (Regex.IsMatch(s,pattern)) return s;
+        
+        int l = 0;
+
+        while(s[l] != '[') l++;
+
+        int k = int.Parse(s[..l]);
+        int r = l;
+
+        while (s[r] != ']') r++;
+
+        StringBuilder sb = new();
+        l++;
+        string segment = s[l..r];
+
+        for (int i=0; i<k; i++) sb.Append(segment);
+        
+        return sb.ToString();
     }
 }
