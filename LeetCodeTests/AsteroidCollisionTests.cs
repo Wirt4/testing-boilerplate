b289f8eec48f743e987797b68dbc9b60e8684b0a
Wirt4 <wirt.salthouse@gmail.com>
2024-01-27 19:14:51 -0800

    initiate with README

---
+++ LeetCodeTests/AsteroidCollisionTests.cs
@@ -0,0 +1,11 @@
+namespace Tests;
+using LeetCodeSolutions;
+ public class AsteroidCollisionTests {
+  private AsteroidCollisionSolution _solution;
+  public AsteroidCollisionTests (){
+      _solution = new();
+  }
+  [Fact]
+  public void Test1(){
+  }
+}
