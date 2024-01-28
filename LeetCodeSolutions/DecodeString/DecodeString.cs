bb4a7914c6303dfd4d29d8e467cf76ce77630c54
Wirt4 <wirt.salthouse@gmail.com>
2024-01-28 15:02:07 -0800

    reduce functions to 20 lines or less

--- LeetCodeSolutions/DecodeString/DecodeString.cs
+++ LeetCodeSolutions/DecodeString/DecodeString.cs
@@ -8,71 +8,153 @@ public class DecodeStringSolution {
                 return true;
                 }
         }
-        
+
         return false;
     }
 
-    public string DecodeString(string inputString) {
-        if (!EditingRequired(inputString)) return inputString;
-
-        StringBuilder returnStringBuilder = new();
-
-        string suffix = "";
-
-        if(inputString[^1] != ']'){
-            int endBracketIndex = inputString.Length - 1;
-            while (inputString[endBracketIndex-1] != ']'){
-                endBracketIndex--;
-            }
-
-            suffix = inputString.Substring(endBracketIndex);
-            inputString = inputString[..endBracketIndex];
+    private class StringIndexer{
+        public int leftIndex;
+        public int rightIndex;
+        private string encodedString;
+        public StringIndexer(string inputString){
+            encodedString = inputString;
+            leftIndex = 0;
+            rightIndex = 0;
         }
 
-        int leftIndex = 0;
+        public void FindEncasingBrackets(){
+            int levelsOfParenthesis = 1;
 
-        while (leftIndex < inputString.Length){
-            int rightIndex = leftIndex;
-
-            while (!Char.IsDigit(inputString[rightIndex])){
-                returnStringBuilder.Append(inputString[rightIndex]);
+             do{
                 rightIndex++;
-            }
-            
-            while(inputString[leftIndex] != '['){
-                leftIndex++;
+
+                if (encodedString[rightIndex] == '['){
+                    levelsOfParenthesis++;
+                }
+
+                if(encodedString[rightIndex] == ']'){
+                    levelsOfParenthesis--;
+                }
+
+            }while(levelsOfParenthesis > 0); 
+        }
+
+        public void AdvanceRightIndexToLeftIndexPosition(){
+            rightIndex =  leftIndex;
+        }
+
+        public void AdvanceLeftIndexToOpeningBracket(){
+            while(encodedString[leftIndex] != '['){
+                IncrementLeftIndex();
             };
-
-            int k = int.Parse(inputString[rightIndex..leftIndex]);
-            rightIndex = leftIndex;
-            int parens = 1;
-
-            do{
-                rightIndex++;
-
-                if (inputString[rightIndex] == '['){
-                    parens++;
-                }
-
-                if(inputString[rightIndex] == ']'){
-                    parens--;
-                }
-
-            }while(parens > 0); 
-
-            leftIndex++;
-            string segment = inputString[leftIndex..rightIndex];
-            string decoded = DecodeString(segment);
-
-            for (int i=0; i<k; i++) {
-                returnStringBuilder.Append(decoded);
-            }
-
-            leftIndex = rightIndex + 1;
         }
 
-        returnStringBuilder.Append(suffix);
+        public void IncrementLeftIndex(){
+            leftIndex++;
+        }
 
-        return returnStringBuilder.ToString();
+        public bool RightIndexPointsToDigit(){
+           return  Char.IsDigit(encodedString[rightIndex]);
+        }
+
+        public string RetrieveSegment(){
+            AdvanceRightIndexToLeftIndexPosition();
+            FindEncasingBrackets();
+            IncrementLeftIndex();
+            return encodedString[leftIndex..rightIndex];
+        }
+
+        public int ParseMultiplier(){
+            AdvanceLeftIndexToOpeningBracket();
+            return int.Parse(encodedString[rightIndex..leftIndex]);
+        }
+
+        public void SetIndecesForNextIteration(){
+            leftIndex = rightIndex;
+            IncrementLeftIndex();
+        }
+
+        public bool HasRemainingChars(){
+            return leftIndex < encodedString.Lengthl
+        }
+    }
+
+    private class StringDecoder{
+        private StringBuilder builder;
+        private string suffix;
+        private bool hasSuffix;
+        public StringDecoder(){
+            builder = new();
+            hasSuffix = false;
+            suffix = "";
+        }
+
+        public string TrimSuffix(string inputString){
+            if (inputString[^1] == ']'){
+                return inputString;
+            }
+
+            int closingBracketIndex = FindClosingBracketIndex(inputString);
+            hasSuffix = true;
+            suffix = inputString[closingBracketIndex..];
+            return inputString[..closingBracketIndex];
+        }
+
+        private void AppendCharacer(ref string inputString, ref StringIndexer indeces){
+            builder.Append(inputString[indeces.rightIndex]);
+            indeces.rightIndex++;
+        }
+
+        public void AppendLeadingChars(ref string inputString, ref StringIndexer indeces){
+            while (!indeces.RightIndexPointsToDigit()){
+                AppendCharacer(ref inputString, ref indeces);
+            }
+        }
+
+        public void AppendMultipleStrings(string segment, int numberOfTimes){
+            for (int i=0; i<numberOfTimes; i++) {
+                builder.Append(segment);
+            }
+        }
+        public string ToString(){
+            if (hasSuffix){
+                 builder.Append(suffix);
+            }
+
+            return builder.ToString();
+        }
+
+        private int FindClosingBracketIndex(string inputString){
+         int closingBracketIndex = inputString.Length - 1;
+
+        while (inputString[closingBracketIndex-1] != ']'){
+                closingBracketIndex--;
+        }
+
+        return closingBracketIndex;
+    }
+    }
+
+
+    public string DecodeString(string inputString) {
+        if (!EditingRequired(inputString)){
+            return inputString;
+        }
+    
+        StringDecoder decoder = new();
+        inputString = decoder.TrimSuffix(inputString);
+        StringIndexer parser = new(inputString);
+
+        while (parser.HasRemainingChars()){
+            parser.AdvanceRightIndexToLeftIndexPosition();
+            decoder.AppendLeadingChars(ref inputString, ref parser);
+            int k = parser.ParseMultiplier();
+            string segment = parser.RetrieveSegment();
+            string decoded = DecodeString(segment);
+            decoder.AppendMultipleStrings(decoded, k);
+            parser.SetIndecesForNextIteration();
+        }
+
+        return decoder.ToString();
     }
 }
