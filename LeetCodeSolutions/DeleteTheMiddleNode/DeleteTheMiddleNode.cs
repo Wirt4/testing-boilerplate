a50c451ef76f8e75878c81d5aeb625a40b14dd6c
Wirt4 <wirt.salthouse@gmail.com>
2024-01-30 14:49:07 -0800

    add class definition provided by LeetCode

---
+++ LeetCodeSolutions/DeleteTheMiddleNode/DeleteTheMiddleNode.cs
@@ -0,0 +1,16 @@
+namespace LeetCodeSolutions;
+/**
+ * LeetCode'sDefinition for singly-linked list.
+ */
+public class ListNode
+{
+    public int val;
+    public ListNode next;
+    public ListNode(int val = 0, ListNode next = null)
+    {
+        this.val = val;
+        this.next = next;
+    }
+}
+
+public class DeleteTheMiddleNodeSolution { }
---
+++ LeetCodeTests/DeleteTheMiddleNodeTests.cs
@@ -0,0 +1,11 @@
+namespace Tests;
+using LeetCodeSolutions;
+ public class DeleteTheMiddleNodeTests {
+  private DeleteTheMiddleNodeSolution _solution;
+  public DeleteTheMiddleNodeTests (){
+      _solution = new();
+  }
+  [Fact]
+  public void Test1(){
+  }
+}
