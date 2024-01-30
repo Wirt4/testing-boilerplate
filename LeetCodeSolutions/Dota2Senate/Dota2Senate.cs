9d894f77a5e6f65c0a8be554c860e3557fd80bdf
Wirt4 <wirt.salthouse@gmail.com>
2024-01-29 17:52:34 -0800

    naeive solution

--- LeetCodeSolutions/Dota2Senate/Dota2Senate.cs
+++ LeetCodeSolutions/Dota2Senate/Dota2Senate.cs
@@ -1,6 +1,39 @@
 namespace LeetCodeSolutions;
 public class Dota2SenateSolution {
+    private bool EqualNumbers(string senate){
+        int DireCount = 0;
+        int RadiantCount = 0;
+        foreach(char senator in senate){
+            if (senator == 'D'){
+                DireCount++;
+                continue;
+            }
+
+            RadiantCount ++;
+        }
+        return DireCount ==  RadiantCount;
+    }
+
+    private bool DireMajority(string senate){
+        int DireCount = 0;
+        int RadiantCount = 0;
+        foreach(char senator in senate){
+            if (senator == 'D'){
+                DireCount++;
+                continue;
+            }
+
+            RadiantCount ++;
+        }
+        return DireCount >  RadiantCount;
+    }
     public string PredictPartyVictory(string senate) {
-      return senate == "D" ? "Dire" : "Radiant";
+        if (EqualNumbers(senate)){
+            return senate[0] == 'D' ? "Dire" : "Radiant";
+        }
+        if (DireMajority(senate)){
+            return "Dire";
+        }
+      return "Radiant";
     }
 }
