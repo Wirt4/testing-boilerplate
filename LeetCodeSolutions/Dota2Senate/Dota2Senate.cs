91b645dde5cc459e8c69a7d3c6b3f6461a27a4e5
Wirt4 <wirt.salthouse@gmail.com>
2024-01-29 17:35:31 -0800

    reduce lines of code

--- LeetCodeSolutions/Dota2Senate/Dota2Senate.cs
+++ LeetCodeSolutions/Dota2Senate/Dota2Senate.cs
@@ -1,7 +1,6 @@
 namespace LeetCodeSolutions;
 public class Dota2SenateSolution {
     public string PredictPartyVictory(string senate) {
-        if (senate == "D")return "Dire";
-        return "Radiant";
+      return senate == "D" ? "Dire" : "Radiant";
     }
 }
