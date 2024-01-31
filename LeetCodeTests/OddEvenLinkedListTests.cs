d002a7f98ec68cba208b77b2697fc25c497b831a
Wirt4 <wirt.salthouse@gmail.com>
2024-01-31 13:36:59 -0800

    initialise with README

---
+++ LeetCodeTests/OddEvenLinkedListTests.cs
@@ -0,0 +1,11 @@
+namespace Tests;
+using LeetCodeSolutions;
+ public class OddEvenLinkedListTests {
+  private OddEvenLinkedListSolution _solution;
+  public OddEvenLinkedListTests (){
+      _solution = new();
+  }
+  [Fact]
+  public void Test1(){
+  }
+}
