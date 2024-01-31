8257f1a6778a3cb82d5679f868e4b8b8db1fc4f2
Wirt4 <wirt.salthouse@gmail.com>
2024-01-31 13:57:08 -0800

    add test for example 2

--- LeetCodeTests/OddEvenLinkedListTests.cs
+++ LeetCodeTests/OddEvenLinkedListTests.cs
@@ -10,9 +10,20 @@ public class OddEvenLinkedListTests
   [Fact]
   public void Example1()
   {
-    ListNode input = LinkedListTesting.ListFromArray([1, 2, 3, 4, 5]);
-    ListNode expectedOutput = LinkedListTesting.ListFromArray([1, 3, 5, 2, 4]);
-    ListNode actualOutput = _solution.OddEvenList(input);
+    CompareFunctionFromArrays([1, 2, 3, 4, 5], [1, 3, 5, 2, 4]);
+  }
+
+  [Fact]
+  public void Example2()
+  {
+    CompareFunctionFromArrays([2, 1, 3, 5, 6, 4, 7], [2, 3, 6, 7, 1, 5, 4]);
+  }
+
+  private void CompareFunctionFromArrays(int[] input, int[] output)
+  {
+    ListNode inputList = LinkedListTesting.ListFromArray(input);
+    ListNode expectedOutput = LinkedListTesting.ListFromArray(output);
+    ListNode actualOutput = _solution.OddEvenList(inputList);
     LinkedListTesting.AssertListsAreEqual(expectedOutput, actualOutput);
   }
 }
