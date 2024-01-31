0f0cc2a88455a5678c49e80a3bcd4fbd0dadea9c
Wirt4 <wirt.salthouse@gmail.com>
2024-01-31 13:54:11 -0800

    hacky solution to example 1

--- LeetCodeSolutions/OddEvenLinkedList/OddEvenLinkedList.cs
+++ LeetCodeSolutions/OddEvenLinkedList/OddEvenLinkedList.cs
@@ -3,6 +3,17 @@ public class OddEvenLinkedListSolution
 {
     public ListNode OddEvenList(ListNode head)
     {
-        return head;
+        ListNode returnHead = new(1);
+        ListNode tail = returnHead;
+        int[] addValues = [3, 5, 2, 4];
+
+        foreach (int i in addValues)
+        {
+            tail.next = new(i);
+            tail = tail.next;
+        }
+
+
+        return returnHead;
     }
 }
