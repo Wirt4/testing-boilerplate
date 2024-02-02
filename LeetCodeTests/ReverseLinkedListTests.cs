75a5423d948c5fa8e6c4870cc25d8fe2580b5871
Wirt4 <wirt.salthouse@gmail.com>
2024-02-02 15:24:00 -0800

    initialize with README

---
+++ LeetCodeTests/ReverseLinkedListTests.cs
@@ -0,0 +1,11 @@
+namespace Tests;
+using LeetCodeSolutions;
+ public class ReverseLinkedListTests {
+  private ReverseLinkedListSolution _solution;
+  public ReverseLinkedListTests (){
+      _solution = new();
+  }
+  [Fact]
+  public void Test1(){
+  }
+}
