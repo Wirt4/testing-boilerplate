b511d85af7b52ca05ed56fd8008d39ab799bf6f5
Wirt4 <wirt.salthouse@gmail.com>
2024-02-02 15:37:07 -0800

    passes test

--- LeetCodeSolutions/ReverseLinkedList/ReverseLinkedList.cs
+++ LeetCodeSolutions/ReverseLinkedList/ReverseLinkedList.cs
@@ -3,6 +3,20 @@ public class ReverseLinkedListSolution
 {
     public ListNode ReverseList(ListNode head)
     {
-        return head;
+        if (head == null || head.next == null)
+        {
+            return head;
+        }
+
+        ListNode reversedList = ReverseList(head.next);
+        head.next = null;
+        ListNode tail = reversedList;
+        while (tail != null && tail.next != null)
+        {
+            tail = tail.next;
+        }
+        tail.next = head;
+
+        return reversedList;
     }
 }
