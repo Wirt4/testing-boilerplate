653f653ebf66515eeea47b57a5bb64a561ec09f5
Wirt4 <wirt.salthouse@gmail.com>
2024-02-02 15:03:45 -0800

    more function decomp

--- LeetCodeSolutions/OddEvenLinkedList/OddEvenLinkedList.cs
+++ LeetCodeSolutions/OddEvenLinkedList/OddEvenLinkedList.cs
@@ -5,8 +5,8 @@ public class OddEvenLinkedListSolution
 {
     private class ListWrapper
     {
-        public ListNode Head;
-        public ListNode Tail;
+        public ListNode? Head;
+        public ListNode? Tail;
 
         public void Add(ref ListNode node)
         {
@@ -23,31 +23,56 @@ public class OddEvenLinkedListSolution
         }
     }
 
+    private class ListIterator
+    {
+        private int position;
+        private ListNode current;
+        public ListIterator(ListNode head)
+        {
+            current = head;
+            position = 1;
+        }
+
+        public bool PositionIsEven()
+        {
+            return position % 2 == 0;
+        }
+
+        public bool HasRemainingListItems()
+        {
+            return current != null;
+        }
+
+        public void Advance()
+        {
+            current = current.next;
+            position++;
+        }
+
+        public ref ListNode CurrentNode => ref current;
+    }
+
     public ListNode OddEvenList(ListNode head)
     {
-        int position = 1;
         ListWrapper evens = new();
         ListWrapper odds = new();
+        ListIterator it = new(head);
 
-        while (head != null)
+        while (it.HasRemainingListItems())
         {
-            if (position % 2 == 0)
+            if (it.PositionIsEven())
             {
-                evens.Add(ref head);
+                evens.Add(ref it.CurrentNode);
             }
             else
             {
-                odds.Add(ref head);
+                odds.Add(ref it.CurrentNode);
             }
-
-            head = head.next;
-            position++;
+            it.Advance();
         }
 
         odds.Tail.next = evens.Head;
         evens.Tail.next = null;
         return odds.Head;
-
-
     }
 }
