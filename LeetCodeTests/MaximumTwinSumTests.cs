6b6e880bbe3f066294b297bd6ac27284a3baa431
Wirt4 <wirt.salthouse@gmail.com>
2024-02-03 15:56:57 -0800

    added case for example 3

--- LeetCodeTests/MaximumTwinSumTests.cs
+++ LeetCodeTests/MaximumTwinSumTests.cs
@@ -14,4 +14,11 @@ public class MaximumTwinSumTests
     ListNode basicInput = LinkedListTesting.ListFromArray([1, 2]);
     Assert.Equal(3, _solution.PairSum(basicInput));
   }
+
+  [Fact]
+  public void Example3()
+  {
+    ListNode basicInput = LinkedListTesting.ListFromArray([1, 100000]);
+    Assert.Equal(100000, _solution.PairSum(basicInput));
+  }
 }
