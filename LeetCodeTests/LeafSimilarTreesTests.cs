1b5b1a78d7b3e0659440a9526e5d37b8c572ffac
Wirt4 <wirt.salthouse@gmail.com>
2024-02-03 16:48:38 -0800

    initialized with README

---
+++ LeetCodeTests/LeafSimilarTreesTests.cs
@@ -0,0 +1,11 @@
+namespace Tests;
+using LeetCodeSolutions;
+ public class LeafSimilarTreesTests {
+  private LeafSimilarTreesSolution _solution;
+  public LeafSimilarTreesTests (){
+      _solution = new();
+  }
+  [Fact]
+  public void Test1(){
+  }
+}
