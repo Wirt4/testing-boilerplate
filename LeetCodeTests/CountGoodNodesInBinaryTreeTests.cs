a83af2a17c79ddc4674df870bd0de3a861432076
Wirt4 <wirt.salthouse@gmail.com>
2024-02-04 10:28:22 -0800

    initiat with README

---
+++ LeetCodeTests/CountGoodNodesInBinaryTreeTests.cs
@@ -0,0 +1,11 @@
+namespace Tests;
+using LeetCodeSolutions;
+ public class CountGoodNodesInBinaryTreeTests {
+  private CountGoodNodesInBinaryTreeSolution _solution;
+  public CountGoodNodesInBinaryTreeTests (){
+      _solution = new();
+  }
+  [Fact]
+  public void Test1(){
+  }
+}
