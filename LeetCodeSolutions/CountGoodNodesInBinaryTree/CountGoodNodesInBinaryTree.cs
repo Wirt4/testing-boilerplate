14c3838a49f5b52aef9160a1ba4d9df604156525
Wirt4 <wirt.salthouse@gmail.com>
2024-02-04 10:36:05 -0800

    initial thought

--- LeetCodeSolutions/CountGoodNodesInBinaryTree/CountGoodNodesInBinaryTree.cs
+++ LeetCodeSolutions/CountGoodNodesInBinaryTree/CountGoodNodesInBinaryTree.cs
@@ -1,2 +1,26 @@
 namespace LeetCodeSolutions;
-public class CountGoodNodesInBinaryTreeSolution {}
+public class CountGoodNodesInBinaryTreeSolution
+{
+    private int NumberOfNodesLessThanRootValue(TreeNode node, int rootValue)
+    {
+        if (node == null)
+        {
+            return 0;
+        }
+
+        int sum = node.val <= rootValue ? 1 : 0;
+        int leftSum = NumberOfNodesLessThanRootValue(node.left, rootValue);
+        int rightSum = NumberOfNodesLessThanRootValue(node.right, rootValue);
+        return sum + leftSum + rightSum;
+    }
+    public int GoodNodes(TreeNode root)
+    {
+        if (root == null)
+        {
+            return 0;
+        }
+
+        int rootValue = root.val;
+        return NumberOfNodesLessThanRootValue(root, rootValue);
+    }
+}
