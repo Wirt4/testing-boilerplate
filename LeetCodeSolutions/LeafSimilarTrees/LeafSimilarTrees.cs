cf69dada8f0827e4d328055a1ccbd4c7f22aa388
Wirt4 <wirt.salthouse@gmail.com>
2024-02-03 16:52:23 -0800

    stub out function

--- LeetCodeSolutions/LeafSimilarTrees/LeafSimilarTrees.cs
+++ LeetCodeSolutions/LeafSimilarTrees/LeafSimilarTrees.cs
@@ -1,2 +1,8 @@
 namespace LeetCodeSolutions;
-public class LeafSimilarTreesSolution {}
+public class LeafSimilarTreesSolution
+{
+    public bool LeafSimilar(TreeNode root1, TreeNode root2)
+    {
+        return false;
+    }
+}
