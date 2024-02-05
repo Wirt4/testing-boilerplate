b5d345e20185fab0249d8e9077ef1cb60eaea09d
Wirt4 <wirt.salthouse@gmail.com>
2024-02-04 18:56:34 -0800

    passes lc tests

--- LeetCodeSolutions/PathSumIII/PathSumIII.cs
+++ LeetCodeSolutions/PathSumIII/PathSumIII.cs
@@ -1,2 +1,22 @@
 namespace LeetCodeSolutions;
-public class PathSumIIISolution {}
+public class PathSumIIISolution
+{
+    public int PathSum(TreeNode root, int targetSum, int parentSum = 0)
+    {
+        if (root == null)
+        {
+            return 0;
+        }
+        int currentVal = root.val + parentSum;
+        if (currentVal == targetSum)
+        {
+            return 1 + PathSum(root.left, targetSum) + PathSum(root.right, targetSum);
+        }
+        if (currentVal > targetSum)
+        {
+            return PathSum(root.left, targetSum) + PathSum(root.right, targetSum);
+        }
+        return PathSum(root.left, targetSum, currentVal) + PathSum(root.right, targetSum, currentVal) + PathSum(root.left, targetSum) + PathSum(root.right, targetSum);
+
+    }
+}
