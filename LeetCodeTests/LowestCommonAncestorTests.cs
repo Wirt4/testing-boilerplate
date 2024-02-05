cd9a286c1c9215a254dbd6d21bd4c5d3591083d3
Wirt4 <wirt.salthouse@gmail.com>
2024-02-05 14:37:11 -0800

    initiate with README

---
+++ LeetCodeTests/LowestCommonAncestorTests.cs
@@ -0,0 +1,11 @@
+namespace Tests;
+using LeetCodeSolutions;
+ public class LowestCommonAncestorTests {
+  private LowestCommonAncestorSolution _solution;
+  public LowestCommonAncestorTests (){
+      _solution = new();
+  }
+  [Fact]
+  public void Test1(){
+  }
+}
