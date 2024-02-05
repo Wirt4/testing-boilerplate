d450b887ca0771f8a3b07a92c16b9f0a10ffa688
Wirt4 <wirt.salthouse@gmail.com>
2024-02-04 20:43:48 -0800

    added breaking test

--- LeetCodeTests/PathSumIITests.cs
+++ LeetCodeTests/PathSumIITests.cs
@@ -14,4 +14,12 @@ public class PathSumIITests
     root.left = new(2);
     Assert.Equal([], _solution.PathSum(root, 0));
   }
+
+  [Fact]
+  public void Example3DifferentTarget()
+  {
+    TreeNode root = new(1);
+    root.left = new(2);
+    Assert.Equal([[1, 2]], _solution.PathSum(root, 3));
+  }
 }
