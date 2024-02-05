8c4c2a26873067d9571c7f5e5d54ce30c9a7517a
Wirt4 <wirt.salthouse@gmail.com>
2024-02-05 12:55:09 -0800

    initialize with README

---
+++ LeetCodeTests/LongestZigZagPathTests.cs
@@ -0,0 +1,11 @@
+namespace Tests;
+using LeetCodeSolutions;
+ public class LongestZigZagPathTests {
+  private LongestZigZagPathSolution _solution;
+  public LongestZigZagPathTests (){
+      _solution = new();
+  }
+  [Fact]
+  public void Test1(){
+  }
+}
