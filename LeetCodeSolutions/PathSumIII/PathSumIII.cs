6159de533f13b1a2f023d1cce780c4e3118805ec
Wirt4 <wirt.salthouse@gmail.com>
2024-02-05 12:23:46 -0800

    memoize with crib notes to make faster

--- LeetCodeSolutions/PathSumIII/PathSumIII.cs
+++ LeetCodeSolutions/PathSumIII/PathSumIII.cs
@@ -4,12 +4,17 @@ public class PathSumIIISolution
     private class TreeSearcher
     {
         private int _numPaths;
+        private Dictionary<int, int> _cache;
         public TreeSearcher()
         {
             _numPaths = 0;
+            _cache = new()
+            {
+                { 0, 1 }
+            };
         }
         public int NumberOfPaths => _numPaths;
-        private void TestAsRoot(TreeNode node, int targetSum)
+        private void SearchForSum(TreeNode node, int targetSum)
         {
             if (node == null)
             {
@@ -22,25 +27,52 @@ public class PathSumIIISolution
             }
 
             int adjustedSum = targetSum - node.val;
-            TestAsRoot(node.left, adjustedSum);
-            TestAsRoot(node.right, adjustedSum);
+            SearchForSum(node.left, adjustedSum);
+            SearchForSum(node.right, adjustedSum);
         }
-        public void DepthFirstSearch(TreeNode node, int targetSum)
+        private void _TraverseMemoized(TreeNode node, int targetSum, int currentPathSum)
+        {
+            if (node == null)
+            {
+                return;
+            }
+            currentPathSum += node.val;
+            int oldPathSum = currentPathSum - targetSum;
+            if (_cache.ContainsKey(oldPathSum))
+            {
+                _numPaths += _cache[oldPathSum];
+            }
+
+            if (_cache.ContainsKey(currentPathSum))
+            {
+                _cache[currentPathSum]++;
+            }
+            else
+            {
+                _cache.Add(currentPathSum, 1);
+            }
+
+            _TraverseMemoized(node.left, targetSum, currentPathSum);
+            _TraverseMemoized(node.right, targetSum, currentPathSum);
+
+            _cache[currentPathSum]--;
+        }
+        public void TraverseTree(TreeNode node, int targetSum)
         {
             if (node == null)
             {
                 return;
             }
 
-            TestAsRoot(node, targetSum);
-            DepthFirstSearch(node.left, targetSum);
-            DepthFirstSearch(node.right, targetSum);
+            SearchForSum(node, targetSum);
+            TraverseTree(node.left, targetSum);
+            TraverseTree(node.right, targetSum);
         }
     }
     public int PathSum(TreeNode root, int targetSum)
     {
         TreeSearcher searchWrapper = new();
-        searchWrapper.DepthFirstSearch(root, targetSum);
+        searchWrapper.TraverseTree(root, targetSum);
         return searchWrapper.NumberOfPaths;
     }
 }
