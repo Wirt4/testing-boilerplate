54c229ef2ecba014db11d9132621507c60eb7f4f
Wirt4 <wirt.salthouse@gmail.com>
2024-02-05 13:36:18 -0800

    added test for example 2

--- LeetCodeTests/LongestZigZagPathTests.cs
+++ LeetCodeTests/LongestZigZagPathTests.cs
@@ -27,6 +27,25 @@ public class LongestZigZagPathTests
 
     Assert.Equal(3, _solution.LongestZigZag(root));
   }
+
+  [Fact]
+  public void Example2()
+  {
+    TreeNode root = new(1);
+
+    root.left = new(1);
+    root.right = new(1);
+
+    root.left.right = new(1);
+
+    root.left.right.left = new(1);
+    root.left.right.right = new(1);
+
+    root.left.right.left.right = new(1);
+    Assert.Equal(4, _solution.LongestZigZag(root));
+
+  }
+
   [Fact]
   public void Example3()
   {
