952dc2aeb9b258b8ccb8f99917f527b4b20f5661
Wirt4 <wirt.salthouse@gmail.com>
2024-02-04 20:30:09 -0800

    don't like comments, make function signatures

--- LeetCodeSolutions/PathSum/PathSum.cs
+++ LeetCodeSolutions/PathSum/PathSum.cs
@@ -1,27 +1,40 @@
 namespace LeetCodeSolutions;
 public class PathSumSolution
 {
+    private static bool IsBaseCase(TreeNode? node)
+    {
+        return node == null;
+    }
+
+    private static bool IsEndLeaf(TreeNode node)
+    {
+        return IsBaseCase(node.left) && IsBaseCase(node.right);
+    }
+
+    private bool RecursiveCall(TreeNode node, int targetSum, int runningSum)
+    {
+        if (HasPathSum(node.left, targetSum, runningSum))
+        {
+            return true;
+        }
+
+        return HasPathSum(node.right, targetSum, runningSum);
+    }
     public bool HasPathSum(TreeNode root, int targetSum, int runningSum = 0)
     {
-        //base case
-        if (root == null)
+        if (IsBaseCase(root))
         {
             return false;
         }
 
         runningSum += root.val;
-        //is leaf
-        if (root.left == null && root.right == null)
+
+        if (IsEndLeaf(root))
         {
             return targetSum == runningSum;
         }
 
-
-
-        //make the recursive call
-        return HasPathSum(root.left, targetSum, runningSum) || HasPathSum(root.right, targetSum, runningSum);
-
-
+        return RecursiveCall(root, targetSum, runningSum);
 
     }
 }
