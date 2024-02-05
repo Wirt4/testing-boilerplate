d8daadb83c14267db992a69771ab1078e3d8370e
Wirt4 <wirt.salthouse@gmail.com>
2024-02-05 13:16:27 -0800

    passes basic tests

--- LeetCodeSolutions/LongestZigZagPath/LongestZigZagPath.cs
+++ LeetCodeSolutions/LongestZigZagPath/LongestZigZagPath.cs
@@ -1,8 +1,59 @@
 namespace LeetCodeSolutions;
 public class LongestZigZagPathSolution
 {
+    private enum Direction
+    {
+        Left,
+        Right
+    }
+    private int TraverseTree(TreeNode node, int longestZigZag)
+    {
+        if (node == null)
+        {
+            return longestZigZag;
+        }
+        int left = MaxZigZag(node, Direction.Left);
+        int right = MaxZigZag(node, Direction.Right);
+        int longestFromNode = left > right ? left : right;
+        if (longestFromNode > longestZigZag)
+        {
+            longestZigZag = longestFromNode;
+        }
+        int leftTraversal = TraverseTree(node.left, longestZigZag);
+        int rightTraversal = TraverseTree(node.right, longestZigZag);
+        return leftTraversal > rightTraversal ? leftTraversal : rightTraversal;
+    }
     public int LongestZigZag(TreeNode root)
     {
-        return 0;
+        return TraverseTree(root, 0);
+    }
+
+    private int MaxZigZag(TreeNode node, Direction direction)
+    {
+        if (node == null)
+        {
+            return 0;
+        }
+
+        if (direction == Direction.Left && node.left == null)
+        {
+            return 0;
+        }
+
+        if (direction == Direction.Right && node.right == null)
+        {
+            return 0;
+        }
+
+        if (direction == Direction.Left)
+        {
+            return 1 + MaxZigZag(node.left, Direction.Right);
+        }
+
+        if (direction == Direction.Right)
+        {
+            return 1 + MaxZigZag(node.right, Direction.Left);
+        }
+        return -1;
     }
 }
