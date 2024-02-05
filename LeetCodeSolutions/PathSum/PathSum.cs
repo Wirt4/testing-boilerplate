1aecb114ae54ee45daa6dd5953908948cb2cd4e9
Wirt4 <wirt.salthouse@gmail.com>
2024-02-04 20:20:35 -0800

    passes basic test set

--- LeetCodeSolutions/PathSum/PathSum.cs
+++ LeetCodeSolutions/PathSum/PathSum.cs
@@ -1,2 +1,26 @@
 namespace LeetCodeSolutions;
-public class PathSumSolution {}
+public class PathSumSolution
+{
+    public bool HasPathSum(TreeNode root, int targetSum, int runningSum = 0)
+    {
+        //base case
+        if (root == null)
+        {
+            return false;
+        }
+
+        //is leaf
+        if (root.left == null && root.right == null)
+        {
+            return targetSum == runningSum + root.val;
+        }
+
+        runningSum += root.val;
+
+        //make the recursive call
+        return HasPathSum(root.left, targetSum, runningSum) || HasPathSum(root.right, targetSum, runningSum);
+
+
+
+    }
+}
