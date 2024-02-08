bbd177b01f8d6b89d964d76183260502cc70b94b
Wirt4 <wirt.salthouse@gmail.com>
2024-02-08 12:02:15 -0800

    reduce one recursive call;

--- LeetCodeSolutions/LowestCommonAncestor/LowestCommonAncestor.cs
+++ LeetCodeSolutions/LowestCommonAncestor/LowestCommonAncestor.cs
@@ -6,14 +6,16 @@ public class LowestCommonAncestorSolution
 
     public TreeNode LowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q)
     {
+        if (p.val > q.val)
+        {
+            TreeNode temp = p;
+            p = q;
+            q = temp;
+        }
+
         if (root.val == p.val || root.val == q.val)
         {
             return root;
-        }
-
-        if (p.val > q.val)
-        {
-            return LowestCommonAncestor(root, q, p);
         }
 
         if (root.val > p.val && root.val < q.val)
