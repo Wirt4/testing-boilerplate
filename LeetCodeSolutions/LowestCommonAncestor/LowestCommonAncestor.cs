91d6211eac73f4f0961b275da5a74ff2781a49ca
Wirt S <34081557+Wirt4@users.noreply.github.com>
2024-02-08 18:27:57 -0800

    some grammatical niceties

--- LeetCodeSolutions/LowestCommonAncestor/LowestCommonAncestor.cs
+++ LeetCodeSolutions/LowestCommonAncestor/LowestCommonAncestor.cs
@@ -2,15 +2,17 @@ namespace LeetCodeSolutions;
 public class LowestCommonAncestorSolution
 {
 
-    public TreeNode LowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q)
+    public TreeNode? LowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q)
     {
+
         if (root == null || root.val == p.val || root.val == q.val)
         {
             return root;
         }
 
-        TreeNode leftQuery = LowestCommonAncestor(root.left, p, q);
-        TreeNode rightQuery = LowestCommonAncestor(root.right, p, q);
+
+        TreeNode? leftQuery = LowestCommonAncestor(root.left, p, q);
+        TreeNode? rightQuery = LowestCommonAncestor(root.right, p, q);
 
         if (leftQuery == null)
         {
