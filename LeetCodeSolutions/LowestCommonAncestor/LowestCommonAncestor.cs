c387605140a7e7aa1dd9ec870e4a538b10be37c8
Wirt S <34081557+Wirt4@users.noreply.github.com>
2024-02-09 16:17:56 -0800

    moved to wrapper class to reduce unecessar arguments

--- LeetCodeSolutions/LowestCommonAncestor/LowestCommonAncestor.cs
+++ LeetCodeSolutions/LowestCommonAncestor/LowestCommonAncestor.cs
@@ -1,29 +1,43 @@
 namespace LeetCodeSolutions;
 public class LowestCommonAncestorSolution
 {
+    private class TreeTraveler
+    {
+        private TreeNode _p;
+        private TreeNode _q;
+        public TreeTraveler(TreeNode p, TreeNode q)
+        {
+            _p = p;
+            _q = q;
+        }
+
+        private bool IsBaseCase(TreeNode? node)
+        {
+            return node == null || node.val == _p.val || node.val == _q.val;
+        }
+        public TreeNode? LCA(TreeNode? node)
+        {
+            if (IsBaseCase(node))
+            {
+                return node;
+            }
+            TreeNode? leftQuery = LCA(node.left);
+            TreeNode? rightQuery = LCA(node.right);
+            if (leftQuery == null)
+            {
+                return rightQuery;
+            }
+            if (rightQuery == null)
+            {
+                return leftQuery;
+            }
+            return node;
+        }
+    }
 
     public TreeNode? LowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q)
     {
-
-        if (root == null || root.val == p.val || root.val == q.val)
-        {
-            return root;
-        }
-
-
-        TreeNode? leftQuery = LowestCommonAncestor(root.left, p, q);
-        TreeNode? rightQuery = LowestCommonAncestor(root.right, p, q);
-
-        if (leftQuery == null)
-        {
-            return rightQuery;
-        }
-
-        if (rightQuery == null)
-        {
-            return leftQuery;
-        }
-
-        return root;
+        TreeTraveler tTrav = new TreeTraveler(p, q);
+        return tTrav.LCA(root);
     }
 }
