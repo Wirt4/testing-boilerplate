547eb56ee9b48164bfc728c6a88a40b61271267b
Wirt S <34081557+Wirt4@users.noreply.github.com>
2024-02-09 16:24:08 -0800

    use primaryt constructor

--- LeetCodeSolutions/LowestCommonAncestor/LowestCommonAncestor.cs
+++ LeetCodeSolutions/LowestCommonAncestor/LowestCommonAncestor.cs
@@ -1,15 +1,10 @@
 namespace LeetCodeSolutions;
 public class LowestCommonAncestorSolution
 {
-    private class TreeTraveler
+    private class TreeTraveler(TreeNode p, TreeNode q)
     {
-        private readonly int p_value;
-        private readonly int q_value;
-        public TreeTraveler(TreeNode p, TreeNode q)
-        {
-            p_value = p.val;
-            q_value = q.val;
-        }
+        private readonly int p_value = p.val;
+        private readonly int q_value = q.val;
 
         private bool IsBaseCase(TreeNode? node)
         {
