0f2880c3f09565b2889f7dcff07301d4b4616691
Wirt S <34081557+Wirt4@users.noreply.github.com>
2024-02-09 16:09:27 -0800

    use an object to make arguments simplier

--- LeetCodeSolutions/LongestZigZagPath/LongestZigZagPath.cs
+++ LeetCodeSolutions/LongestZigZagPath/LongestZigZagPath.cs
@@ -10,19 +10,31 @@ public class LongestZigZagPathSolution
         {
             return Math.Max(left, right);
         }
+
+        public Lengths ShiftLeft()
+        {
+            return new Lengths(left + 1, 0);
+        }
+
+        public Lengths ShiftRight()
+        {
+            return new Lengths(0, right + 1);
+        }
     }
 
-    private int LongestZigZag_R(TreeNode node, Lengths lengths)
+    private static int LongestZigZag_R(TreeNode node, Lengths lengths)
     {
         if (node != null)
         {
-            lengths.left = LongestZigZag_R(node.left, new(lengths.left + 1, 0));
-            lengths.right = LongestZigZag_R(node.right, new(0, lengths.right + 1));
+            lengths.left = LongestZigZag_R(node.left, lengths.ShiftLeft());
+            lengths.right = LongestZigZag_R(node.right, lengths.ShiftRight());
         }
+
         return lengths.MaxLength();
     }
     public int LongestZigZag(TreeNode root)
     {
-        return LongestZigZag_R(root, new());
+        Lengths lengthsOfZero = new();
+        return LongestZigZag_R(root, lengthsOfZero);
     }
 }
