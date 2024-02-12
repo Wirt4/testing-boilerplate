0b07a63f7ad70ccf0fd19fee3a110b697de48302
Wirt S <34081557+Wirt4@users.noreply.github.com>
2024-02-11 22:39:48 -0800

    passes tests

--- LeetCodeSolutions/BinaryTreeRightSideView/BinaryTreeRightSideView.cs
+++ LeetCodeSolutions/BinaryTreeRightSideView/BinaryTreeRightSideView.cs
@@ -4,11 +4,25 @@ public class BinaryTreeRightSideViewSolution
     public IList<int> RightSideView(TreeNode? root)
     {
         List<int> returnList = new();
-        TreeNode? cursor = root;
-        while (cursor != null)
+        int level = 0;
+        Queue<TreeNode?> levelTraversalQueue = new();
+
+        levelTraversalQueue.Enqueue(root);
+        while (levelTraversalQueue.Count > 0)
         {
-            returnList.Add(cursor.val);
-            cursor = cursor.right;
+            Stack<int> valueStack = new();
+            for (int i = 0; i < (int)Math.Pow(2, level); i++)
+            {
+                TreeNode? cur = levelTraversalQueue.Dequeue();
+                if (cur == null) continue;
+                levelTraversalQueue.Enqueue(cur.left);
+                levelTraversalQueue.Enqueue(cur.right);
+                valueStack.Push(cur.val);
+            }
+            if (valueStack.TryPop(out int value))
+            {
+                returnList.Add(value);
+            }
         }
 
         return [.. returnList];
