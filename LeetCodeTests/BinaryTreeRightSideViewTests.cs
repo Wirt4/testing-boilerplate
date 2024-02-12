9ec10e573178c21baeef0bfea0b6a6b6f9338d56
Wirt S <34081557+Wirt4@users.noreply.github.com>
2024-02-11 22:12:43 -0800

    intialize with README

---
+++ LeetCodeTests/BinaryTreeRightSideViewTests.cs
@@ -0,0 +1,11 @@
+namespace Tests;
+using LeetCodeSolutions;
+ public class BinaryTreeRightSideViewTests {
+  private BinaryTreeRightSideViewSolution _solution;
+  public BinaryTreeRightSideViewTests (){
+      _solution = new();
+  }
+  [Fact]
+  public void Test1(){
+  }
+}
