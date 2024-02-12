33f1ebdac9ae96d65eeff6d872507a5dea600543
Wirt S <34081557+Wirt4@users.noreply.github.com>
2024-02-12 14:28:20 -0800

    pass test

--- LeetCodeSolutions/LongestZigZagPath/LongestZigZagPath.cs
+++ LeetCodeSolutions/LongestZigZagPath/LongestZigZagPath.cs
@@ -6,7 +6,7 @@ public class LongestZigZagPathSolution
 
     public int LongestZigZag(TreeNode? root)
     {
-        if (root == null) return -1;
+        if (root == null) return 0;
         if (root.left == null && root.right == null) { return 0; }
         return 1;
     }
