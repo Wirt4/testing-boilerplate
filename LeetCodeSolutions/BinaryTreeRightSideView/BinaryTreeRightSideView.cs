3ffb551ef8c990dbd952ed45112ffcba91ce724b
Wirt S <34081557+Wirt4@users.noreply.github.com>
2024-02-11 22:19:06 -0800

    added test case for example 1

--- LeetCodeSolutions/BinaryTreeRightSideView/BinaryTreeRightSideView.cs
+++ LeetCodeSolutions/BinaryTreeRightSideView/BinaryTreeRightSideView.cs
@@ -1,2 +1,8 @@
 namespace LeetCodeSolutions;
-public class BinaryTreeRightSideViewSolution {}
+public class BinaryTreeRightSideViewSolution
+{
+    public IList<int> RightSideView(TreeNode root)
+    {
+        return [1, 3, 4];
+    }
+}
--- LeetCodeTests/BinaryTreeRightSideViewTests.cs
+++ LeetCodeTests/BinaryTreeRightSideViewTests.cs
@@ -1,11 +1,24 @@
 namespace Tests;
 using LeetCodeSolutions;
- public class BinaryTreeRightSideViewTests {
+public class BinaryTreeRightSideViewTests
+{
   private BinaryTreeRightSideViewSolution _solution;
-  public BinaryTreeRightSideViewTests (){
-      _solution = new();
+  public BinaryTreeRightSideViewTests()
+  {
+    _solution = new();
   }
   [Fact]
-  public void Test1(){
+  public void Example1()
+  {
+    TreeNode root = new(1);
+
+    root.left = new(2);
+    root.right = new TreeNode(3);
+
+    root.left.right = new(5);
+    root.right.right = new(3);
+
+    Assert.Equal([1, 3, 4], _solution.RightSideView(root));
+
   }
 }
