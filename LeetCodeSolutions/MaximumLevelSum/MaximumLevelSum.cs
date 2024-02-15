6f7882a8a9593083f5e7b7c561b515b7d3bba8ce
Wirt S <34081557+Wirt4@users.noreply.github.com>
2024-02-15 15:02:54 -0800

    passes test

--- LeetCodeSolutions/MaximumLevelSum/MaximumLevelSum.cs
+++ LeetCodeSolutions/MaximumLevelSum/MaximumLevelSum.cs
@@ -3,6 +3,22 @@ public class MaximumLevelSumSolution
 {
     public int MaxLevelSum(TreeNode root)
     {
-        return root.val;
+        Stack<TreeNode> nodeStack = new();
+        nodeStack.Push(root);
+        int highest = root.val;
+        while (nodeStack.Count > 0)
+        {
+            TreeNode current = nodeStack.Pop();
+            if (current.left != null)
+            {
+                nodeStack.Push(current.left);
+            }
+            if (current.right != null)
+            {
+                nodeStack.Push(current.right);
+            }
+            highest = highest > current.val ? highest : current.val;
+        }
+        return highest;
     }
 }
