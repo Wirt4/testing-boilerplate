1332f6c9979cf134d45514f0feac6a4c4adfdf8e
Wirt S <34081557+Wirt4@users.noreply.github.com>
2024-02-15 14:53:26 -0800

    initialize files

---
+++ LeetCodeTests/MaximumLevelSumTests.cs
@@ -0,0 +1,11 @@
+namespace Tests;
+using LeetCodeSolutions;
+ public class MaximumLevelSumTests {
+  private MaximumLevelSumSolution _solution;
+  public MaximumLevelSumTests (){
+      _solution = new();
+  }
+  [Fact]
+  public void Test1(){
+  }
+}
