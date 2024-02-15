dbf46f67b734465e218401ee2c256b976bc337df
Wirt S <34081557+Wirt4@users.noreply.github.com>
2024-02-15 15:10:34 -0800

    added failing test

--- LeetCodeTests/MaximumLevelSumTests.cs
+++ LeetCodeTests/MaximumLevelSumTests.cs
@@ -27,4 +27,13 @@ public class MaximumLevelSumTests
     root.left = new(8);
     Assert.Equal(8, _solution.MaxLevelSum(root));
   }
+
+  [Fact]
+  public void TwoLevelTree2()
+  {
+    TreeNode root = new(4);
+    root.left = new(8);
+    root.right = new(2);
+    Assert.Equal(10, _solution.MaxLevelSum(root));
+  }
 }
