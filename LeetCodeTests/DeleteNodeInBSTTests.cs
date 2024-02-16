7537c4aeb51e8dadb85554da4c9300a4c6971d2c
Wirt S <34081557+Wirt4@users.noreply.github.com>
2024-02-15 18:32:41 -0800

    create files

---
+++ LeetCodeTests/DeleteNodeInBSTTests.cs
@@ -0,0 +1,11 @@
+namespace Tests;
+using LeetCodeSolutions;
+ public class DeleteNodeInBSTTests {
+  private DeleteNodeInBSTSolution _solution;
+  public DeleteNodeInBSTTests (){
+      _solution = new();
+  }
+  [Fact]
+  public void Test1(){
+  }
+}
