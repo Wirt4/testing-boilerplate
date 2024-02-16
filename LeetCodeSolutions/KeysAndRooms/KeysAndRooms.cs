401ee4e78987330e4ccc9a863023e8941286fc69
Wirt S <34081557+Wirt4@users.noreply.github.com>
2024-02-15 22:59:47 -0800

    passes tests

--- LeetCodeSolutions/KeysAndRooms/KeysAndRooms.cs
+++ LeetCodeSolutions/KeysAndRooms/KeysAndRooms.cs
@@ -1,8 +1,61 @@
+using System.Dynamic;
+
 namespace LeetCodeSolutions;
 public class KeysAndRoomsSolution
 {
+
+
+    private class Rooms
+    {
+        private IList<IList<int>> _rooms;
+        public Rooms(IList<IList<int>> rooms)
+        {
+            _rooms = rooms;
+        }
+
+        private bool DFSUtil(int v, bool[] visited)
+        {
+            visited[v] = true;
+            bool traversed = true;
+            foreach (bool thing in visited)
+            {
+                if (!thing)
+                {
+                    traversed = false;
+                    break;
+                }
+            }
+
+            if (traversed)
+            {
+                return true;
+            }
+
+            IList<int> vList = _rooms[v];
+            foreach (var n in vList)
+            {
+                if (!visited[n])
+                {
+                    bool path = DFSUtil(n, visited);
+                    if (path)
+                    {
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+        public Boolean DepthFirstSearch(int startRoom)
+        {
+            bool[] visited = new bool[_rooms.Count];
+            return DFSUtil(startRoom, visited);
+        }
+
+    }
     public bool CanVisitAllRooms(IList<IList<int>> rooms)
     {
-        return true;
+        Rooms roomGraph = new(rooms);
+        return roomGraph.DepthFirstSearch(0);
+
     }
 }
