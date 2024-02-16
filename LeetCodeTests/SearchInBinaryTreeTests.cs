b2df5b394adedda440f7622e88423182891e5749
Wirt S <34081557+Wirt4@users.noreply.github.com>
2024-02-15 17:24:45 -0800

    create files

---
+++ LeetCodeTests/SearchInBinaryTreeTests.cs
@@ -0,0 +1,11 @@
+namespace Tests;
+using LeetCodeSolutions;
+ public class SearchInBinaryTreeTests {
+  private SearchInBinaryTreeSolution _solution;
+  public SearchInBinaryTreeTests (){
+      _solution = new();
+  }
+  [Fact]
+  public void Test1(){
+  }
+}
