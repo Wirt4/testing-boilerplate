c1d4602b792ec0c7671c027d2ea91c96dd270c30
Wirt S <34081557+Wirt4@users.noreply.github.com>
2024-02-15 17:28:26 -0800

    added test

--- LeetCodeSolutions/SearchInBinaryTree/SearchInBinaryTree.cs
+++ LeetCodeSolutions/SearchInBinaryTree/SearchInBinaryTree.cs
@@ -1,2 +1,8 @@
 namespace LeetCodeSolutions;
-public class SearchInBinaryTreeSolution {}
+public class SearchInBinaryTreeSolution
+{
+    public TreeNode SearchBST(TreeNode root, int val)
+    {
+        return root;
+    }
+}
--- LeetCodeTests/SearchInBinaryTreeTests.cs
+++ LeetCodeTests/SearchInBinaryTreeTests.cs
@@ -1,11 +1,16 @@
 namespace Tests;
 using LeetCodeSolutions;
- public class SearchInBinaryTreeTests {
+public class SearchInBinaryTreeTests
+{
   private SearchInBinaryTreeSolution _solution;
-  public SearchInBinaryTreeTests (){
-      _solution = new();
+  public SearchInBinaryTreeTests()
+  {
+    _solution = new();
   }
   [Fact]
-  public void Test1(){
+  public void ValueNotInTree()
+  {
+    TreeNode root = new(2);
+    Assert.Null(_solution.SearchBST(root, 1));
   }
 }
