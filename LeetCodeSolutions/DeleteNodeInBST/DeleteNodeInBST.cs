f217b26c782df51a46e045020759d30ac71625aa
Wirt S <34081557+Wirt4@users.noreply.github.com>
2024-02-15 19:34:19 -0800

    wrote method to test equality of trees

--- LeetCodeSolutions/DeleteNodeInBST/DeleteNodeInBST.cs
+++ LeetCodeSolutions/DeleteNodeInBST/DeleteNodeInBST.cs
@@ -3,6 +3,7 @@ public class DeleteNodeInBSTSolution
 {
     public TreeNode? DeleteNode(TreeNode root, int key)
     {
+
         return root;
     }
 }
--- LeetCodeTests/DeleteNodeInBSTTests.cs
+++ LeetCodeTests/DeleteNodeInBSTTests.cs
@@ -13,4 +13,26 @@ public class DeleteNodeInBSTTests
     TreeNode root = new(1);
     Assert.Equal(root, _solution.DeleteNode(root, 49));
   }
+
+  [Fact]
+  public void OneDTreeOfTwo()
+  {
+    TreeNode root = new(1);
+    root.left = new(4);
+    Assert.True(EqualTrees(new TreeNode(1), _solution.DeleteNode(root, 4)));
+  }
+
+  private bool EqualTrees(TreeNode? root1, TreeNode? root2)
+  {
+    if (root1 == null && root2 == null)
+    {
+      return true;
+    }
+
+    if (root1 == null || root2 == null || root1.val != root2.val)
+    {
+      return false;
+    }
+    return EqualTrees(root1.left, root2.left) && EqualTrees(root1.right, root2.right);
+  }
 }
