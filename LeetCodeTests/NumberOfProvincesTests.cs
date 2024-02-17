ec660aa82d52d15dabdfc143c495f05c064ef3be
Wirt S <34081557+Wirt4@users.noreply.github.com>
2024-02-17 13:23:18 -0800

    files created

---
+++ LeetCodeTests/NumberOfProvincesTests.cs
@@ -0,0 +1,11 @@
+namespace Tests;
+using LeetCodeSolutions;
+ public class NumberOfProvincesTests {
+  private NumberOfProvincesSolution _solution;
+  public NumberOfProvincesTests (){
+      _solution = new();
+  }
+  [Fact]
+  public void Test1(){
+  }
+}
