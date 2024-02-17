47d6769154d6d238fcac467a5c87ebaf9715cad9
Wirt S <34081557+Wirt4@users.noreply.github.com>
2024-02-17 14:21:34 -0800

    trying to avoid unnecessary traversal

--- LeetCodeSolutions/NumberOfProvinces/NumberOfProvinces.cs
+++ LeetCodeSolutions/NumberOfProvinces/NumberOfProvinces.cs
@@ -5,21 +5,25 @@ public class NumberOfProvincesSolution
 
     public int FindCircleNum(int[][] isConnected)
     {
-        HashSet<int> visited = new();
+        HashSet<int> remaining = new();
+        for (int i = 0; i < isConnected.Length; i++)
+        {
+            remaining.Add(i);
+        }
         Stack<int> adjacent = new();
         adjacent.Push(0);
         int provinces = 0;
-        int curNdex = 0;
+        int curNdex;
 
-        while (curNdex < isConnected.Length)
+        while (remaining.Count > 0)
         {
             while (adjacent.Count > 0)
             {
                 curNdex = adjacent.Pop();
-                visited.Add(curNdex);
+                remaining.Remove(curNdex);
                 for (int j = 0; j < isConnected.Length; j++)
                 {
-                    if (!visited.Contains(j) && isConnected[curNdex][j] == 1)
+                    if (remaining.Contains(j) && isConnected[curNdex][j] == 1)
                     {
                         adjacent.Push(j);
                     }
@@ -28,21 +32,12 @@ public class NumberOfProvincesSolution
 
             provinces++;
 
-            int i = 0;
-            while (visited.Contains(i))
+            foreach (int unVisited in remaining)
             {
-                i++;
+                adjacent.Push(unVisited);
+                break;
             }
-
-            curNdex = i;
-            adjacent.Push(curNdex);
-
         }
-
-
-
-
-
 
         return provinces;
     }
