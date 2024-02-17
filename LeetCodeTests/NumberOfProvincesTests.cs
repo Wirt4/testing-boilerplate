4e90be046762feaf4ca4dec32c7f4e88e22730aa
Wirt S <34081557+Wirt4@users.noreply.github.com>
2024-02-17 13:29:29 -0800

    added test for Example 1

--- LeetCodeTests/NumberOfProvincesTests.cs
+++ LeetCodeTests/NumberOfProvincesTests.cs
@@ -8,6 +8,12 @@ public class NumberOfProvincesTests
     _solution = new();
   }
   [Fact]
+  public void Example1()
+  {
+    int[][] cities = [[1, 1, 0], [1, 1, 0], [0, 0, 1]];
+    Assert.Equal(2, _solution.FindCircleNum(cities));
+  }
+  [Fact]
   public void Example2()
   {
     int[][] cities = [[1, 0, 0], [0, 1, 0], [0, 0, 1]];
