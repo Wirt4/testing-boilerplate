066411205175e54e16421e5f19a24dbc923e880f
Wirt S <34081557+Wirt4@users.noreply.github.com>
2024-02-20 07:44:44 -0800

    create files

---
+++ LeetCodeTests/ReorderRoutesToLeadToZeroTests.cs
@@ -0,0 +1,11 @@
+namespace Tests;
+using LeetCodeSolutions;
+ public class ReorderRoutesToLeadToZeroTests {
+  private ReorderRoutesToLeadToZeroSolution _solution;
+  public ReorderRoutesToLeadToZeroTests (){
+      _solution = new();
+  }
+  [Fact]
+  public void Test1(){
+  }
+}
