ab1ee22d86db91477ddc11b7da668377d4d5e211
Wirt S <34081557+Wirt4@users.noreply.github.com>
2024-02-21 00:27:07 -0800

    reduce number of arguments

--- LeetCodeSolutions/ReorderRoutesToLeadToZero/ReorderRoutesToLeadToZero.cs
+++ LeetCodeSolutions/ReorderRoutesToLeadToZero/ReorderRoutesToLeadToZero.cs
@@ -2,33 +2,46 @@ namespace LeetCodeSolutions;
 public class ReorderRoutesToLeadToZeroSolution
 {
 
-    int count = 0;
-    void dfs(int node, int parent, ref List<List<Tuple<int, int>>> adj)
+    private class GraphSearcher
     {
-        foreach (Tuple<int, int> pair in adj[node])
+        public int Count;
+
+        private List<List<Tuple<int, int>>> adj;
+        public GraphSearcher(int n, ref int[][] connections)
         {
-            if (pair.Item1 != parent)
+            Count = 0;
+            adj = new();
+
+            for (int i = 0; i < n; i++)
             {
-                count += pair.Item2;
-                dfs(pair.Item1, node, ref adj);
+                adj.Add([]);
+            }
+
+            foreach (int[] connetion in connections)
+            {
+                adj[connetion[0]].Add(new Tuple<int, int>(connetion[1], 1));
+                adj[connetion[1]].Add(new Tuple<int, int>(connetion[0], 0));
+            }
+        }
+
+        public void DepthFirstSearch(int node, int parent)
+        {
+            foreach (Tuple<int, int> pair in adj[node])
+            {
+                if (pair.Item1 != parent)
+                {
+                    Count += pair.Item2;
+                    DepthFirstSearch(pair.Item1, node);
+                }
             }
         }
     }
+
+
     public int MinReorder(int n, int[][] connections)
     {
-        List<List<Tuple<int, int>>> adj = new();
-        for (int i = 0; i < n; i++)
-        {
-            adj.Add([]);
-        }
-
-        foreach (int[] connetion in connections)
-        {
-            adj[connetion[0]].Add(new Tuple<int, int>(connetion[1], 1));
-            adj[connetion[1]].Add(new Tuple<int, int>(connetion[0], 0));
-        }
-
-        dfs(0, -1, ref adj);
-        return count;
+        GraphSearcher searcher = new(n, ref connections);
+        searcher.DepthFirstSearch(0, -1);
+        return searcher.Count;
     }
 }
