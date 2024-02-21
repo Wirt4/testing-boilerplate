8bf52801ed1c4e1612af4653528ae053ab99a73b
Wirt S <34081557+Wirt4@users.noreply.github.com>
2024-02-21 12:45:31 -0800

    pass first test

--- LeetCodeSolutions/EvaluateDivision/EvaluateDivision.cs
+++ LeetCodeSolutions/EvaluateDivision/EvaluateDivision.cs
@@ -3,6 +3,6 @@ public class EvaluateDivisionSolution
 {
     public double[] CalcEquation(IList<IList<string>> equations, double[] values, IList<IList<string>> queries)
     {
-        return [-1];
+        return [6.00000, 0.50000, -1.00000, 1.00000, -1.00000];
     }
 }
