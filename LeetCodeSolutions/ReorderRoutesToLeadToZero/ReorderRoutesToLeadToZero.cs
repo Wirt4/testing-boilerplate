92f939e117e4a17126fe5d9c1edc3a5e5a75ac0b
Wirt S <34081557+Wirt4@users.noreply.github.com>
2024-02-21 00:36:32 -0800

    reduce number of arguments in calling level

--- LeetCodeSolutions/ReorderRoutesToLeadToZero/ReorderRoutesToLeadToZero.cs
+++ LeetCodeSolutions/ReorderRoutesToLeadToZero/ReorderRoutesToLeadToZero.cs
@@ -6,27 +6,35 @@ public class ReorderRoutesToLeadToZeroSolution
     {
         public int Count;
 
-        private List<List<Tuple<int, int>>> adj;
+        private readonly List<List<Tuple<int, int>>> adjacents;
         public GraphSearcher(int n, ref int[][] connections)
         {
             Count = 0;
-            adj = new();
+            adjacents = [];
 
             for (int i = 0; i < n; i++)
             {
-                adj.Add([]);
+                adjacents.Add([]);
             }
 
-            foreach (int[] connetion in connections)
+            foreach (int[] connection in connections)
             {
-                adj[connetion[0]].Add(new Tuple<int, int>(connetion[1], 1));
-                adj[connetion[1]].Add(new Tuple<int, int>(connetion[0], 0));
+                AddTuple(0, connection);
+                AddTuple(1, connection);
             }
         }
 
-        public void DepthFirstSearch(int node, int parent)
+        private void AddTuple(int ndx, int[] connection)
         {
-            foreach (Tuple<int, int> pair in adj[node])
+            int ndx2 = ndx == 0 ? 1 : 0;
+
+            adjacents[connection[ndx]].Add(new Tuple<int, int>(connection[ndx2], ndx2));
+
+        }
+
+        public void DepthFirstSearch(int node = 0, int parent = -1)
+        {
+            foreach (Tuple<int, int> pair in adjacents[node])
             {
                 if (pair.Item1 != parent)
                 {
@@ -41,7 +49,7 @@ public class ReorderRoutesToLeadToZeroSolution
     public int MinReorder(int n, int[][] connections)
     {
         GraphSearcher searcher = new(n, ref connections);
-        searcher.DepthFirstSearch(0, -1);
+        searcher.DepthFirstSearch();
         return searcher.Count;
     }
 }
