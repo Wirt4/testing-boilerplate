4215bebb88f5ebb553826d7c672841b926aedf5c
Wirt S <34081557+Wirt4@users.noreply.github.com>
2024-02-20 16:34:45 -0800

    passes test for example 2

--- LeetCodeSolutions/ReorderRoutesToLeadToZero/ReorderRoutesToLeadToZero.cs
+++ LeetCodeSolutions/ReorderRoutesToLeadToZero/ReorderRoutesToLeadToZero.cs
@@ -1,8 +1,93 @@
+using System.Dynamic;
+
 namespace LeetCodeSolutions;
 public class ReorderRoutesToLeadToZeroSolution
 {
     public int MinReorder(int n, int[][] connections)
     {
-        return 3;
+        Dictionary<int, HashSet<int>> ConnectionsDefault = new();
+        Dictionary<int, HashSet<int>> ConnectionsReversed = new();
+        foreach (int[] connection in connections)
+        {
+            if (ConnectionsDefault.ContainsKey(connection[0]))
+            {
+                ConnectionsDefault[connection[0]].Add(connection[1]);
+            }
+            else
+            {
+                ConnectionsDefault.Add(connection[0], new());
+                ConnectionsDefault[connection[0]].Add(connection[1]);
+            }
+
+            if (ConnectionsReversed.ContainsKey(connection[1]))
+            {
+                ConnectionsReversed[connection[1]].Add(connection[0]);
+            }
+            else
+            {
+                ConnectionsReversed.Add(connection[1], new());
+                ConnectionsReversed[connection[1]].Add(connection[0]);
+            }
+        }
+
+        Stack<int> Adjacent = new();
+        HashSet<int> visited = [0];
+        int flips = 0;
+        if (connections[0][0] == 0)
+        {
+            foreach (int connection in ConnectionsDefault[0])
+            {
+                Adjacent.Push(connection);
+            }
+        }
+        else
+        {
+            foreach (int connection in ConnectionsReversed[0])
+            {
+                Adjacent.Push(connection);
+            }
+        }
+
+
+        while (Adjacent.Count > 0 && visited.Count < n)
+        {
+            int currentNode = Adjacent.Pop();
+            visited.Add(currentNode);
+
+            if (ConnectionsDefault.ContainsKey(currentNode))
+            {
+
+                foreach (int connection in ConnectionsDefault[currentNode])
+                {
+                    if (!visited.Contains(connection))
+                    {
+                        Adjacent.Push(connection);
+                    }
+                }
+            }
+            else if (ConnectionsReversed.ContainsKey(currentNode))
+            {
+                flips++;
+                foreach (int connection in ConnectionsReversed[currentNode])
+                {
+                    if (!visited.Contains(connection))
+                    {
+                        Adjacent.Push(connection);
+                    }
+                }
+
+            }
+            else
+            {
+                throw new Exception("hash sets not built properly");
+            }
+
+
+
+        }
+
+
+
+        return flips;
     }
 }
--- LeetCodeTests/ReorderRoutesToLeadToZeroTests.cs
+++ LeetCodeTests/ReorderRoutesToLeadToZeroTests.cs
@@ -12,4 +12,10 @@ public class ReorderRoutesToLeadToZeroTests
   {
     Assert.Equal(3, _solution.MinReorder(6, [[0, 1], [1, 3], [2, 3], [4, 0], [4, 5]]));
   }
+
+  [Fact]
+  public void Example2()
+  {
+    Assert.Equal(2, _solution.MinReorder(5, [[1, 0], [1, 2], [3, 2], [3, 4]]));
+  }
 }
