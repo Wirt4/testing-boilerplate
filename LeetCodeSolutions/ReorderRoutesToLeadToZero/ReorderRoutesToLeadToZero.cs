b08abc79638c76001b5024f1af5c7952f35be325
Wirt S <34081557+Wirt4@users.noreply.github.com>
2024-02-21 00:20:31 -0800

    researched answer, passes tests

--- LeetCodeSolutions/ReorderRoutesToLeadToZero/ReorderRoutesToLeadToZero.cs
+++ LeetCodeSolutions/ReorderRoutesToLeadToZero/ReorderRoutesToLeadToZero.cs
@@ -1,93 +1,34 @@
-using System.Dynamic;
-
 namespace LeetCodeSolutions;
 public class ReorderRoutesToLeadToZeroSolution
 {
+
+    int count = 0;
+    void dfs(int node, int parent, ref List<List<Tuple<int, int>>> adj)
+    {
+        foreach (Tuple<int, int> pair in adj[node])
+        {
+            if (pair.Item1 != parent)
+            {
+                count += pair.Item2;
+                dfs(pair.Item1, node, ref adj);
+            }
+        }
+    }
     public int MinReorder(int n, int[][] connections)
     {
-        Dictionary<int, HashSet<int>> ConnectionsDefault = new();
-        Dictionary<int, HashSet<int>> ConnectionsReversed = new();
-        foreach (int[] connection in connections)
+        List<List<Tuple<int, int>>> adj = new();
+        for (int i = 0; i < n; i++)
         {
-            if (ConnectionsDefault.ContainsKey(connection[0]))
-            {
-                ConnectionsDefault[connection[0]].Add(connection[1]);
-            }
-            else
-            {
-                ConnectionsDefault.Add(connection[0], new());
-                ConnectionsDefault[connection[0]].Add(connection[1]);
-            }
-
-            if (ConnectionsReversed.ContainsKey(connection[1]))
-            {
-                ConnectionsReversed[connection[1]].Add(connection[0]);
-            }
-            else
-            {
-                ConnectionsReversed.Add(connection[1], new());
-                ConnectionsReversed[connection[1]].Add(connection[0]);
-            }
+            adj.Add([]);
         }
 
-        Stack<int> Adjacent = new();
-        HashSet<int> visited = [0];
-        int flips = 0;
-        if (connections[0][0] == 0)
+        foreach (int[] connetion in connections)
         {
-            foreach (int connection in ConnectionsDefault[0])
-            {
-                Adjacent.Push(connection);
-            }
-        }
-        else
-        {
-            foreach (int connection in ConnectionsReversed[0])
-            {
-                Adjacent.Push(connection);
-            }
+            adj[connetion[0]].Add(new Tuple<int, int>(connetion[1], 1));
+            adj[connetion[1]].Add(new Tuple<int, int>(connetion[0], 0));
         }
 
-
-        while (Adjacent.Count > 0 && visited.Count < n)
-        {
-            int currentNode = Adjacent.Pop();
-            visited.Add(currentNode);
-
-            if (ConnectionsDefault.ContainsKey(currentNode))
-            {
-
-                foreach (int connection in ConnectionsDefault[currentNode])
-                {
-                    if (!visited.Contains(connection))
-                    {
-                        Adjacent.Push(connection);
-                    }
-                }
-            }
-            else if (ConnectionsReversed.ContainsKey(currentNode))
-            {
-                flips++;
-                foreach (int connection in ConnectionsReversed[currentNode])
-                {
-                    if (!visited.Contains(connection))
-                    {
-                        Adjacent.Push(connection);
-                    }
-                }
-
-            }
-            else
-            {
-                throw new Exception("hash sets not built properly");
-            }
-
-
-
-        }
-
-
-
-        return flips;
+        dfs(0, -1, ref adj);
+        return count;
     }
 }
