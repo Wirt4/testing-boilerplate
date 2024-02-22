8187f7f928135ea011b51fc016267b149e3c1a3c
Wirt S <34081557+Wirt4@users.noreply.github.com>
2024-02-21 18:44:04 -0800

    move functionality to class

--- LeetCodeSolutions/EvaluateDivision/EvaluateDivision.cs
+++ LeetCodeSolutions/EvaluateDivision/EvaluateDivision.cs
@@ -8,50 +8,89 @@ public class EvaluateDivisionSolution
         public string Next;
     }
 
+    private class QueryBox
+    {
+        private Dictionary<string, List<Connection>> table = [];
+        public QueryBox(IList<IList<string>> equations, double[] values)
+        {
+            for (int i = 0; i < equations.Count; i++)
+            {
+                if (!table.ContainsKey(equations[i][0]))
+                {
+                    table.Add(equations[i][0], []);
+                }
 
+                Connection connection1 = new()
+                {
+                    Cost = values[i],
+                    Next = equations[i][1]
+                };
+
+                table[equations[i][0]].Add(connection1);
+
+                if (!table.ContainsKey(equations[i][1]))
+                {
+                    table.Add(equations[i][1], []);
+                }
+
+                Connection connection2 = new()
+                {
+                    Cost = Math.Pow(values[i], -1),
+                    Next = equations[i][0]
+                };
+
+                table[equations[i][1]].Add(connection2);
+            }
+        }
+
+        public bool Contains(string key)
+        {
+            return table.ContainsKey(key);
+        }
+
+        public double FindPath(string n1, string n2)
+        {
+            return Find(n1, n2, 1.0, []);
+        }
+
+        private double Find(string q1, string q2, double cost, HashSet<string> visited)
+        {
+            if (table.ContainsKey(q1) && table.ContainsKey(q2) && !visited.Contains(q1) && !visited.Contains(q2))
+            {
+                if (q1 == q2)
+                {
+                    return cost;
+                }
+
+                visited.Add(q1);
+
+                for (int i = 0; i < table[q1].Count; i++)
+                {
+                    double path = Find(table[q1][i].Next, q2, table[q1][i].Cost * cost, visited);
+                    if (path > 0)
+                    {
+                        return path;
+                    }
+                }
+            }
+
+            return -1;
+        }
+    }
     public double[] CalcEquation(IList<IList<string>> equations, double[] values, IList<IList<string>> queries)
     {
         List<double> answers = [];
-        Dictionary<string, List<Connection>> table = [];
-
-        for (int i = 0; i < equations.Count; i++)
-        {
-            if (!table.ContainsKey(equations[i][0]))
-            {
-                table.Add(equations[i][0], []);
-            }
-
-            Connection connection1 = new()
-            {
-                Cost = values[i],
-                Next = equations[i][1]
-            };
-
-            table[equations[i][0]].Add(connection1);
-
-            if (!table.ContainsKey(equations[i][1]))
-            {
-                table.Add(equations[i][1], []);
-            }
-
-            Connection connection2 = new()
-            {
-                Cost = Math.Pow(values[i], -1),
-                Next = equations[i][0]
-            };
-
-            table[equations[i][1]].Add(connection2);
-        }
+        QueryBox qBox = new QueryBox(equations, values);
 
         foreach (IList<string> query in queries)
         {
-            if (!table.ContainsKey(query[0]) || !table.ContainsKey(query[1]))
+            if (!qBox.Contains(query[0]) || !qBox.Contains(query[1]))
             {
                 answers.Add(-1);
                 continue;
             }
 
-            double path = FindPath(query[0], query[1], 1.0, table, []);
+            double path = qBox.FindPath(query[0], query[1]);
 
             if (path > 0)
             {
@@ -59,34 +98,10 @@ public class EvaluateDivisionSolution
             }
             else
             {
-                answers.Add(path * FindPath(query[1], query[0], 1.0, table, []));
+                answers.Add(path * qBox.FindPath(query[1], query[0]));
             }
         }
 
         return answers.ToArray();
-    }
-
-    private double FindPath(string q1, string q2, double cost, Dictionary<string, List<Connection>> table, HashSet<string> visited)
-    {
-        if (table.ContainsKey(q1) && table.ContainsKey(q2) && !visited.Contains(q1) && !visited.Contains(q2))
-        {
-            if (q1 == q2)
-            {
-                return cost;
-            }
-
-            visited.Add(q1);
-
-            for (int i = 0; i < table[q1].Count; i++)
-            {
-                double path = FindPath(table[q1][i].Next, q2, table[q1][i].Cost * cost, table, visited);
-                if (path > 0)
-                {
-                    return path;
-                }
-            }
-        }
-
-        return -1;
     }
 }
