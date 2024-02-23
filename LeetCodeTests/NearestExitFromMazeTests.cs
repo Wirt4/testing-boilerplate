bcc2994ce91201936fda671a98297c5de5b0ce36
Wirt S <34081557+Wirt4@users.noreply.github.com>
2024-02-23 15:09:12 -0800

    created files

---
+++ LeetCodeTests/NearestExitFromMazeTests.cs
@@ -0,0 +1,11 @@
+namespace Tests;
+using LeetCodeSolutions;
+ public class NearestExitFromMazeTests {
+  private NearestExitFromMazeSolution _solution;
+  public NearestExitFromMazeTests (){
+      _solution = new();
+  }
+  [Fact]
+  public void Test1(){
+  }
+}
