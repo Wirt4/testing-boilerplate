1e82340c8332dc4c401f7c38a912ee0058422a8e
Wirt S <34081557+Wirt4@users.noreply.github.com>
2024-02-23 19:40:24 -0800

    added LC test 3

--- LeetCodeTests/NearestExitFromMazeTests.cs
+++ LeetCodeTests/NearestExitFromMazeTests.cs
@@ -26,4 +26,14 @@ public class NearestExitFromMazeTests
     int expected = 2;
     Assert.Equal(expected, answer);
   }
+
+  [Fact]
+  public void LCExample3()
+  {
+    char[][] maze = [['.', '+']];
+    int[] entrance = [0, 0];
+    int answer = _solution.NearestExit(maze, entrance);
+    int expected = -1;
+    Assert.Equal(expected, answer);
+  }
 }
