79d8c3e7ce7b8b7534cf5c01c2fa2350c11b26e8
Wirt S <34081557+Wirt4@users.noreply.github.com>
2024-02-23 21:02:58 -0800

    first failing test

--- LeetCodeSolutions/RottingOranges/RottingOranges.cs
+++ LeetCodeSolutions/RottingOranges/RottingOranges.cs
@@ -1,2 +1,8 @@
 namespace LeetCodeSolutions;
-public class RottingOrangesSolution {}
+public class RottingOrangesSolution
+{
+    public int OrangesRotting(int[][] grid)
+    {
+        return -1;
+    }
+}
--- LeetCodeTests/RottingOrangesTests.cs
+++ LeetCodeTests/RottingOrangesTests.cs
@@ -1,11 +1,18 @@
 namespace Tests;
 using LeetCodeSolutions;
- public class RottingOrangesTests {
+public class RottingOrangesTests
+{
   private RottingOrangesSolution _solution;
-  public RottingOrangesTests (){
-      _solution = new();
+  public RottingOrangesTests()
+  {
+    _solution = new();
   }
   [Fact]
-  public void Test1(){
+  public void LCExample1()
+  {
+    int[][] grid = [[2, 1, 1], [1, 1, 0], [0, 1, 1]];
+    int desiredOutput = 4;
+    int output = _solution.OrangesRotting(grid);
+    Assert.Equal(desiredOutput, output);
   }
 }
