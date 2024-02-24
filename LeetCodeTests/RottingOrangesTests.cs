59706b5636fb51e723d7236d963b32afb8764066
Wirt S <34081557+Wirt4@users.noreply.github.com>
2024-02-23 21:00:26 -0800

    initialized with README

---
+++ LeetCodeTests/RottingOrangesTests.cs
@@ -0,0 +1,11 @@
+namespace Tests;
+using LeetCodeSolutions;
+ public class RottingOrangesTests {
+  private RottingOrangesSolution _solution;
+  public RottingOrangesTests (){
+      _solution = new();
+  }
+  [Fact]
+  public void Test1(){
+  }
+}
