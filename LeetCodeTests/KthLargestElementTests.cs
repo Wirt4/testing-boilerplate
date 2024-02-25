92bab8be8ac0b2d340f64abd7fba87a3feba36f0
Wirt S <34081557+Wirt4@users.noreply.github.com>
2024-02-25 11:58:11 -0800

    red

--- LeetCodeTests/KthLargestElementTests.cs
+++ LeetCodeTests/KthLargestElementTests.cs
@@ -17,4 +17,15 @@ public class KthLargestElementTests
     int actual = _solution.FindKthLargest(nums, k);
     Assert.Equal(desired, actual);
   }
+
+  [Fact]
+  public void Example2()
+  {
+    int[] nums = [3, 2, 3, 1, 2, 4, 5, 5, 6];
+    int k = 4;
+    int desired = 4;
+
+    int actual = _solution.FindKthLargest(nums, k);
+    Assert.Equal(desired, actual);
+  }
 }
