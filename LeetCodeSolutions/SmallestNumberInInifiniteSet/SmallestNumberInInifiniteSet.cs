a2cafbddc31e095e7f91080ddc4130e46a89b0df
Wirt S <34081557+Wirt4@users.noreply.github.com>
2024-02-25 22:31:55 -0800

    finite solution

--- LeetCodeSolutions/SmallestNumberInInifiniteSet/SmallestNumberInInifiniteSet.cs
+++ LeetCodeSolutions/SmallestNumberInInifiniteSet/SmallestNumberInInifiniteSet.cs
@@ -1,19 +1,26 @@
+
+
 namespace LeetCodeSolutions;
 public class SmallestInfiniteSet
 {
+    SortedSet<int> set;
 
     public SmallestInfiniteSet()
     {
-
+        int[] n = Enumerable.Range(1, 1000).ToArray();
+        set = new SortedSet<int>(n);
     }
 
     public int PopSmallest()
     {
-        return -1;
+        int num = set.First();
+        set.Remove(num);
+        return num;
     }
 
     public void AddBack(int num)
     {
-
+        set.Add(num);
     }
 }
+
