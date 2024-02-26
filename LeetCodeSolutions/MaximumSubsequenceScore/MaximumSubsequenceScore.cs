4c2a78f6a4aa6dd1d790d2b25c799488e35356c6
Wirt S <34081557+Wirt4@users.noreply.github.com>
2024-02-25 22:55:39 -0800

    added pissant test

--- LeetCodeSolutions/MaximumSubsequenceScore/MaximumSubsequenceScore.cs
+++ LeetCodeSolutions/MaximumSubsequenceScore/MaximumSubsequenceScore.cs
@@ -1,2 +1,8 @@
 namespace LeetCodeSolutions;
-public class MaximumSubsequenceScoreSolution {}
+public class MaximumSubsequenceScoreSolution
+{
+    public long MaxScore(int[] nums1, int[] nums2, int k)
+    {
+        return -1;
+    }
+}
--- LeetCodeTests/MaximumSubsequenceScoreTests.cs
+++ LeetCodeTests/MaximumSubsequenceScoreTests.cs
@@ -1,11 +1,21 @@
 namespace Tests;
 using LeetCodeSolutions;
- public class MaximumSubsequenceScoreTests {
+public class MaximumSubsequenceScoreTests
+{
   private MaximumSubsequenceScoreSolution _solution;
-  public MaximumSubsequenceScoreTests (){
-      _solution = new();
+  public MaximumSubsequenceScoreTests()
+  {
+    _solution = new();
   }
   [Fact]
-  public void Test1(){
+  public void MinimumValuesCase()
+  {
+    int[] nums1 = [0];
+    int[] nums2 = [0];
+    int k = 1;
+    int expectedAnswer = 0;
+
+    long actualAnswer = _solution.MaxScore(nums1, nums2, k);
+    Assert.Equal(expectedAnswer, actualAnswer);
   }
 }
