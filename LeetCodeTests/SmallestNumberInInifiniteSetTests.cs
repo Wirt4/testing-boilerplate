bf87013402ff1dbf8eb8ed368dbc922955d438a1
Wirt S <34081557+Wirt4@users.noreply.github.com>
2024-02-25 20:35:00 -0800

    README

---
+++ LeetCodeTests/SmallestNumberInInifiniteSetTests.cs
@@ -0,0 +1,11 @@
+namespace Tests;
+using LeetCodeSolutions;
+ public class SmallestNumberInInifiniteSetTests {
+  private SmallestNumberInInifiniteSetSolution _solution;
+  public SmallestNumberInInifiniteSetTests (){
+      _solution = new();
+  }
+  [Fact]
+  public void Test1(){
+  }
+}
