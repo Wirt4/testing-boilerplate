fd7b28c70888e0ff0eb8fc38d510facf6cb78c75
Wirt S <34081557+Wirt4@users.noreply.github.com>
2024-02-27 13:27:30 -0800

    reveiw changes

--- LeetCodeSolutions/MaximumSubsequenceScore/MaximumSubsequenceScore.cs
+++ LeetCodeSolutions/MaximumSubsequenceScore/MaximumSubsequenceScore.cs
@@ -2,7 +2,7 @@ namespace LeetCodeSolutions;
 public class MaximumSubsequenceScoreSolution
 {
 
-    private static (long, int)[] NumsDescendingByMultiplier(int[] nums1, int[] nums2)
+    private static List<(long, int)> NumsDescendingByMultiplier(int[] nums1, int[] nums2)
     {
         int size = nums1.Length;
         List<(long, int)> PairList = new(size);
@@ -12,29 +12,32 @@ public class MaximumSubsequenceScoreSolution
             PairList.Add((nums1[i], nums2[i]));
         }
 
-        return [.. PairList.OrderByDescending(x => x.Item2)];
+        return PairList.OrderByDescending(x => x.Item2).ToList();
     }
 
-    private class PriorityQueueWrapper((long, int)[] sorted, int capacity)
+    private class PriorityQueueWrapper(List<(long, int)> sorted, int capacity)
     {
         private PriorityQueue<long, long> _minAdders = new();
         private long _sum = 0;
         private long _maxValue = 0;
-        private int _capacity = capacity;
-        private (long, int)[] _sortedByMultiplier = sorted;
+        private readonly int _capacity = capacity;
+        private List<(long, int)> _sortedByMultiplier = sorted;
 
         public long MaxProduct => _maxValue;
 
         public void Traverse()
         {
-            for (int i = 0; i < _sortedByMultiplier.Length; i++)
+            for (int i = 0; i < _sortedByMultiplier.Count; i++)
             {
                 (long, int) current = _sortedByMultiplier[i];
                 _sum += current.Item1;
 
                 if (i >= _capacity - 1)
                 {
-                    _sum -= i >= _capacity ? _minAdders.Dequeue() : 0;
+                    if (i >= _capacity)
+                    {
+                        _sum -= _minAdders.Dequeue();
+                    }
                     _maxValue = Math.Max(_maxValue, _sum * current.Item2);
                 }
 
@@ -45,7 +48,7 @@ public class MaximumSubsequenceScoreSolution
 
     public long MaxScore(int[] nums1, int[] nums2, int k)
     {
-        (long, int)[] sortedByMultiplier = NumsDescendingByMultiplier(nums1, nums2);
+        List<(long, int)> sortedByMultiplier = NumsDescendingByMultiplier(nums1, nums2);
         PriorityQueueWrapper wrapper = new(sortedByMultiplier, k);
         wrapper.Traverse();
         return wrapper.MaxProduct;
