f1fdc0e5f297eed084dae3977d2d5ffec5612336
Wirt S <34081557+Wirt4@users.noreply.github.com>
2024-02-26 20:01:46 -0800

    passes tests based on verbal description of algorithm

--- LeetCodeSolutions/MaximumSubsequenceScore/MaximumSubsequenceScore.cs
+++ LeetCodeSolutions/MaximumSubsequenceScore/MaximumSubsequenceScore.cs
@@ -1,8 +1,58 @@
+using System.Reflection.Metadata.Ecma335;
+
 namespace LeetCodeSolutions;
 public class MaximumSubsequenceScoreSolution
 {
     public long MaxScore(int[] nums1, int[] nums2, int k)
     {
-        return 0;
+
+        PriorityQueue<int[], int> pQueue = new();
+        for (int i = 0; i < nums1.Length; i++)
+        {
+            pQueue.Enqueue([nums2[i], nums1[i]], nums2[i]);
+        }
+
+        int[][] allPairs = new int[nums1.Length][];
+
+        int j = 0;
+
+        while (pQueue.Count > 0)
+        {
+            allPairs[j] = pQueue.Dequeue();
+            j++;
+        }
+
+        int sum = 0;
+
+        for (int m = 0; m < k; m++)
+        {
+            sum += allPairs[m][1];
+            pQueue.Enqueue(allPairs[m], allPairs[m][0]);
+        }
+
+        int max = sum * pQueue.Peek()[0];
+
+        for (int p = k; p < nums1.Length; p++)
+        {
+            int[] removedPair = pQueue.Dequeue();
+            sum -= removedPair[1];
+
+            sum += allPairs[p][1];
+
+            int currentMax = sum * pQueue.Peek()[0];
+
+            if (currentMax > max)
+            {
+                max = currentMax;
+            }
+
+
+        }
+
+        // now there are k pairs in the priority queue
+
+
+        return max;
+
     }
 }
