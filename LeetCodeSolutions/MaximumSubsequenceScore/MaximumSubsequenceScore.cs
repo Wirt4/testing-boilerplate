64502c4a8474b2af69fdc8bd8103b1294761ebfd
Wirt S <34081557+Wirt4@users.noreply.github.com>
2024-02-26 20:40:02 -0800

    start migrating some functionality to a class

--- LeetCodeSolutions/MaximumSubsequenceScore/MaximumSubsequenceScore.cs
+++ LeetCodeSolutions/MaximumSubsequenceScore/MaximumSubsequenceScore.cs
@@ -1,12 +1,38 @@
 namespace LeetCodeSolutions;
 public class MaximumSubsequenceScoreSolution
 {
-    private int[][] getSortedPairs(int[] nums1, int[] nums2)
+
+    private class PriorityQueueWrapper
     {
-        PriorityQueue<int[], int> pQueue = new();
+        private PriorityQueue<int[], int> _priorityQueue;
+        public PriorityQueueWrapper()
+        {
+            _priorityQueue = new();
+        }
+
+        public void Enqueue(int[] pair)
+        {
+            _priorityQueue.Enqueue(pair, pair[0]);
+        }
+
+        public int[] Dequeue()
+        {
+            return _priorityQueue.Dequeue();
+        }
+
+        public int Count => _priorityQueue.Count;
+
+        public int[] Peek()
+        {
+            return _priorityQueue.Peek();
+        }
+    }
+    private int[][] GetSortedPairs(int[] nums1, int[] nums2)
+    {
+        PriorityQueueWrapper pQueue = new();
         for (int i = 0; i < nums1.Length; i++)
         {
-            pQueue.Enqueue([nums2[i], nums1[i]], nums2[i]);
+            pQueue.Enqueue([nums2[i], nums1[i]]);
         }
 
         int[][] allPairs = new int[nums1.Length][];
@@ -22,32 +48,26 @@ public class MaximumSubsequenceScoreSolution
     }
     public long MaxScore(int[] nums1, int[] nums2, int k)
     {
-        int[][] allPairs = getSortedPairs(nums1, nums2);
-        PriorityQueue<int[], int> pQueue = new();
+        int[][] allPairs = GetSortedPairs(nums1, nums2);
+        PriorityQueueWrapper pQueue = new();
         int sum = 0;
 
         for (int m = 0; m < k; m++)
         {
             sum += allPairs[m][1];
-            pQueue.Enqueue(allPairs[m], allPairs[m][0]);
+            pQueue.Enqueue(allPairs[m]);
         }
 
         int max = sum * pQueue.Peek()[0];
 
         for (int p = k; p < nums1.Length; p++)
         {
-            int[] removedPair = pQueue.Dequeue();
-            sum -= removedPair[1];
+            sum -= pQueue.Dequeue()[1];
             sum += allPairs[p][1];
-            pQueue.Enqueue(allPairs[p], allPairs[p][0]);
+            pQueue.Enqueue(allPairs[p]);
             int currentMax = sum * pQueue.Peek()[0];
-
-            if (currentMax > max)
-            {
-                max = currentMax;
-            }
+            max = currentMax > max ? currentMax : max;
         }
         return max;
-
     }
 }
