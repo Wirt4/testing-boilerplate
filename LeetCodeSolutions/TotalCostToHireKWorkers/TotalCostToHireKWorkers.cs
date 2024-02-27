057f81b36800815e189d5c38cd43f96a4c2a48b5
Wirt S <34081557+Wirt4@users.noreply.github.com>
2024-02-26 23:28:23 -0800

    added failing test

--- LeetCodeSolutions/TotalCostToHireKWorkers/TotalCostToHireKWorkers.cs
+++ LeetCodeSolutions/TotalCostToHireKWorkers/TotalCostToHireKWorkers.cs
@@ -1,2 +1,8 @@
 namespace LeetCodeSolutions;
-public class TotalCostToHireKWorkersSolution {}
+public class TotalCostToHireKWorkersSolution
+{
+    public long TotalCost(int[] costs, int hiringQuota, int selectionRange)
+    {
+        return -1;
+    }
+}
--- LeetCodeTests/TotalCostToHireKWorkersTests.cs
+++ LeetCodeTests/TotalCostToHireKWorkersTests.cs
@@ -1,11 +1,22 @@
 namespace Tests;
 using LeetCodeSolutions;
- public class TotalCostToHireKWorkersTests {
+public class TotalCostToHireKWorkersTests
+{
   private TotalCostToHireKWorkersSolution _solution;
-  public TotalCostToHireKWorkersTests (){
-      _solution = new();
+  public TotalCostToHireKWorkersTests()
+  {
+    _solution = new();
   }
   [Fact]
-  public void Test1(){
+  public void MinCase1()
+  {
+    int[] costs = [1, 2];
+    int quota = 1;
+    int range = 1;
+
+    long desired = 1;
+    long actual = _solution.TotalCost(costs, quota, range);
+    Assert.Equal(desired, actual);
+
   }
 }
