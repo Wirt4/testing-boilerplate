1a289f6bc34b649ca75cbdb011598cd885aa978a
Wirt S <34081557+Wirt4@users.noreply.github.com>
2024-02-26 23:16:39 -0800

    use wrapper class to arrange code in chunks of less than 20 lines each

--- LeetCodeSolutions/MaximumSubsequenceScore/MaximumSubsequenceScore.cs
+++ LeetCodeSolutions/MaximumSubsequenceScore/MaximumSubsequenceScore.cs
@@ -1,48 +1,54 @@
 namespace LeetCodeSolutions;
 public class MaximumSubsequenceScoreSolution
 {
-    private class PairOfOperands
-    {
-        public int adder;
-        public int multiplier;
-    }
-    private PairOfOperands[] NumsDescendingByMultiplier(int[] nums1, int[] nums2)
+
+    private static (long, int)[] NumsDescendingByMultiplier(int[] nums1, int[] nums2)
     {
         int size = nums1.Length;
-        List<PairOfOperands> PairList = new(size);
+        List<(long, int)> PairList = new(size);
 
         for (int i = 0; i < size; i++)
         {
-            PairList.Add(new()
-            {
-                adder = nums1[i],
-                multiplier = nums2[i]
-            });
+            PairList.Add((nums1[i], nums2[i]));
         }
 
-        return PairList.OrderByDescending(x => x.multiplier).ToArray();
+        return [.. PairList.OrderByDescending(x => x.Item2)];
     }
+
+    private class PriorityQueueWrapper((long, int)[] sorted, int capacity)
+    {
+        private PriorityQueue<long, long> _minAdders = new();
+        private long _sum = 0;
+        private long _maxValue = 0;
+        private int _capacity = capacity;
+
+        private (long, int)[] _sortedByMultiplier = sorted;
+
+        public long MaxProduct => _maxValue;
+
+        public void Traverse()
+        {
+            for (int i = 0; i < _sortedByMultiplier.Length; i++)
+            {
+                (long, int) current = _sortedByMultiplier[i];
+                _sum += current.Item1;
+
+                if (i >= _capacity - 1)
+                {
+                    _sum -= i >= _capacity ? _minAdders.Dequeue() : 0;
+                    _maxValue = Math.Max(_maxValue, _sum * current.Item2);
+                }
+
+                _minAdders.Enqueue(current.Item1, current.Item1);
+            }
+        }
+    }
+
     public long MaxScore(int[] nums1, int[] nums2, int k)
     {
-        PairOfOperands[] sortedByMultiplier = NumsDescendingByMultiplier(nums1, nums2);
-        PriorityQueue<PairOfOperands, int> minAdders = new();
-        long maxSum = 0;
-        long result = 0;
-
-        for (int i = 0; i < sortedByMultiplier.Length; i++)
-        {
-            PairOfOperands current = sortedByMultiplier[i];
-            maxSum += current.adder;
-
-            if (i >= k - 1)
-            {
-                maxSum -= i >= k ? minAdders.Dequeue().adder : 0;
-                result = Math.Max(result, maxSum * current.multiplier);
-            }
-
-            minAdders.Enqueue(current, current.adder);
-        }
-
-        return result;
+        (long, int)[] sortedByMultiplier = NumsDescendingByMultiplier(nums1, nums2);
+        PriorityQueueWrapper wrapper = new(sortedByMultiplier, k);
+        wrapper.Traverse();
+        return wrapper.MaxProduct;
     }
 }
