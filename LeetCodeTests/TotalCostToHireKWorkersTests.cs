7e24a2ee0d09de48528ee468260237a10e4e5334
Wirt S <34081557+Wirt4@users.noreply.github.com>
2024-02-26 23:21:43 -0800

    initialize with README

---
+++ LeetCodeTests/TotalCostToHireKWorkersTests.cs
@@ -0,0 +1,11 @@
+namespace Tests;
+using LeetCodeSolutions;
+ public class TotalCostToHireKWorkersTests {
+  private TotalCostToHireKWorkersSolution _solution;
+  public TotalCostToHireKWorkersTests (){
+      _solution = new();
+  }
+  [Fact]
+  public void Test1(){
+  }
+}
