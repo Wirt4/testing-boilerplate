d0aa9cd5be7b669fdbdbb8d0f8bdfdd324dd4e85
Wirt S <34081557+Wirt4@users.noreply.github.com>
2024-02-26 23:31:43 -0800

    red

--- LeetCodeTests/TotalCostToHireKWorkersTests.cs
+++ LeetCodeTests/TotalCostToHireKWorkersTests.cs
@@ -17,6 +17,17 @@ public class TotalCostToHireKWorkersTests
     long desired = 1;
     long actual = _solution.TotalCost(costs, quota, range);
     Assert.Equal(desired, actual);
+  }
 
+  [Fact]
+  public void LeetCodeExample1()
+  {
+    int[] costs = [17, 12, 10, 2, 7, 2, 11, 20, 8];
+    int quota = 3;
+    int range = 4;
+
+    long desired = 11;
+    long actual = _solution.TotalCost(costs, quota, range);
+    Assert.Equal(desired, actual);
   }
 }
