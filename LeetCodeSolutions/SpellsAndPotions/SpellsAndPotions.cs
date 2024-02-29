5cbe0b38f2677b10a3a7168d5bf5175584c88d79
Wirt S <34081557+Wirt4@users.noreply.github.com>
2024-02-29 15:41:24 -0800

    failing test

--- LeetCodeSolutions/SpellsAndPotions/SpellsAndPotions.cs
+++ LeetCodeSolutions/SpellsAndPotions/SpellsAndPotions.cs
@@ -1,2 +1,8 @@
 namespace LeetCodeSolutions;
-public class SpellsAndPotionsSolution {}
+public class SpellsAndPotionsSolution
+{
+    public int[] SuccessfulPairs(int[] spells, int[] potions, long success)
+    {
+        return [];
+    }
+}
--- LeetCodeTests/SpellsAndPotionsTests.cs
+++ LeetCodeTests/SpellsAndPotionsTests.cs
@@ -1,11 +1,18 @@
 namespace Tests;
 using LeetCodeSolutions;
- public class SpellsAndPotionsTests {
+public class SpellsAndPotionsTests
+{
   private SpellsAndPotionsSolution _solution;
-  public SpellsAndPotionsTests (){
-      _solution = new();
+  public SpellsAndPotionsTests()
+  {
+    _solution = new();
   }
   [Fact]
-  public void Test1(){
+  public void ArrayOfOneCase1()
+  {
+    int[] spells = [2];
+    int[] potions = [2];
+    long success = 3;
+    Assert.Equal([1], _solution.SuccessfulPairs(spells, potions, success));
   }
 }
