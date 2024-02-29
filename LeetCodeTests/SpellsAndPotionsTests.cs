49f495c5df71c5804feb47d57faf043f9b3ede2b
Wirt S <34081557+Wirt4@users.noreply.github.com>
2024-02-29 15:36:49 -0800

    initaite with README

---
+++ LeetCodeTests/SpellsAndPotionsTests.cs
@@ -0,0 +1,11 @@
+namespace Tests;
+using LeetCodeSolutions;
+ public class SpellsAndPotionsTests {
+  private SpellsAndPotionsSolution _solution;
+  public SpellsAndPotionsTests (){
+      _solution = new();
+  }
+  [Fact]
+  public void Test1(){
+  }
+}
