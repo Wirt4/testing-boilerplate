84fde2cb9e488fdf82ceefd622d88209381027ff
Wirt S <34081557+Wirt4@users.noreply.github.com>
2024-02-29 19:39:39 -0800

    more tinkering

--- LeetCodeSolutions/FindPeakElement/FindPeakElement.cs
+++ LeetCodeSolutions/FindPeakElement/FindPeakElement.cs
@@ -11,38 +11,43 @@ public class FindPeakElementSolution
 
     private bool GreaterThanLeft(int ndx, ref int[] arr)
     {
+        if (ndx == 0)
+        {
+            return true;
+        }
+
         return arr[ndx] > arr[ndx - 1];
     }
 
     private bool GreaterThanRight(int ndx, ref int[] arr)
     {
+        if (ndx == arr.Length - 1)
+        {
+            return true;
+        }
         return arr[ndx] > arr[ndx + 1];
     }
     public int FindPeakElement(int[] nums)
     {
-        if (nums.Length == 1) { return 0; }
-
         Stack<IndexPair> stack = new();
         IndexPair cursor = new(0, nums.Length);
         stack.Push(cursor);
-        int mid = -1;
+        int mid;
         while (stack.Count > 0)
         {
             cursor = stack.Pop();
             if (cursor.start >= cursor.range) { continue; }
             mid = (cursor.start + cursor.range) / 2;
 
-
-            if (mid == 0 && GreaterThanRight(mid, ref nums)) { break; }
-
-            if (mid == nums.Length - 1 && GreaterThanLeft(mid, ref nums)) { break; }
-
-            if (GreaterThanLeft(mid, ref nums) && GreaterThanRight(mid, ref nums)) { break; }
+            if (GreaterThanLeft(mid, ref nums) && GreaterThanRight(mid, ref nums))
+            {
+                return mid;
+            }
 
             stack.Push(new(cursor.start, mid));
             stack.Push(new(cursor.start, mid));
         }
-        return mid;
 
+        return -1;
     }
 }
