f9f126d33091ee194f803086cd475d9d51abbea6
Wirt S <34081557+Wirt4@users.noreply.github.com>
2024-02-29 18:57:25 -0800

    green

--- LeetCodeSolutions/FindPeakElement/FindPeakElement.cs
+++ LeetCodeSolutions/FindPeakElement/FindPeakElement.cs
@@ -4,6 +4,6 @@ public class FindPeakElementSolution
     public int FindPeakElement(int[] nums)
     {
         if (nums.Length == 1) { return 0; }
-        return 1;
+        return nums[0] > nums[1] ? 0 : 1;
     }
 }
