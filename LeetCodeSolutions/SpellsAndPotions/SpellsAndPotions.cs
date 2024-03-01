1ccadf2c29d4d125316ef99cab91e969f482ee2b
Wirt S <34081557+Wirt4@users.noreply.github.com>
2024-02-29 16:36:28 -0800

    start migrating to an object

--- LeetCodeSolutions/SpellsAndPotions/SpellsAndPotions.cs
+++ LeetCodeSolutions/SpellsAndPotions/SpellsAndPotions.cs
@@ -1,58 +1,74 @@
 namespace LeetCodeSolutions;
 public class SpellsAndPotionsSolution
 {
-    private int Find(int spell, int[] potions, long success)
+
+
+    private class PotionsContainer
     {
-        //assumss potions is sorted
-        int i = 0;
-        int j = potions.Length;
-        while (i <= j)
+        private readonly int[] potions;
+        private readonly long success;
+        public PotionsContainer(int[] potions, long success)
         {
-            if (i == j)
-            {
-                return i;
-            }
-            int m = (i + j) / 2;
-            long query = spell * potions[m];
-            if (query == success)
-            {
-                return m;
-            }
-            if (query > success)
-            {
-                if (m == 0)
-                {
-                    return m;
-                }
-                long query2 = spell * potions[m - 1];
-                if (query2 < success)
-                {
-                    return m;
-                }
-                j = m - 1;
-            }
-            else
-            {
-                i = m + 1;
-            }
+            this.potions = potions;
+            Array.Sort(this.potions);
+            this.success = success;
         }
-        return -1;
-    }
+
+        private int Find(int spell, int[] potions, long success)
+        {
+            //assumss potions is sorted
+            int i = 0;
+            int j = potions.Length;
+            while (i <= j)
+            {
+                if (i == j)
+                {
+                    return i;
+                }
+                int m = (i + j) / 2;
+                long query = spell * potions[m];
+                if (query == success)
+                {
+                    return m;
+                }
+                if (query > success)
+                {
+                    if (m == 0)
+                    {
+                        return m;
+                    }
+                    long query2 = spell * potions[m - 1];
+                    if (query2 < success)
+                    {
+                        return m;
+                    }
+                    j = m - 1;
+                }
+                else
+                {
+                    i = m + 1;
+                }
+            }
+            return -1;
+        }
+
+        public int NumberOfMixes(int spell)
+        {
+            int index = Find(spell, potions, success);
+            if (index == -1)
+            {
+                return 0;
+            }
+            return potions.Length - index;
+        }
+    };
     public int[] SuccessfulPairs(int[] spells, int[] potions, long success)
     {
+        PotionsContainer continer = new(potions, success);
         int[] answer = new int[spells.Length];
-        Array.Sort(potions);
         for (int i = 0; i < spells.Length; i++)
         {
-            int matchIndex = Find(spells[i], potions, success);
-            if (matchIndex >= 0)
-            {
-                answer[i] = potions.Length - matchIndex;
-            }
-            else
-            {
-                answer[i] = 0;
-            }
+            answer[i] = continer.NumberOfMixes(spells[i]);
         }
 
         return answer;
