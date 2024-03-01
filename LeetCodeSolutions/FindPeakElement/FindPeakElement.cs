b25116513234a3b0264b7d9e723559eba38eb012
Wirt S <34081557+Wirt4@users.noreply.github.com>
2024-02-29 19:36:25 -0800

    remove duplicated and unnecessary work

--- LeetCodeSolutions/FindPeakElement/FindPeakElement.cs
+++ LeetCodeSolutions/FindPeakElement/FindPeakElement.cs
@@ -1,58 +1,48 @@
+using System.Security.AccessControl;
+
 namespace LeetCodeSolutions;
 public class FindPeakElementSolution
 {
-    private int BinarySearch(ref int[] nums, int i, int j)
+    private class IndexPair(int start, int range)
     {
-        if (i >= j)
-        {
-            return -1;
-        }
-        int m = (i + j) / 2;
-        if ((m == 0 && nums[m + 1] < nums[m]) || (m == j - 1 && nums[m - 1] < nums[m]) || (nums[m - 1] < nums[m] && nums[m + 1] < nums[m]))
-        {
-            return m;
-        }
-        int left = BinarySearch(ref nums, i, m);
-        if (left >= 0)
-        {
-            return left;
-        }
+        public int start = start;
+        public int range = range;
+    }
 
-        return BinarySearch(ref nums, m + 1, j);
+    private bool GreaterThanLeft(int ndx, ref int[] arr)
+    {
+        return arr[ndx] > arr[ndx - 1];
+    }
+
+    private bool GreaterThanRight(int ndx, ref int[] arr)
+    {
+        return arr[ndx] > arr[ndx + 1];
     }
     public int FindPeakElement(int[] nums)
     {
         if (nums.Length == 1) { return 0; }
 
-        //return BinarySearch(ref nums, 0, nums.Length);
-        Stack<(int start, int end)> stack = new();
-        stack.Push((0, nums.Length));
+        Stack<IndexPair> stack = new();
+        IndexPair cursor = new(0, nums.Length);
+        stack.Push(cursor);
+        int mid = -1;
         while (stack.Count > 0)
         {
-            (int start, int end) current = stack.Pop();
-            if (current.start >= current.end) { continue; }
-            int mid = (current.start + current.end) / 2;
-            if (mid == 0 && nums[mid + 1] < nums[mid])
-            {
-                return mid;
-            }
-            if (mid == nums.Length - 1 && nums[mid - 1] < nums[mid])
-            {
-                return mid;
-            }
-
-            if (nums[mid - 1] < nums[mid] && nums[mid + 1] < nums[mid])
-            {
-                return mid;
-            }
-            (int start, int end) left = (current.start, mid);
-            (int start, int end) right = (mid + 1, current.end);
-            stack.Push(left);
-            stack.Push(right);
+            cursor = stack.Pop();
+            if (cursor.start >= cursor.range) { continue; }
+            mid = (cursor.start + cursor.range) / 2;
 
 
+            if (mid == 0 && GreaterThanRight(mid, ref nums)) { break; }
+
+            if (mid == nums.Length - 1 && GreaterThanLeft(mid, ref nums)) { break; }
+
+            if (GreaterThanLeft(mid, ref nums) && GreaterThanRight(mid, ref nums)) { break; }
+
+            stack.Push(new(cursor.start, mid));
+            stack.Push(new(cursor.start, mid));
         }
-        return -1;
+        return mid;
 
     }
 }
