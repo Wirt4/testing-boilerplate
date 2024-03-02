4371a224097084efaae2565a8ebd25e32adad312
Wirt S <34081557+Wirt4@users.noreply.github.com>
2024-03-02 12:28:13 -0800

    some refactoring to cut down number of lines of code per function

--- LeetCodeSolutions/LetterCombinationOfPhoneNumber/LetterCombinationOfPhoneNumber.cs
+++ LeetCodeSolutions/LetterCombinationOfPhoneNumber/LetterCombinationOfPhoneNumber.cs
@@ -1,11 +1,34 @@
-using System.ComponentModel.Design.Serialization;
-using System.Dynamic;
-using System.Reflection.Metadata.Ecma335;
-using System.Security.Cryptography;
-
 namespace LeetCodeSolutions;
 public class LetterCombinationOfPhoneNumberSolution
 {
+
+    private readonly Dictionary<char, char[]> characterValuesToNumbers = new()
+    {
+        ['2'] = ['a', 'b', 'c'],
+        ['3'] = ['d', 'e', 'f'],
+        ['4'] = ['g', 'h', 'i'],
+        ['5'] = ['j', 'k', 'l'],
+        ['6'] = ['m', 'n', 'o'],
+        ['7'] = ['p', 'q', 'r', 's'],
+        ['8'] = ['t', 'u', 'v'],
+        ['9'] = ['w', 'x', 'y', 'z']
+    };
+
+    private void AddPermutations(char number, ref Queue<string> permutations)
+    {
+        foreach (char ch in characterValuesToNumbers[number])
+        {
+            permutations.Enqueue(ch.ToString());
+        }
+    }
+
+    private void AddConcactedPermutations(string exisingPermutation, char number, ref Queue<string> permutations)
+    {
+        foreach (char ch in characterValuesToNumbers[number])
+        {
+            permutations.Enqueue(exisingPermutation + ch.ToString());
+        }
+    }
     public IList<string> LetterCombinations(string digits)
     {
         if (digits.Length == 0)
@@ -13,47 +36,19 @@ public class LetterCombinationOfPhoneNumberSolution
             return [];
         }
 
-        Dictionary<char, char[]> numberKey = new()
-        {
-            ['2'] = ['a', 'b', 'c'],
-            ['3'] = ['d', 'e', 'f'],
-            ['4'] = ['g', 'h', 'i'],
-            ['5'] = ['j', 'k', 'l'],
-            ['6'] = ['m', 'n', 'o'],
-            ['7'] = ['p', 'q', 'r', 's'],
-            ['8'] = ['t', 'u', 'v'],
-            ['9'] = ['w', 'x', 'y', 'z']
-        };
-
-        Queue<string> perms = new();
-
-        foreach (char ch in numberKey[digits[0]])
-        {
-            perms.Enqueue(ch.ToString());
-        }
+        Queue<string> permutations = new();
+        AddPermutations(digits[0], ref permutations);
 
         for (int i = 1; i < digits.Length; i++)
         {
-            int count = perms.Count;
+            int count = permutations.Count;
             for (int j = 0; j < count; j++)
             {
-                string cur = perms.Dequeue();
-                foreach (char ch in numberKey[digits[i]])
-                {
-                    perms.Enqueue(cur + ch.ToString());
-                }
+                string currentPermutation = permutations.Dequeue();
+                AddConcactedPermutations(currentPermutation, digits[i], ref permutations);
             }
         }
 
-
-
-
-
-
-        return [.. perms];
+        return [.. permutations];
     }
-
-
-
-
 }
