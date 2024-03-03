11666bc1edc44ae2bebd1d63f98ca1c9baa72833
Wirt S <34081557+Wirt4@users.noreply.github.com>
2024-03-02 19:23:14 -0800

    passes test

--- LeetCodeSolutions/CombinationSumIII/CombinationSumIII.cs
+++ LeetCodeSolutions/CombinationSumIII/CombinationSumIII.cs
@@ -1,8 +1,41 @@
+using System.Globalization;
+
 namespace LeetCodeSolutions;
 public class CombinationSumIIISolution
 {
     public IList<IList<int>> CombinationSum3(int k, int n)
     {
-        return [[n]];
+
+        //create the first array
+        Queue<List<int>> queue = new();
+        IList<IList<int>> answer = [];
+        for (int digit = 1; digit <= 9; digit++)
+        {
+            queue.Enqueue(new(digit));
+            int count = queue.Count;
+            for (int i = 0; i < count; i++)
+            {
+                List<int> permutation = queue.Dequeue();
+                permutation.Add(digit);
+                int sum = permutation.Sum();
+                if (permutation.Count > k || sum > n)
+                {
+                    continue;
+                }
+                else if (permutation.Count == k && sum == n)
+                {
+                    answer.Add(permutation);
+                }
+                else
+                {
+                    queue.Enqueue(permutation);
+                }
+
+            }
+        }
+
+
+
+        return answer;
     }
 }
