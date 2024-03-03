6c60b12a552048f5809710ae3da04bc54798087e
Wirt S <34081557+Wirt4@users.noreply.github.com>
2024-03-02 20:57:01 -0800

    refactored to track via memoization

--- LeetCodeSolutions/DominoAndTrominoTiling/DominoAndTrominoTiling.cs
+++ LeetCodeSolutions/DominoAndTrominoTiling/DominoAndTrominoTiling.cs
@@ -3,19 +3,31 @@ public class DominoAndTrominoTilingSolution
 {
     public int NumTilings(int n)
     {
+        Dictionary<int, int> memos = new();
+        return NumTilingsMemoed(n, ref memos);
+    }
+    public int NumTilingsMemoed(int n, ref Dictionary<int, int> memos)
+    {
+        if (memos.TryGetValue(n, out int value))
+        {
+            return value;
+        }
         switch (n)
         {
             case 0:
+                memos.Add(0, 0);
                 return 0;
             case 1:
+                memos.Add(1, 1);
                 return 1;
             case 2:
+                memos.Add(2, 2);
                 return 2;
             case 3:
+                memos.Add(3, 5);
                 return 5;
             default:
-                return (NumTilings(n - 1) * 2) + NumTilings(n - 3);
+                return (NumTilingsMemoed(n - 1, ref memos) * 2) + NumTilingsMemoed(n - 3, ref memos);
         }
-
     }
 }
