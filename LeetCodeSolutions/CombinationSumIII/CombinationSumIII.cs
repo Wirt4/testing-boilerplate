1a4d25a0ef5c7889fb3949159b081c6cd2e0d0cb
Wirt S <34081557+Wirt4@users.noreply.github.com>
2024-03-02 19:28:47 -0800

    refactored tests

--- LeetCodeSolutions/CombinationSumIII/CombinationSumIII.cs
+++ LeetCodeSolutions/CombinationSumIII/CombinationSumIII.cs
@@ -30,7 +30,6 @@ public class CombinationSumIIISolution
                 {
                     queue.Enqueue(permutation);
                 }
-
             }
         }
 
--- LeetCodeTests/CombinationSumIIITests.cs
+++ LeetCodeTests/CombinationSumIIITests.cs
@@ -7,35 +7,48 @@ public class CombinationSumIIITests
   {
     _solution = new();
   }
+  private class Parameters
+  {
+    public int k;
+    public int n;
+  }
+  private void TestParams(IList<IList<int>> desired, Parameters parameters)
+  {
+    IList<IList<int>> actual = _solution.CombinationSum3(parameters.k, parameters.n);
+    Assert.Equal(desired, actual);
+  }
   [Fact]
   public void ArrayOfOne1()
   {
-    int k = 1;
-    int n = 1;
+    Parameters parameters = new()
+    {
+      k = 1,
+      n = 1
+    };
     IList<IList<int>> desired = [[1]];
-    IList<IList<int>> actual = _solution.CombinationSum3(k, n);
-
-    Assert.Equal(desired, actual);
+    TestParams(desired, parameters);
   }
 
   [Fact]
   public void ArrayOfOne2()
   {
-    int k = 1;
-    int n = 7;
+    Parameters parameters = new()
+    {
+      k = 1,
+      n = 7
+    };
     IList<IList<int>> desired = [[7]];
-    IList<IList<int>> actual = _solution.CombinationSum3(k, n);
-
-    Assert.Equal(desired, actual);
+    TestParams(desired, parameters);
   }
   [Fact]
   public void ArrayOfTwo1()
   {
-    int k = 2;
-    int n = 3;
+    Parameters parameters = new()
+    {
+      k = 2,
+      n = 3
+    };
     IList<IList<int>> desired = [[1, 2]];
-    IList<IList<int>> actual = _solution.CombinationSum3(k, n);
-
-    Assert.Equal(desired, actual);
+    TestParams(desired, parameters);
   }
 }
