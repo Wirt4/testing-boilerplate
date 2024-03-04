0d6f183aa41c8916cd9549282d5f4f9b3cf7fc85
Wirt S <34081557+Wirt4@users.noreply.github.com>
2024-03-03 19:46:15 -0800

    green

--- LeetCodeSolutions/UniquePaths/UniquePaths.cs
+++ LeetCodeSolutions/UniquePaths/UniquePaths.cs
@@ -1,8 +1,36 @@
+
+
 namespace LeetCodeSolutions;
 public class UniquePathsSolution
 {
     public int UniquePaths(int m, int n)
     {
-        return 1;
+        int UniquePaths = 0;
+        Queue<int[]> queue = new();
+        queue.Enqueue([1, 1]);
+        while (queue.Count > 0)
+        {
+            int c = queue.Count;
+            for (int i = 0; i < c; i++)
+            {
+                int[] current = queue.Dequeue();
+                if (m == current[0] && n == current[1])
+                {
+                    UniquePaths++;
+                }
+
+                if (current[0] < m)
+                {
+                    queue.Enqueue([current[0] + 1, current[1]]);
+                }
+
+                if (current[1] < n)
+                {
+                    queue.Enqueue([current[0], current[1] + 1]);
+                }
+            }
+        }
+
+        return UniquePaths;
     }
 }
