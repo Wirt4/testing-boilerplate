d911c2c77a2b555c361bec118e61512fbed67692
Wirt S <34081557+Wirt4@users.noreply.github.com>
2024-03-03 22:13:58 -0800

    recursive solution

--- LeetCodeSolutions/LongestCommonSubsequence/LongestCommonSubsequence.cs
+++ LeetCodeSolutions/LongestCommonSubsequence/LongestCommonSubsequence.cs
@@ -3,6 +3,14 @@ public class LongestCommonSubsequenceSolution
 {
     public int LongestCommonSubsequence(string text1, string text2)
     {
-        return -1;
+        if (text1.Length == 0 || text2.Length == 0)
+        {
+            return 0;
+        }
+        if (text1[0] == text2[0])
+        {
+            return 1 + LongestCommonSubsequence(text1.Substring(1), text2.Substring(1));
+        }
+        return Math.Max(LongestCommonSubsequence(text1, text2.Substring(1)), LongestCommonSubsequence(text1.Substring(1), text2));
     }
 }
--- LeetCodeTests/LongestCommonSubsequenceTests.cs
+++ LeetCodeTests/LongestCommonSubsequenceTests.cs
@@ -8,8 +8,24 @@ public class LongestCommonSubsequenceTests
     _solution = new();
   }
   [Fact]
-  public void TextLengthsOfOne()
+  public void TextLengthsOfOne1()
   {
     Assert.Equal(1, _solution.LongestCommonSubsequence("a", "a"));
+  }
+  [Fact]
+  public void TextLengthsOfOne2()
+  {
+    Assert.Equal(0, _solution.LongestCommonSubsequence("a", "b"));
+  }
+
+  [Fact]
+  public void TextLengthsOfTwo1()
+  {
+    Assert.Equal(2, _solution.LongestCommonSubsequence("fh", "fh"));
+  }
+  [Fact]
+  public void TextLengthsOfTwo2()
+  {
+    Assert.Equal(1, _solution.LongestCommonSubsequence("fh", "fg"));
   }
 }
