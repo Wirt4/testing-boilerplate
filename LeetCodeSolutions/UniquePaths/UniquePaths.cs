8821aedd9ac62a61f7fdc259285d1892056b769d
Wirt S <34081557+Wirt4@users.noreply.github.com>
2024-03-03 19:18:02 -0800

    first test

--- LeetCodeSolutions/UniquePaths/UniquePaths.cs
+++ LeetCodeSolutions/UniquePaths/UniquePaths.cs
@@ -1,2 +1,8 @@
 namespace LeetCodeSolutions;
-public class UniquePathsSolution {}
+public class UniquePathsSolution
+{
+    public int UniquePaths(int m, int n)
+    {
+        return -1;
+    }
+}
--- LeetCodeTests/UniquePathsTests.cs
+++ LeetCodeTests/UniquePathsTests.cs
@@ -1,11 +1,19 @@
 namespace Tests;
 using LeetCodeSolutions;
- public class UniquePathsTests {
+public class UniquePathsTests
+{
   private UniquePathsSolution _solution;
-  public UniquePathsTests (){
-      _solution = new();
+  public UniquePathsTests()
+  {
+    _solution = new();
   }
   [Fact]
-  public void Test1(){
+  public void GridOfOne()
+  {
+    int m = 1;
+    int n = 1;
+    int expected = 1;
+    int acutal = _solution.UniquePaths(m, n);
+    Assert.Equal(expected, acutal);
   }
 }
