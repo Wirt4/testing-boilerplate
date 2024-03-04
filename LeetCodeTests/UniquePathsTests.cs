747a3fc5cf14dc36427c546671eaf9140f8551de
Wirt S <34081557+Wirt4@users.noreply.github.com>
2024-03-03 19:15:02 -0800

    initialize with README

---
+++ LeetCodeTests/UniquePathsTests.cs
@@ -0,0 +1,11 @@
+namespace Tests;
+using LeetCodeSolutions;
+ public class UniquePathsTests {
+  private UniquePathsSolution _solution;
+  public UniquePathsTests (){
+      _solution = new();
+  }
+  [Fact]
+  public void Test1(){
+  }
+}
