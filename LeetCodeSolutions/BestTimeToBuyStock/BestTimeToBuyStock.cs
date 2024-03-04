51ac386331149d65dc5c8afe1565b5ffa99f7b57
Wirt S <34081557+Wirt4@users.noreply.github.com>
2024-03-04 00:01:13 -0800

    ready for review

--- LeetCodeSolutions/BestTimeToBuyStock/BestTimeToBuyStock.cs
+++ LeetCodeSolutions/BestTimeToBuyStock/BestTimeToBuyStock.cs
@@ -1,30 +1,16 @@
+
 namespace LeetCodeSolutions;
 public class BestTimeToBuyStockSolution
 {
     public int MaxProfit(int[] prices, int fee)
     {
-        return Helper(0, true, fee, ref prices);
-    }
-    private int Helper(int indx, bool buy, int fee, ref int[] prices)
-    {
-        if (indx == prices.Length)
+        int[] buys = new int[prices.Length + 1];
+        int[] sales = new int[prices.Length + 1];
+        for (int i = prices.Length - 1; i >= 0; i--)
         {
-            return 0;
+            buys[i] = Math.Max(buys[i + 1], sales[i + 1] - prices[i]);
+            sales[i] = Math.Max(sales[i + 1], buys[i + 1] + prices[i] - fee);
         }
-        // there are two options: to take action or move on
-        int noTransaction = Helper(indx + 1, buy, fee, ref prices);
-
-        int transaction;
-        if (buy)
-        {
-            transaction = Helper(indx + 1, !buy, fee, ref prices) - prices[indx];
-        }
-        else
-        {
-            transaction = prices[indx] - fee + Helper(indx + 1, !buy, fee, ref prices);
-        }
-
-        return Math.Max(noTransaction, transaction);
-
+        return buys[0];
     }
 }
