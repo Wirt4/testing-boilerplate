feda3493bbb9d7158c6b5f969d280d7af58313c2
Wirt S <34081557+Wirt4@users.noreply.github.com>
2024-03-04 19:47:48 -0800

    tested helper function 1

--- LeetCodeSolutions/EditDistance/EditDistance.cs
+++ LeetCodeSolutions/EditDistance/EditDistance.cs
@@ -1,8 +1,44 @@
 namespace LeetCodeSolutions;
 public class EditDistanceSolution
 {
+    public int[][] GenerateBlankTable(string word1, string word2)
+    {
+        int[][] table = new int[word2.Length + 1][];
+        for (int i = 0; i <= word2.Length; i++)
+        {
+            table[i] = new int[word1.Length + 1];
+        }
+
+        for (int i = 0; i <= word2.Length; i++)
+        {
+            table[i][0] = i;
+        }
+
+        for (int i = 0; i <= word1.Length; i++)
+        {
+            table[0][i] = i;
+        }
+        return table;
+    }
     public int MinDistance(string word1, string word2)
     {
-        return word1 == word2 ? 0 : 1;
+        // create a table
+        int[][] EditDistanceTable = new int[word1.Length + 1][];
+        for (int i = 0; i <= word1.Length; i++)
+        {
+            EditDistanceTable[i] = new int[word2.Length + 1];
+        }
+
+        //initialize the border values
+        for (int i = 0; i < EditDistanceTable.Length; i++)
+        {
+            EditDistanceTable[i][0] = i;
+        }
+
+        for (int i = 0; i < EditDistanceTable[0].Length; i++)
+        {
+            EditDistanceTable[0][i] = i;
+        }
+        return -1;
     }
 }
--- LeetCodeTests/EditDistanceTests.cs
+++ LeetCodeTests/EditDistanceTests.cs
@@ -49,4 +49,43 @@ public class EditDistanceTests
     };
     TestMinDistance(parameters, 3);
   }
+
+  [Fact]
+  public void TestBlankDistanceTable()
+  {
+    int[][] answer = [[0, 1], [1, 0]];
+    Assert.Equal(answer, _solution.GenerateBlankTable("a", "a"));
+  }
+
+  [Fact]
+  public void TestBlankDistanceTable2()
+  {
+    int[][] answer = [
+      [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
+      [1, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [2, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [3, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [4, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [5, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [6, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [7, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [8, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [9, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+    ];
+    Assert.Equal(answer, _solution.GenerateBlankTable("intention", "execution"));
+  }
+
+  [Fact]
+  public void TestBlankDistanceTableDifferentWordLengths()
+  {
+    int[][] answer = [
+      [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
+      [1, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [2, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [3, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [4, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+    ];
+    Assert.Equal(answer, _solution.GenerateBlankTable("intention", "damn"));
+  }
+
 }
