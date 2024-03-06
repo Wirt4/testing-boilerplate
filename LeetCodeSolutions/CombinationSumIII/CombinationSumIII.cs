556a269ca7c858994f54d9e228053d83c30bc9c1
Wirt S <34081557+Wirt4@users.noreply.github.com>
2024-03-06 13:57:54 -0800

    decompose functionality into smaller methods

--- LeetCodeSolutions/CombinationSumIII/CombinationSumIII.cs
+++ LeetCodeSolutions/CombinationSumIII/CombinationSumIII.cs
@@ -1,35 +1,75 @@
 namespace LeetCodeSolutions;
 public class CombinationSumIIISolution
 {
-    public IList<IList<int>> CombinationSum3(int k, int n)
+    private class CombinationParser
     {
-        Queue<List<int>> queue = new();
-        IList<IList<int>> answer = [];
-        int endRange = 9;
-        for (int digit = 1; digit <= endRange; digit++)
+        private Queue<List<int>> queue;
+        private IList<IList<int>> answer;
+        private int endRange;
+        private int startRange;
+        private int k;
+        private int n;
+        public CombinationParser(int k, int n)
+        {
+            this.k = k;
+            this.n = n;
+            queue = new();
+            answer = [];
+            endRange = 9;
+            startRange = 1;
+        }
+
+        public void AddToQueue(int digit)
         {
             queue.Enqueue([digit]);
-            int count = queue.Count;
-            for (int i = 0; i < count; i++)
+        }
+
+        public int CurrentQueueCount => queue.Count;
+        public int StartIndex => startRange;
+        public int EndIndex => endRange;
+        public IList<IList<int>> Answer => answer;
+
+        public void ProccessPermutation()
+        {
+            List<int> permutation = queue.Dequeue();
+            int sum = permutation.Sum();
+            if (sum == n && permutation.Count == k)
             {
-                List<int> permutation = queue.Dequeue();
-                int sum = permutation.Sum();
-                if (sum == n && permutation.Count == k)
-                {
-                    answer.Add(permutation);
-                    continue;
-                }
-                if (sum < n && permutation.Count < k)
-                {
-                    for (int j = permutation[^1] + 1; j <= endRange; j++)
-                    {
-                        List<int> permutationCopy = new(permutation);
-                        permutationCopy.Add(j);
-                        queue.Enqueue(permutationCopy);
-                    }
-                }
+                answer.Add(permutation);
+                return;
+            }
+
+            if (sum < n && permutation.Count < k)
+            {
+                EnqueueAdjacents(permutation);
             }
         }
-        return answer;
+
+        private void EnqueueAdjacents(List<int> permutation)
+        {
+            for (int j = permutation[^1] + 1; j <= endRange; j++)
+            {
+                List<int> permutationCopy = new(permutation);
+                permutationCopy.Add(j);
+                queue.Enqueue(permutationCopy);
+            }
+        }
+
+    }
+    public IList<IList<int>> CombinationSum3(int k, int n)
+    {
+        CombinationParser parser = new(k, n);
+
+        for (int digit = parser.StartIndex; digit <= parser.EndIndex; digit++)
+        {
+            parser.AddToQueue(digit);
+            int count = parser.CurrentQueueCount;
+            for (int i = 0; i < count; i++)
+            {
+                parser.ProccessPermutation();
+            }
+        }
+
+        return parser.Answer;
     }
 }
