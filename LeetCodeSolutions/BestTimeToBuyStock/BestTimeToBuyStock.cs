62762a305c49893af9afebffa38747aad3d18680
Wirt S <34081557+Wirt4@users.noreply.github.com>
2024-03-06 14:08:09 -0800

    made requested changes

--- LeetCodeSolutions/BestTimeToBuyStock/BestTimeToBuyStock.cs
+++ LeetCodeSolutions/BestTimeToBuyStock/BestTimeToBuyStock.cs
@@ -4,13 +4,17 @@ public class BestTimeToBuyStockSolution
 {
     public int MaxProfit(int[] prices, int fee)
     {
-        int[] buys = new int[prices.Length + 1];
-        int[] sales = new int[prices.Length + 1];
+        int arrLength = prices.Length + 1;
+        int[] buys = new int[arrLength];
+        int[] sales = new int[arrLength];
+
         for (int i = prices.Length - 1; i >= 0; i--)
         {
-            buys[i] = Math.Max(buys[i + 1], sales[i + 1] - prices[i]);
-            sales[i] = Math.Max(sales[i + 1], buys[i + 1] + prices[i] - fee);
+            int futureIndex = i + 1;
+            buys[i] = Math.Max(buys[futureIndex], sales[futureIndex] - prices[i]);
+            sales[i] = Math.Max(sales[futureIndex], buys[futureIndex] + prices[i] - fee);
         }
+
         return buys[0];
     }
 }
