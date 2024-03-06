2d082c42efb27241c30894787e0beaaf781708b1
Wirt S <34081557+Wirt4@users.noreply.github.com>
2024-03-06 14:14:19 -0800

    refactored tests

--- LeetCodeTests/DominoAndTrominoTilingTests.cs
+++ LeetCodeTests/DominoAndTrominoTilingTests.cs
@@ -2,44 +2,42 @@ namespace Tests;
 using LeetCodeSolutions;
 public class DominoAndTrominoTilingTests
 {
-  private DominoAndTrominoTilingSolution _solution;
-  public DominoAndTrominoTilingTests()
+  private class TestWrapper(int n)
   {
-    _solution = new();
+    private int _n = n;
+    private DominoAndTrominoTilingSolution _solution = new();
+
+    public void AssertNumTilingsIsEqualTo(int desired)
+    {
+      int actual = _solution.NumTilings(_n);
+      Assert.Equal(desired, actual);
+    }
   }
   [Fact]
   public void LCExample2()
   {
-    int n = 1;
-    int desired = 1;
-    int actual = _solution.NumTilings(n);
-    Assert.Equal(desired, actual);
+    TestWrapper testWrapper = new(1);
+    testWrapper.AssertNumTilingsIsEqualTo(1);
   }
 
   [Fact]
   public void TwoByTwoGrid()
   {
-    int n = 2;
-    int desired = 2;
-    int actual = _solution.NumTilings(n);
-    Assert.Equal(desired, actual);
+    TestWrapper testWrapper = new(2);
+    testWrapper.AssertNumTilingsIsEqualTo(2);
   }
 
   [Fact]
   public void LCExample1()
   {
-    int n = 3;
-    int desired = 5;
-    int actual = _solution.NumTilings(n);
-    Assert.Equal(desired, actual);
+    TestWrapper testWrapper = new(3);
+    testWrapper.AssertNumTilingsIsEqualTo(5);
   }
 
   [Fact]
   public void BreakingCase1()
   {
-    int n = 30;
-    int desired = 312342182;
-    int actual = _solution.NumTilings(n);
-    Assert.Equal(desired, actual);
+    TestWrapper testWrapper = new(30);
+    testWrapper.AssertNumTilingsIsEqualTo(312342182);
   }
 }
