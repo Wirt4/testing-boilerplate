07c2d6dddadd24f2c373624d82f7a16c136c24ed
Wirt S <34081557+Wirt4@users.noreply.github.com>
2024-03-06 14:05:15 -0800

    refactored tests

--- LeetCodeTests/BestTimeToBuyStockTests.cs
+++ LeetCodeTests/BestTimeToBuyStockTests.cs
@@ -7,62 +7,52 @@ public class BestTimeToBuyStockTests
   {
     _solution = new();
   }
-  private class Parameters
-  {
-    public int[] prices;
-    public int fee;
-  }
 
-  private void TestFunction(Parameters paramteters, int expected)
+  private class TestWrapper
   {
-    int actual = _solution.MaxProfit(paramteters.prices, paramteters.fee);
-    Assert.Equal(expected, actual);
+    private int[] _prices;
+    private int _fee;
+    private BestTimeToBuyStockSolution _solution;
+
+    public TestWrapper(int[] prices, int fee)
+    {
+      _prices = prices;
+      _fee = fee;
+      _solution = new();
+    }
+
+    public void AssertMaxProfitIsEqualTo(int expectedAnswer)
+    {
+      int actualAnswer = _solution.MaxProfit(_prices, _fee);
+      Assert.Equal(expectedAnswer, actualAnswer);
+    }
   }
 
   [Fact]
   public void ArrayOfTwo1()
   {
-    Parameters parameters = new()
-    {
-      prices = [1, 8],
-      fee = 2
-    };
-    int expected = 5;
-    TestFunction(parameters, expected);
+    TestWrapper testWrapper = new([1, 8], 2);
+    testWrapper.AssertMaxProfitIsEqualTo(5);
   }
 
   [Fact]
   public void ArrayOfTwo2()
   {
-    Parameters parameters = new()
-    {
-      prices = [4, 9],
-      fee = 3
-    };
-    int expected = 2;
-    TestFunction(parameters, expected);
+    TestWrapper testWrapper = new([4, 9], 3);
+    testWrapper.AssertMaxProfitIsEqualTo(2);
   }
 
   [Fact]
   public void LCExample1()
   {
-    Parameters parameters = new()
-    {
-      prices = [1, 3, 2, 8, 4, 9],
-      fee = 2
-    };
-    int expected = 8;
-    TestFunction(parameters, expected);
+    TestWrapper testWrapper = new([1, 3, 2, 8, 4, 9], 2);
+    testWrapper.AssertMaxProfitIsEqualTo(8);
   }
+
   [Fact]
   public void LCExample2()
   {
-    Parameters parameters = new()
-    {
-      prices = [1, 3, 7, 5, 10, 3],
-      fee = 3
-    };
-    int expected = 6;
-    TestFunction(parameters, expected);
+    TestWrapper testWrapper = new([1, 3, 7, 5, 10, 3], 3);
+    testWrapper.AssertMaxProfitIsEqualTo(6);
   }
 }
