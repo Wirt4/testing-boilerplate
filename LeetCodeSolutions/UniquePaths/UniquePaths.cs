b52817df5c7221be85199dff02c0280b1210328a
Wirt S <34081557+Wirt4@users.noreply.github.com>
2024-03-08 12:51:55 -0800

    move table to class

--- LeetCodeSolutions/UniquePaths/UniquePaths.cs
+++ LeetCodeSolutions/UniquePaths/UniquePaths.cs
@@ -1,47 +1,55 @@
 
 
+using System.ComponentModel;
+
 namespace LeetCodeSolutions;
 public class UniquePathsSolution
 {
-    private int[][] AllocateSpaceForPascalTable(int m, int n)
-    {
-        int[][] table = new int[m][];
-        for (int i = 0; i < m; i++)
-        {
-            table[i] = new int[n];
-        }
-        return table;
-    }
 
-    private void PopulateOnesOfPascalTable(ref int[][] table)
-    {
-        for (int i = 0; i < table.Length; i++)
-        {
-            table[i][0] = 1;
 
-        }
-        for (int j = 0; j < table[0].Length; j++)
-        {
-            table[0][j] = 1;
-        }
-    }
-
-    private void PopulateTableWithSums(ref int[][] table)
+    private class PascalTriangle
     {
-        for (int i = 1; i < table.Length; i++)
+        public int[][] table;
+        public PascalTriangle(int m, int n)
         {
-            for (int j = 1; j < table[0].Length; j++)
+            table = new int[m][];
+            for (int i = 0; i < m; i++)
             {
-                table[i][j] = table[i - 1][j] + table[i][j - 1];
+                table[i] = new int[n];
+            }
+        }
+
+        public void PopulateOnesOfPascalTable()
+        {
+            for (int i = 0; i < table.Length; i++)
+            {
+                table[i][0] = 1;
+
+            }
+            for (int j = 0; j < table[0].Length; j++)
+            {
+                table[0][j] = 1;
+            }
+        }
+
+        public void PopulateTableWithSums()
+        {
+            for (int i = 1; i < table.Length; i++)
+            {
+                for (int j = 1; j < table[0].Length; j++)
+                {
+                    table[i][j] = table[i - 1][j] + table[i][j - 1];
+                }
             }
         }
     }
 
+
     public int UniquePaths(int m, int n)
     {
-        int[][] table = AllocateSpaceForPascalTable(m, n);
-        PopulateOnesOfPascalTable(ref table);
-        PopulateTableWithSums(ref table);
-        return table[m - 1][n - 1];
+        PascalTriangle table = new(m, n);
+        table.PopulateOnesOfPascalTable();
+        table.PopulateTableWithSums();
+        return table.table[m - 1][n - 1];
     }
 }
