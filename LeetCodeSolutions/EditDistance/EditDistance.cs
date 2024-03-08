ade04dea408d6d3fb16fd7946ebf7b804bb5da5c
Wirt S <34081557+Wirt4@users.noreply.github.com>
2024-03-08 15:49:46 -0800

    remove unnecessary work

--- LeetCodeSolutions/EditDistance/EditDistance.cs
+++ LeetCodeSolutions/EditDistance/EditDistance.cs
@@ -1,42 +1,78 @@
-using System.Globalization;
-
 namespace LeetCodeSolutions;
 public class EditDistanceSolution
 {
 
-
-    public int MinDistance(string word1, string word2)
+    private class LevenshteinTable
     {
-        word1 = " " + word1;
-        word2 = " " + word2;
-        int[][] LevenshteinTable = new int[word1.Length][];
-        for (int i = 0; i < word1.Length; i++)
+        private string string1;
+        private string string2
+        ;
+        private int[][] table;
+        public LevenshteinTable(string word1, string word2)
         {
-            LevenshteinTable[i] = new int[word2.Length];
-            LevenshteinTable[i][0] = i;
+            string1 = AddLeadingSpacte(word1);
+            string2 = AddLeadingSpacte(word2);
+            table = InitializeTableSize();
         }
 
-        for (int i = 0; i < word2.Length; i++)
+        /**
+            the base case will always be an empty string, so adding a space the beginning to simulate that
+        **/
+        private string AddLeadingSpacte(string s)
         {
-            LevenshteinTable[0][i] = i;
+            return " " + s;
         }
 
-        for (int i = 1; i < word1.Length; i++)
+        private int[][] InitializeTableSize()
         {
-            for (int j = 1; j < word2.Length; j++)
+            int[][] LevenshteinTable = new int[string1.Length][];
+            for (int i = 0; i < string1.Length; i++)
             {
-                int previousCost = LevenshteinTable[i - 1][j - 1];
-                if (word1[i] == word2[j])
+                LevenshteinTable[i] = new int[string2.Length];
+                LevenshteinTable[i][0] = i;
+            }
+
+            for (int i = 0; i < string2.Length; i++)
+            {
+                LevenshteinTable[i][0] = i;
+            }
+
+            return LevenshteinTable;
+        }
+
+        public void CalculateSubProblems()
+        {
+            for (int i = 1; i < string1.Length; i++)
+            {
+                for (int j = 1; j < string2.Length; j++)
                 {
-                    LevenshteinTable[i][j] = previousCost;
-                    continue;
+                    int previousCost = table[i - 1][j - 1];
+                    if (string1[i] == string2[j])
+                    {
+                        table[i][j] = previousCost;
+                        continue;
+                    }
+                    int previousInsertionCost = table[i][j - 1];
+                    int previousDeletionCost = table[i - 1][j];
+                    table[i][j] = Math.Min(previousCost, Math.Min(previousInsertionCost, previousDeletionCost)) + 1;
                 }
-                int previousInsertionCost = LevenshteinTable[i][j - 1];
-                int previousDeletionCost = LevenshteinTable[i - 1][j];
-                LevenshteinTable[i][j] = Math.Min(previousCost, Math.Min(previousInsertionCost, previousDeletionCost)) + 1;
             }
         }
 
-        return LevenshteinTable[word1.Length - 1][word2.Length - 1];
+        public int LastCell => table[string1.Length - 1][string2.Length - 1];
+    }
+
+    public int MinDistance(string word1, string word2)
+    {
+        if (word1.Length == 0)
+        {
+            return word2.Length;
+        }
+
+        LevenshteinTable table = new(word1, word2);
+        table.CalculateSubProblems();
+
+        return table.LastCell;
     }
 }
+
