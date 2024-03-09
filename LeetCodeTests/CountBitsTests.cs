b1ae51c22a64865d42b07a18e9fcc3e15f8e1608
Wirt S <34081557+Wirt4@users.noreply.github.com>
2024-03-08 16:17:13 -0800

    create boilerplate

---
+++ LeetCodeTests/CountBitsTests.cs
@@ -0,0 +1,11 @@
+namespace Tests;
+using LeetCodeSolutions;
+ public class CountBitsTests {
+  private CountBitsSolution _solution;
+  public CountBitsTests (){
+      _solution = new();
+  }
+  [Fact]
+  public void Test1(){
+  }
+}
