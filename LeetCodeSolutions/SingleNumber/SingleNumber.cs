8c9a5081328ceee7cd6157fb2999934c73be64d7
Wirt S <34081557+Wirt4@users.noreply.github.com>
2024-03-09 18:05:12 -0800

    added minimum test

--- LeetCodeSolutions/SingleNumber/SingleNumber.cs
+++ LeetCodeSolutions/SingleNumber/SingleNumber.cs
@@ -1,2 +1,8 @@
 namespace LeetCodeSolutions;
-public class SingleNumberSolution {}
+public class SingleNumberSolution
+{
+    public int SingleNumber(int[] nums)
+    {
+        return -1;
+    }
+}
--- LeetCodeTests/SingleNumberTests.cs
+++ LeetCodeTests/SingleNumberTests.cs
@@ -1,11 +1,30 @@
 namespace Tests;
 using LeetCodeSolutions;
- public class SingleNumberTests {
-  private SingleNumberSolution _solution;
-  public SingleNumberTests (){
+public class SingleNumberTests
+{
+
+
+  private class SingleNumberTester
+  {
+    private SingleNumberSolution _solution;
+    private int[] _nums;
+    public SingleNumberTester(int[] nums)
+    {
+      _nums = nums;
       _solution = new();
+    }
+
+    public void AssertSingleNumberOutputEquals(int expectedAnswer)
+    {
+      int output = _solution.SingleNumber(_nums);
+      Assert.Equal(expectedAnswer, output);
+    }
   }
   [Fact]
-  public void Test1(){
+  public void MinimumCase1()
+  {
+    SingleNumberTester singleNumber = new([2, 2, 1]);
+    singleNumber.AssertSingleNumberOutputEquals(1);
+
   }
 }
