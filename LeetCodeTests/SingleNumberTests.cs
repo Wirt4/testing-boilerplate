1dd21bd2741b629007c4527c8feedb8c9d3b20ed
Wirt S <34081557+Wirt4@users.noreply.github.com>
2024-03-09 18:00:15 -0800

    bolierplate

---
+++ LeetCodeTests/SingleNumberTests.cs
@@ -0,0 +1,11 @@
+namespace Tests;
+using LeetCodeSolutions;
+ public class SingleNumberTests {
+  private SingleNumberSolution _solution;
+  public SingleNumberTests (){
+      _solution = new();
+  }
+  [Fact]
+  public void Test1(){
+  }
+}
