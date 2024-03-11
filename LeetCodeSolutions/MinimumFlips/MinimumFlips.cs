8d11d5fbbf4aa51965a86c250449eaa1af06dd32
Wirt S <34081557+Wirt4@users.noreply.github.com>
2024-03-10 17:43:21 -0700

    added test for first leetcode example

--- LeetCodeSolutions/MinimumFlips/MinimumFlips.cs
+++ LeetCodeSolutions/MinimumFlips/MinimumFlips.cs
@@ -1,2 +1,8 @@
 namespace LeetCodeSolutions;
-public class MinimumFlipsSolution {}
+public class MinimumFlipsSolution
+{
+    public int MinFlips(int a, int b, int c)
+    {
+        return -1;
+    }
+}
--- LeetCodeTests/MinimumFlipsTests.cs
+++ LeetCodeTests/MinimumFlipsTests.cs
@@ -1,11 +1,32 @@
 namespace Tests;
 using LeetCodeSolutions;
- public class MinimumFlipsTests {
-  private MinimumFlipsSolution _solution;
-  public MinimumFlipsTests (){
-      _solution = new();
+public class MinimumFlipsTests
+{
+
+  private class MinFlipsTest
+  {
+    private int a;
+    private int b;
+    private int c;
+    private MinimumFlipsSolution solution;
+    public MinFlipsTest(int a, int b, int c)
+    {
+      solution = new();
+      this.a = a;
+      this.b = b;
+      this.c = c;
+    }
+
+    public void AssertOutputIsEqualTo(int expectedOutput)
+    {
+      int actual = solution.MinFlips(a, b, c);
+      Assert.Equal(actual, expectedOutput);
+    }
   }
   [Fact]
-  public void Test1(){
+  public void LCExample1()
+  {
+    MinFlipsTest test = new(2, 6, 5);
+    test.AssertOutputIsEqualTo(3);
   }
 }
