cba72644eec3ad6d55afc06b5b6b9cdc05e3d305
Wirt S <34081557+Wirt4@users.noreply.github.com>
2024-03-11 13:26:27 -0700

    passes green

--- LeetCodeSolutions/ImplementTrie/ImplementTrie.cs
+++ LeetCodeSolutions/ImplementTrie/ImplementTrie.cs
@@ -1,14 +1,60 @@
+using System.ComponentModel.Design.Serialization;
+
 namespace LeetCodeSolutions;
-public class ImplementTrieSolution
+
+
+public class Trie
 {
+    private class TrieNode
+    {
+        public TrieNode?[] children = new TrieNode[26];
+        public bool EndOfWord;
+        public TrieNode()
+        {
+            EndOfWord = false;
+            for (int i = 0; i < 26; i++)
+                children[i] = null;
+        }
+    }
+
+    private TrieNode trieRoot;
+
+    public Trie()
+    {
+        trieRoot = new();
+    }
     public void Insert(string word)
     {
+        TrieNode cursor = trieRoot;
+        for (int treeLevel = 0; treeLevel < word.Length; treeLevel++)
+        {
+            int ndx = word[treeLevel] - 'a';
+            if (cursor.children[ndx] == null)
+            {
+                cursor.children[ndx] = new();
+            }
+            cursor = cursor.children[ndx];
+            if (treeLevel == word.Length - 1)
+            {
+                cursor.EndOfWord = true;
+            }
+        }
 
     }
 
     public bool Search(string word)
     {
-        return true;
+        TrieNode searchNode = trieRoot;
+        for (int treeLevel = 0; treeLevel < word.Length; treeLevel++)
+        {
+            int ndx = word[treeLevel] - 'a';
+            if (searchNode.children[ndx] == null)
+            {
+                return false;
+            }
+            searchNode = searchNode.children[ndx];
+        }
+        return searchNode.EndOfWord;
     }
 
     public bool StartsWith(string prefix)
--- LeetCodeTests/ImplementTrieTests.cs
+++ LeetCodeTests/ImplementTrieTests.cs
@@ -1,14 +1,13 @@
 namespace Tests;
 
-using System.Diagnostics.CodeAnalysis;
 using LeetCodeSolutions;
 public class ImplementTrieTests
 {
-  private ImplementTrieSolution _Trie;
+
   [Fact]
   public void SearchWordTrue()
   {
-    _Trie = new();
+    Trie _Trie = new();
     _Trie.Insert("word");
     Assert.True(_Trie.Search("word"));
   }
@@ -16,7 +15,7 @@ public class ImplementTrieTests
   [Fact]
   public void SearchWordFalse()
   {
-    _Trie = new();
+    Trie _Trie = new();
     _Trie.Insert("word");
     Assert.False(_Trie.Search("bird"));
   }
