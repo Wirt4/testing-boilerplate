124fd571973ea9dd507851aea2ed900fcda44b58
Wirt S <34081557+Wirt4@users.noreply.github.com>
2024-03-15 16:07:17 -0700

    remove redundant check

--- LeetCodeSolutions/KokoEatsBananas/KokoEatsBananas.cs
+++ LeetCodeSolutions/KokoEatsBananas/KokoEatsBananas.cs
@@ -23,38 +23,26 @@ public class KokoEatsBananasSolution
     public int MinEatingSpeed(int[] piles, int h)
     {
         (int min, int max) rates = new(piles[0], piles[0]);
-
         foreach (int pile in piles)
         {
             rates.min = Math.Min(rates.min, pile);
             rates.max = Math.Max(rates.max, pile);
         }
-
         if (piles.Length == h)
         {
             return rates.max;
         }
-
         rates.min = CelingDivide(rates.min, h);
-
         while (rates.min < rates.max)
         {
-            if (rates.min == rates.max)
-            {
-                return rates.min;
-            }
-
             int m = (rates.min + rates.max) / 2;
-
             if (IsViableRate(m, piles, h))
             {
                 rates.max = m;
                 continue;
             }
-
             rates.min = m + 1;
         }
-
         return rates.min;
     }
 }
