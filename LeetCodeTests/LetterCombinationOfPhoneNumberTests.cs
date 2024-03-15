776ffea55ffd99d06bfb68b813f811fd252a33e0
Wirt S <34081557+Wirt4@users.noreply.github.com>
2024-03-15 16:30:21 -0700

    refactored tests

--- LeetCodeTests/LetterCombinationOfPhoneNumberTests.cs
+++ LeetCodeTests/LetterCombinationOfPhoneNumberTests.cs
@@ -1,56 +1,60 @@
 namespace Tests;
 using LeetCodeSolutions;
-using NuGet.Frameworks;
+
 
 public class LetterCombinationOfPhoneNumberTests
 {
-  private LetterCombinationOfPhoneNumberSolution _solution;
-  public LetterCombinationOfPhoneNumberTests()
+  private class TestWrapper
   {
-    _solution = new();
+    private string input;
+    private LetterCombinationOfPhoneNumberSolution _solution;
+    public TestWrapper(string input)
+    {
+      this.input = input;
+      _solution = new();
+    }
+
+    public void AssertIsEqualTo(IList<string> desiredOutcome)
+    {
+      IList<string> result = _solution.LetterCombinations(input);
+      Assert.Equal(desiredOutcome, result);
+    }
   }
+
   [Fact]
   public void EmptyString()
   {
-    string input = "";
-    IList<string> output = [];
-    IList<string> result = _solution.LetterCombinations(input);
-    Assert.Equal(output, result);
+    TestWrapper test = new("");
+    test.AssertIsEqualTo([]);
   }
 
   [Fact]
   public void StringOfLength1Case1()
   {
-    string input = "2";
-    IList<string> output = ["a", "b", "c"];
-    IList<string> result = _solution.LetterCombinations(input);
-    Assert.Equal(output, result);
+    TestWrapper test = new("2");
+    test.AssertIsEqualTo(["a", "b", "c"]);
   }
 
   [Fact]
   public void StringOfLength1Case2()
   {
-    string input = "7";
-    IList<string> output = ["p", "q", "r", "s"];
-    IList<string> result = _solution.LetterCombinations(input);
-    Assert.Equal(output, result);
+    TestWrapper test = new("7");
+    test.AssertIsEqualTo(["p", "q", "r", "s"]);
   }
 
   [Fact]
   public void Example1()
   {
-    string input = "23";
+    TestWrapper test = new("23");
     IList<string> output = ["ad", "ae", "af", "bd", "be", "bf", "cd", "ce", "cf"];
-    IList<string> result = _solution.LetterCombinations(input);
-    Assert.Equal(output, result);
+    test.AssertIsEqualTo(output);
   }
 
   [Fact]
   public void Example22()
   {
-    string input = "22";
+    TestWrapper test = new("22");
     IList<string> output = ["aa", "ab", "ac", "ba", "bb", "bc", "ca", "cb", "cc"];
-    IList<string> result = _solution.LetterCombinations(input);
-    Assert.Equal(output, result);
+    test.AssertIsEqualTo(output);
   }
 }
