07db30ca3215e394b36d345d34052600890e1818
Wirt S <34081557+Wirt4@users.noreply.github.com>
2024-03-15 17:44:37 -0700

    first test

--- LeetCodeTests/SearchSuggestionsSystemTests.cs
+++ LeetCodeTests/SearchSuggestionsSystemTests.cs
@@ -1,11 +1,32 @@
 namespace Tests;
 using LeetCodeSolutions;
- public class SearchSuggestionsSystemTests {
-  private SearchSuggestionsSystemSolution _solution;
-  public SearchSuggestionsSystemTests (){
+public class SearchSuggestionsSystemTests
+{
+
+  private class TestWrapper
+  {
+    private string[] _products;
+    private string _searchWord;
+    private SearchSuggestionsSystemSolution _solution;
+    public TestWrapper(string[] products, string searchWord)
+    {
+      _products = products;
+      _searchWord = searchWord;
       _solution = new();
+    }
+
+    public void AssertAnswerIsEqualTo(IList<IList<string>> expected)
+    {
+      IList<IList<string>> answer = _solution.SuggestedProducts(_products, _searchWord);
+      Assert.Equal(expected, answer);
+    }
   }
   [Fact]
-  public void Test1(){
+  public void LCExample1()
+  {
+    string[] products = ["mobile", "mouse", "moneypot", "monitor", "mousepad"];
+    TestWrapper test = new(products, "mouse");
+    IList<IList<string>> output = [["mobile", "moneypot", "monitor"], ["mobile", "moneypot", "monitor"], ["mouse", "mousepad"], ["mouse", "mousepad"], ["mouse", "mousepad"]];
+    test.AssertAnswerIsEqualTo(output);
   }
 }
