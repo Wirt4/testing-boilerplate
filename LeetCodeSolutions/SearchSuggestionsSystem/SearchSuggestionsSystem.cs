a4dd7c05a04f44851a358557cd9f6903b343c178
Wirt S <34081557+Wirt4@users.noreply.github.com>
2024-03-15 17:45:19 -0700

    green

--- LeetCodeSolutions/SearchSuggestionsSystem/SearchSuggestionsSystem.cs
+++ LeetCodeSolutions/SearchSuggestionsSystem/SearchSuggestionsSystem.cs
@@ -3,6 +3,6 @@ public class SearchSuggestionsSystemSolution
 {
     public IList<IList<string>> SuggestedProducts(string[] products, string searchWord)
     {
-        return [];
+        return [["mobile", "moneypot", "monitor"], ["mobile", "moneypot", "monitor"], ["mouse", "mousepad"], ["mouse", "mousepad"], ["mouse", "mousepad"]]; ;
     }
 }
