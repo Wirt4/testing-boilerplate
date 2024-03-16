49ea25b80ce118e5574fdef5d3ec43a5e264e855
Wirt S <34081557+Wirt4@users.noreply.github.com>
2024-03-15 17:38:14 -0700

    Initialized with README

---
+++ LeetCodeSolutions/SearchSuggestionsSystem/SearchSuggestionsSystem.cs
@@ -0,0 +1,8 @@
+namespace LeetCodeSolutions;
+public class SearchSuggestionsSystemSolution
+{
+    public IList<IList<string>> SuggestedProducts(string[] products, string searchWord)
+    {
+        return [];
+    }
+}
---
+++ LeetCodeTests/SearchSuggestionsSystemTests.cs
@@ -0,0 +1,11 @@
+namespace Tests;
+using LeetCodeSolutions;
+ public class SearchSuggestionsSystemTests {
+  private SearchSuggestionsSystemSolution _solution;
+  public SearchSuggestionsSystemTests (){
+      _solution = new();
+  }
+  [Fact]
+  public void Test1(){
+  }
+}
