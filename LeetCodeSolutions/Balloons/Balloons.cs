5f441372490c6f9bf68d5d1f36fe7b63cfe812ae
Wirt S <34081557+Wirt4@users.noreply.github.com>
2024-03-18 12:10:48 -0700

    green

--- LeetCodeSolutions/Balloons/Balloons.cs
+++ LeetCodeSolutions/Balloons/Balloons.cs
@@ -1,15 +1,37 @@
 namespace LeetCodeSolutions;
 public class BalloonsSolution
 {
+    private static void SortByColumn<T>(T[][] data, int column)
+    {
+        Comparer<T> comparer = Comparer<T>.Default;
+        Array.Sort<T[]>(data, (x, y) => comparer.Compare(x[column], y[column]));
+    }
     public int FindMinArrowShots(int[][] points)
     {
-        int arrows = points.Length;
-        for (int i = 1; i < points.Length; i++)
+        SortByColumn<int>(points, 1);
+        int arrows = 0;
+        int i = 0;
+        while (i < points.Length)
         {
-            if (points[i][0] < points[i - 1][1])
+            if (i == points.Length - 1 || points[i][1] < points[i + 1][0])
             {
-                arrows--;
+                arrows++;
+                i++;
+                continue;
             }
+
+            int j = i + 1;
+            while (j < points.Length && points[i][1] >= points[j][0])
+            {
+                j++;
+            }
+            arrows++;
+            if (j == points.Length - 1)
+            {
+                break;
+            }
+            i = j;
+
         }
         return arrows;
     }
