c1cb285b8e3583c1070a0cd29acb8cf88c16142b
Wirt S <34081557+Wirt4@users.noreply.github.com>
2024-05-04 17:58:06 -0700

    stuff

--- LeetCodeSolutions/GasStation/GasStation.cs
+++ LeetCodeSolutions/GasStation/GasStation.cs
@@ -1,64 +1,26 @@
 namespace LeetCodeSolutions;
 public class GasStationSolution
 {
-    private class Trip(int startingIndex)
-    {
-        private int currentIndex = startingIndex;
-        private int tank = 0;
-        private readonly int startingIndex = startingIndex;
-        public int Index
-        {
-            get => currentIndex;
-        }
-        public bool Traversed
-        {
-            get => currentIndex == startingIndex;
-        }
-        public bool MakeTrip(int[] gas, int[] cost)
-        {
-            int nextIndex = (currentIndex + 1) % gas.Length;
-            tank += gas[currentIndex];
-            if (tank < cost[nextIndex])
-            {
-                return false;
-            }
-            tank -= cost[currentIndex];
-            currentIndex = nextIndex;
-            return true;
-        }
-    }
-
-    private Stack<Trip> InitiateTrips(int arrLen)
-    {
-        Stack<Trip> Trips = new Stack<Trip>();
-        for (int i = 0; i < arrLen; i++)
-        {
-            Trips.Push(new Trip(i));
-        }
-        return Trips;
-    }
     public int CanCompleteCircuit(int[] gas, int[] cost)
     {
-        int current = 0;
-        int total = 0;
+        int tank = 0;
+        int totalGas = 0;
+        int startingIndex = 0;
         int diff;
-        int start = 0;
+
         for (int i = 0; i < gas.Length; i++)
         {
             diff = gas[i] - cost[i];
-            total += diff;
-            current += diff;
-            if (current < 0)
+            totalGas += diff;
+            tank += diff;
+
+            if (tank < 0)
             {
-                start = i + 1;
-                current = 0;
+                startingIndex = i + 1;
+                tank = 0;
             }
         }
-        if (total < 0)
-        {
-            return -1;
-        }
 
-        return start;
+        return totalGas < 0 ? -1 : startingIndex;
     }
 }
