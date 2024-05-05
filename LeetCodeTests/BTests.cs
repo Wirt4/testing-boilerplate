6e86b556fcbd5bc30b86d2a1ae4e60f580d5d1cb
Wirt S <34081557+Wirt4@users.noreply.github.com>
2024-05-04 19:14:14 -0700

    branch

---
+++ LeetCodeTests/BTests.cs
@@ -0,0 +1,11 @@
+namespace Tests;
+using LeetCodeSolutions;
+ public class BTests {
+  private BSolution _solution;
+  public BTests (){
+      _solution = new();
+  }
+  [Fact]
+  public void Test1(){
+  }
+}
