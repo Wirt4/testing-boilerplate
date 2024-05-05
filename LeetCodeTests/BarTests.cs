3094ca5d4d21678ad08488c8129f91929b98023c
Wirt S <34081557+Wirt4@users.noreply.github.com>
2024-05-04 19:08:32 -0700

    clean tests

---
+++ LeetCodeTests/BarTests.cs
@@ -0,0 +1,11 @@
+namespace Tests;
+using LeetCodeSolutions;
+ public class BarTests {
+  private BarSolution _solution;
+  public BarTests (){
+      _solution = new();
+  }
+  [Fact]
+  public void Test1(){
+  }
+}
---
+++ LeetCodeTests/FooTests.cs
@@ -0,0 +1,11 @@
+namespace Tests;
+using LeetCodeSolutions;
+ public class FooTests {
+  private FooSolution _solution;
+  public FooTests (){
+      _solution = new();
+  }
+  [Fact]
+  public void Test1(){
+  }
+}
